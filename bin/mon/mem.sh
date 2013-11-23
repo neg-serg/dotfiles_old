a734e75176180d5485dad7b1c5841320ae20510e
Neg-serg <serg.zorg@gmail.com>
2013-11-23 14:31:14 +0400

    continue garbage collection in ~/bin
    Former-commit-id: 249422e2c54059ae268ca4c6a200b4cb8bc586f2

--- bin/mon/mem.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-free -m | awk '/\-\/\+ buffers\/cache/ {print $3}'
--- bin/mon/mounts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-f=`cat /proc/mounts|grep usb|grep -v usbfs|cut -d '/' -f3|tr -d ' '|tr -d '\n';`&&
-if [[ $f != "" ]];then echo"[ $f ]";else echo ""; fi
