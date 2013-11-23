249422e2c54059ae268ca4c6a200b4cb8bc586f2
Neg-serg <serg.zorg@gmail.com>
2013-11-23 14:31:14 +0400

    continue garbage collection in ~/bin

--- bin/mon/batt.sh
+++ bin/mon/batt.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-a=`grep charged /proc/acpi/battery/BAT0/state`
-if [[ $a == "" ]]; then echo "[ batt: `acpi|awk '{print $4}'|tr -d ","` ]" ;else echo "";fi
+echo "[ batt: `acpi|awk '{print $4}'|tr -d ","` ]" 
