55634289937e98be0c5063d939e4556361c9bfac
Neg-serg <serg.zorg@gmail.com>
2013-12-05 15:01:59 +0400

    update jumpdirs

--- .zsh/14-dirjumps.zsh
+++ .zsh/14-dirjumps.zsh
@@ -1,10 +1,18 @@
-bindkey -s "1" 'cd ~/1st_level'
-bindkey -s "2" 'cd ~/dw'
-bindkey -s "3" 'cd ~/dev'
-bindkey -s "4" 'cd ~/pic'
-bindkey -s "5" 'cd ~/vid'
-bindkey -s "6" 'cd ~/trash'
+jump_dirs=( 
+                ~/1st_level  
+                ~/dw         
+                ~/dev        
+                ~/pic        
+                ~/vid        
+                ~/trash      
+            )
 
+for index in $(seq 1 $((${#jump_dirs[@]} )))
+do
+    # echo "jump_dir ${index} is ${jump_dirs[$index]}"
+    bindkey -s "${index}" "cd ${jump_dirs[$index]}"
+done
+  
 # function up-one-dir   { pushd .. > /dev/null; zle redisplay; zle -M `pwd` }
 # function back-one-dir { popd     > /dev/null; zle redisplay; zle -M `pwd` }
 # zle -N up-one-dir
