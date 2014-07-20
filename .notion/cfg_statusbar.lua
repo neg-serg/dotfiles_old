33ee373c023b886b4d71fc4f26cf166645f8168f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2014-07-20 22:59:12 +0400

    return stalonetray support
    Former-commit-id: d5b66a456dc0598fc94aa797d871003a9cd2102d [formerly ef90bef20283f6655192641ab967194a00c95b04]
    Former-commit-id: 1252ff2044f47adbc6a13bc7c866aef39193bfc4

--- .notion/cfg_statusbar.lua
+++ .notion/cfg_statusbar.lua
@@ -10,11 +10,11 @@ mod_statusbar.create{
     "[ %>workspace_name ]"..
     "[ %exec_xkb ]"..
     "[ net: %netmon_kbsin/%netmon_kbsout ]"..
-   --  "[ %linuxbatt ]"..
+    -- "[ %linuxbatt ]"..
     "%filler"..
-   --  "%mpd"
-    -- "%linuxbatt"..
-    "%>exec_kt"
+    --  "%mpd"
+    "%>exec_kt"..
+    "%systray%filler%systray_stalone"
     --" | %net_essid_wlan0 %net_strength_wlan0 :: %net_in_eth0/%net_out_eth0 :: %net_in_ppp0/%net_out_ppp0"
     --" | %systray_cpu %laptopstatus_temperature"..
     --" | %systray %colorize%date ",
@@ -24,14 +24,13 @@ mod_statusbar.create{
 -- Launch ion-statusd. This must be done after creating any statusbars
 -- for necessary modules to be parsed from the templates.
 mod_statusbar.launch_statusd{
-  date={ date_format='[ %H:%M ]',
-       --date_format='[ %H:%M ][ %a %d %b %y ]',
-  },
+    date={ date_format='[ %H:%M ]',
+        --date_format='[ %H:%M ][ %a %d %b %y ]',
+    },
 
-exec = {
-    xkb={program = "~/bin/mon/klay",retry_delay = 1 * 300,},
-    kt={program ="~/bin/mon/crntsng.sh",retry_delay=1 * 1000,},
-}
-
+    exec = {
+        xkb={program = "~/bin/mon/klay",retry_delay = 1 * 300,},
+        kt={program ="~/bin/mon/crntsng.sh",retry_delay=1 * 1000,},
+    }
 }
 
