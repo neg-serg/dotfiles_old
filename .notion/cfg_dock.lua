1dae7faf8b21fa4459049fd43d1d155ca742e68f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2014-08-19 12:13:45 +0400

    fix dock config

--- .notion/cfg_dock.lua
+++ .notion/cfg_dock.lua
@@ -11,7 +11,7 @@ mod_dock.create{
     -- Corner or side of the screen to place the dock on.
     -- For embedded dock the valid values are: tl|tr|bl|br
     -- For floating dock the following are also valid: tc|bc|ml|mc|mr
-    pos="bc",
+    -- pos="bc",
     -- Growth direction: left|right|up|down
     grow="right",
     -- Whether new dockapps should be added automatically to this dock
@@ -26,7 +26,7 @@ mod_dock.create{
 -- For floating docks, you may want the following toggle binding.
 defbindings("WScreen", {
     bdoc("Toggle floating dock."),
-    kpress(META.."D", "mod_dock.set_floating_shown_on(_, 'toggle')")
+    -- kpress(META.."D", "mod_dock.set_floating_shown_on(_, 'toggle')")
 })
 
 
