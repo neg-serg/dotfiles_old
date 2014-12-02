dc207bb41b7dcb4b70dc95de0f75c1ca487fe4e1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2014-12-02 21:41:45 +0300

    add comments from original file
    Former-commit-id: d18a6741192646914dc44a6190860c97790d2a58 [formerly 3ecb6eeede7b809bf050f8f7f1dc973eecae4fa9] [formerly 628bebac6764c460395ec8bb2ef537a1f841c721 [formerly 4d25da45c42dd13b1c4c536c07770223a5cc4ecd]]
    Former-commit-id: e3c4d123b73d90f53c855edbec0d37acb2e15076 [formerly b0243f87045bd33ac2b97e5e951b5bf851c2d4c6]
    Former-commit-id: 5e376bf80720a6411719b982ce75c187c1723a8c

--- .zsh/60-functional.zsh
+++ .zsh/60-functional.zsh
@@ -1,3 +1,57 @@
+# These scripts provide a functional shell environment. This gives you the ability
+# to use constructs similar to those in functional programming languages, but on
+# the shell level.
+#
+# This project is uninspired at present and needs ideas.
+#
+# The 'each' command takes each line of input and maps it to _ in the command you
+# give it. Iteration terminates if the command returns nonzero status.
+#
+# ls | each echo _                                        # The same as ls
+# ls | each cat _                                         # Concatenates everything in this directory
+# ls | each 'ln -s _ _.txt'                               # Symlinks each file as file.txt
+# ls | each 'ln -s _ $(md5sum _ | awk "{print \$1}")'     # Symlinks each file as the MD5 of its contents
+# ls | each 'test -f _ && echo _ || :'                    # Only print regular files
+# ls | each 'echo _ $(($(wc -c < _) / $(wc -l < _)))'     # Display characters per line for each file
+#
+# You can also use 'each' to emulate other concepts:
+#
+# ls | each cat _                                         # This is really flat-mapping across files and lines of files
+# ls | each 'test -d _ && ls _ || echo _'                 # Flat-map one level across directories
+# ls | each 'test -d _ && ls _ | cat || echo _'           # Ditto, but this one gets proper output from ls
+#
+# And you can use it with things besides ls:
+#
+# ps -e | grep '\bdhclient\b' | cut -d' ' -f1 | each kill -9 _
+# ifconfig | grep '^\w' | sed 's/\s.*//' | each ifconfig _ down
+#
+# 'peach' is like 'each', but runs its jobs in parallel:
+#
+# seq 1 10 | peach 'sleep 1 && echo _'                    # Returns after one second
+#
+# The 'y' command repeats something until the command returns nonzero status. From
+# a functional point of view, it's a sort of fixed point on system state.
+#
+# y ps -el | grep perl                                    # Print all Perl processes continuously (CPU hog)
+# y 'sleep 1 && ps -el' | grep perl                       # Print all Perl processes each second (not a CPU hog)
+# y 'sleep 1 && ps -el | grep perl | wc -l'               # Print the number of Perl processes each second
+# y 'test -f foo || touch foo'                            # Make sure the file 'foo' never goes away (CPU hog)
+# y y                                                     # No clue, but seems dangerous
+#
+# 'uncat' does the opposite of cat:
+#
+# cat foo | command                                       # Prints the contents of foo into the command
+# command | uncat bar                                     # Writes the output of command into bar
+# command | uncat                                         # Writes the output of command into a tempfile and prints the tempfile
+#
+# In general, uncat has the invariant that 'cat $(cmd | uncat)' is equivalent to 'cmd', modulo buffering.
+#
+# 'repeat' repeats something a fixed number of times:
+#
+# repeat 10 echo hi                                       # Says 'hi' ten times
+
+# This is useful for things like benchmarking.
+
 function each(){
     while read line; do
         eval "${*//_/'$line'}" &
