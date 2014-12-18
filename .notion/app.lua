0e63936ffe74439dc064bd93275a7b882e0e07ae
Sergey Miroshnichenko <serg.zorg@gmail.com>
2014-12-18 10:10:44 +0300

    goto_focus fix for new lua
    Former-commit-id: 27a68a9bdf93d6f02261672cc797528431cee19f [formerly a2ecf9bb2bd52a8e7705bb6ee68d78d8e0503d6e] [formerly 399c6eb5b7eebeefcef8eeeee68012c9b96a18ef [formerly 9c8f0fe4de6a9e737475994b3c6dfd9362976b2e]]
    Former-commit-id: c5fe0c83314426668d3385d4012dfc91c1ac4068 [formerly 3ad0c60a0ff213050f83177c143f227eb71f085c]
    Former-commit-id: 96d7b29259c8ff085153b5535dbbb1bfc6aeb4e7

--- .notion/app.lua
+++ .notion/app.lua
@@ -1,77 +1,79 @@
 app={}
+moveapp={}
+
 function app.match_class(class, instance)
-   -- Return matching client windows as a table.
-   -- If the current window is among them, it's placed last on the list
-   -- and its successor at the beginning of the list. This facilitates
-   -- cycling multiple windows of an application.
-   local result = {}
-   local offset = 0
-   local currwin = ioncore.current()
-   ioncore.clientwin_i(function (win)
-      if class == win:get_ident().class then
-        if instance then
-            if instance == win:get_ident().instance then
+    -- Return matching client windows as a table.
+    -- If the current window is among them, it's placed last on the list
+    -- and its successor at the beginning of the list. This facilitates
+    -- cycling multiple windows of an application.
+    local result = {}
+    local offset = 0
+    local currwin = ioncore.current()
+    ioncore.clientwin_i(function (win)
+        if class == win:get_ident().class then
+            if instance then
+                if instance == win:get_ident().instance then
+                    table.insert(result, #(result)-offset+1, win)
+                end
+            else
                 table.insert(result, #(result)-offset+1, win)
             end
-        else
-            table.insert(result, #(result)-offset+1, win)
         end
-      end
-      if win == currwin then
-	 -- Current client window found, continue filling the table from
-	 -- the beginning.
-	 offset = #(result)
-      end
-      return true
-   end)
+        if win == currwin then
+            -- Current client window found, continue filling the table from
+            -- the beginning.
+            offset = #(result)
+        end
+        return true
+    end)
 
-   return result
+    return result
 end
 
 function app.byname(prog, name, where)
-   local win = ioncore.lookup_clientwin(name)
-   if win then
-      ioncore.defer(function () win:goto_focus() end)
-   else
-      if where then
-	  ioncore.exec_on(where, prog)
-      else
-	  ioncore.exec(prog)
-      end
-   end
+    local win = ioncore.lookup_clientwin(name)
+    if win then
+        ioncore.defer(function () win:goto_focus() end)
+    else
+        if where then
+            ioncore.exec_on(where, prog)
+        else
+            ioncore.exec(prog)
+        end
+    end
 end
 
 function app.byclass(prog, class, where)
-   local win = app.match_class(class)[1]
-   if win then
-      ioncore.defer(function () win:goto_focus() end)
-   else
-      if where then
-	  ioncore.exec_on(where, prog)
-      else
-	  ioncore.exec(prog)
-      end
-   end
+    local win = app.match_class(class)[1]
+    if win then
+        ioncore.defer(function () win:goto_focus() end)
+    else
+        if where then
+            ioncore.exec_on(where, prog)
+        else
+            ioncore.exec(prog)
+        end
+    end
 end
 
 function app.byinstance(prog, class, instance, where)
-   local win = app.match_class(class, instance)[1]
-   if win then
-      ioncore.defer(function () win:goto_focus() end)
-   else
-      if where then
-	  ioncore.exec_on(where, prog)
-      else
-	  ioncore.exec(prog)
-      end
-   end
+    local win = app.match_class(class, instance)[1]
+    if win then
+        ioncore.defer(function () win:goto_focus() end)
+    else
+        if where then
+            ioncore.exec_on(where, prog)
+        else
+            ioncore.exec(prog)
+        end
+    end
 end
 
 function moveapp.byclass(ws, class)
-   local win = app.match_class(class)[1]
-   if win then
-       local frame = ws:current()
-       frame:attach(win, { switchto = true })
-       win:goto()
-   end
+    local win = app.match_class(class)[1]
+    if win then
+        local frame = ws:current()
+        frame:attach(win, { switchto = true })
+        win:goto_focus()
+    end
 end
