55ed27fe9be964c3e7656dadb652f6b2e4b06de2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-01-07 14:04:39 +0300

    new kludges and various update for cfg_notion
    Former-commit-id: 022f832d4a954b0f3c18ba3849c4ec9561bb9726 [formerly 468fd171f53813aadf197076558a03699a629dc8] [formerly 9b767861c479c89ebb6723a3e81fc83e436a7f55 [formerly b4063237ebd73bc2b3a01e5eb2e8dad3ffb60696]]
    Former-commit-id: f339748dae0be695aafe64b209bc2aaa9fb21215 [formerly 83e19118f1880627db3891a5c16351867172dd84]
    Former-commit-id: d606f1483cfb48ca94f6ca3a48af8c5b9b27e742

--- .notion/cfg_notion.lua
+++ .notion/cfg_notion.lua
@@ -9,6 +9,9 @@ ioncore.set{
     ioncore.defshortening("[^:]+: (.*)(<[0-9]+>)", "$1$2$|$1$<...$2"),
     ioncore.defshortening("[^:]+: (.*)", "$1$|$1$<..."),
     ioncore.defshortening("(.*)", "$1$|$1$<..."),
+    ioncore.defshortening("(.*) - Mozilla(<[0-9]+>)", "$1$2$|$1$<...$2"),
+    ioncore.defshortening("(.*) - Mozilla", "$1$|$1$<..."),
+    ioncore.defshortening("XMMS - (.*)", "$1$|...$>$1"),
     ignore_net_active_window=false,
     float_placement_padding=2,
     warp=false,
@@ -44,6 +47,10 @@ dopath("mod_notionflux")
 dopath("vim_bindings")
 dopath("goto-by-tag")
 dopath("mod_statusbar")
+dopath("nest_ws")
+dopath("screenshot")
+-- dopath("mod_dock")
+-- dopath("cfg_dock")
 -------------------------------------[[ KLUDGES ]]----------------------------------
 defwinprop{lazy_resize=true}
 -------------------------------------[[ TERM ]]-------------------------------------
@@ -62,6 +69,7 @@ defwinprop{class="Dwb",transient_mode="off",jumpto="on",target="web",lazy_resize
 defwinprop{class="Firefox",role="browser",transient_mode="off",jumpto="on",target="web",lazy_resize=true,tag="browser"}
 defwinprop{class="Firefox",role="Manager",instance="Download",transient_mode="off",jumpto="off",target="float2",lazy_resize=true,tag="browser"}
 defwinprop{class="Firefox",instance="Dialog",float=true,tag="browser"}
+defwinprop{class="Firefox",role="Organizer",target="float2",tag="browser"}
 defwinprop{class="Firefox",instance="firefox",role="GtkFileChooserDialog",
     max_size = {w=1024,h=768},
     min_size = {w=800,h=600},
@@ -83,6 +91,7 @@ defwinprop{class="Okular",instance="okular",transient_mode="off",jumpto=true,tar
 defwinprop{name="Open Document*",class="Okular",instance="okular",transient_mode="off",jumpto=true,target="doc",float=true,lazy_resize=true}
 defwinprop{class="Apvlv",instance="apvlv",transient_mode="off",jumpto=true,target="doc",lazy_resize=true}
 defwinprop{class="Zathura",instance="zathura",transient_mode="off",jumpto=true,target="doc",lazy_resize=true}
+defwinprop{ class = "Xpdf", instance = "openDialog_popup", ignore_cfgrq = true}
 defwinprop{class="Evince",instance="evince",transient_mode="off",jumpto=true,target="doc",lazy_resize=true}
 defwinprop{class="XDvi", target="doc",lazy_resize=true}
 defwinprop{class="libreoffice*", instance="*", target="doc",lazy_resize=true}
@@ -110,7 +119,8 @@ ignore_max_size=false, ignore_min_size=false, ignore_aspect=false,ignore_resizei
 defwinprop{class="Emacs",instance="emacs",target="dev",lazyresize=true,jumpto=true}
 defwinprop{class="Emacs",instance="emacs",name="Question",float=true}
 defwinprop{class="com-sun-javaws-Main", instance="sun-awt-X11-XFramePeer", target="topcoder"}
-defwinprop{class="jetbrains-idea", instance="*", target="jetbrains", floating=true}
+defwinprop{class="jetbrains-idea", instance="*", target="jetbrains", floating=true,transient_mode = "current"}
+defwinprop{class = "jetbrains-android-studio", instance = "sun-awt-X11-XWindowPeer", transient_mode = "current", }
 defwinprop{class="*", instance="sun-awt-X11-XFramePeer", floating=true}
 -------------------------------------[[ VM ]]--------------------------------------
 defwinprop{class="vmware",instance="Vmware" ,jumpto=true,transient_mode="off", float=true,target="vm"}
@@ -120,7 +130,6 @@ defwinprop{class="QEMU", jumpto=false,target="vm",transient_mode="on",lazy_resiz
 -------------------------------------[[   GIMP  ]]-----------------------------
 defwinprop{class = "Gimp",acrobatic = true}
 -- defwinprop{class="Gimp",instance="gimp",jumpto=true,role="gimp-image-window",transient_mode="off",target="g-win"}
---
 -- defwinprop{class="Gimp",instance="gimp",jumpto=true,role="gimp-file-open",transient_mode="off",target="g-win",float=true}
 -- defwinprop{class="Gimp",instance="gimp",jumpto=true,role="gimp-file-save",transient_mode="off",target="g-win",float=true}
 -- defwinprop{class="Gimp",instance="gimp",jumpto=true,role="gimp-message-dialog",transient_mode="off",target="g-win",float=true}
@@ -154,6 +163,7 @@ defwinprop{class="rdesktop",instance="rdesktop",transient_mode="off",jumpto=true
 defwinprop{class="Ktorrent",winlist_ignore=true,transient_mode="off",instance="ktorrent",target="torrent",lazy_resize=true}
 defwinprop{class="URxvt", instance="mpd-pad",winlist_ignore=true,transient_mode="off",target="ncmpcpp",lazy_resize=true}
 defwinprop{class="URxvt", instance="mpd-pad2",winlist_ignore=true,transient_mode="off",target="ncmpcpp",lazy_resize=true}
+defwinprop { role = "mpd", target = "ncmpcpp" }
 defwinprop{class="Tilda", instance="tilda",winlist_ignore=true,transient_mode="off",lazy_resize=true,float=true}
 defwinprop{class="Guake", instance="guake",winlist_ignore=true,transient_mode="off",lazy_resize=true,float=true}
 defwinprop{class="Pavucontrol", instance="pavucontrol",winlist_ignore=true,transient_mode="off",target="float",lazy_resize=true}
@@ -167,9 +177,29 @@ defwinprop{instance="gcolor2", winlist_ignore=true,transient_mode="off",lazy_res
 defwinprop{instance="gpick", winlist_ignore=true,transient_mode="off",lazy_resize=true,target=float}
 defwinprop{class="Vuze", winlist_ignore=true,transient_mode="off",target="torrent",lazy_resize=true}
 defwinprop{class="Anamnesis", winlist_ignore=true,transient_mode="off",target="float2",lazy_resize=true,jumpto=true}
-defwinprop{class="Nicotine", instance="nicotine", transient_mode="off", target="nicotine"}
+defwinprop{class="Nicotine", instance="nicotine", transient_mode="off", target="float2"}
+defwinprop{class="Nicotine.py", instance="nicotine.py", transient_mode="off", target="float2"}
 defwinprop{class="*",instance="*", transparent=false,lazy_resize=true}
 -- defwinprop{class="*",instance="*", winlist_ignore=true,transient_mode="off",target="etc",float=true,lazy_resize=true}
+defwinprop { class = "Operapluginwrapper-ia32-linux", instance = "operapluginwrapper-ia32-linux",
+    match = function(prop, cwin, id) return is_fullscreen(cwin); end,
+    switchto = false, flash_fullscreen = true, }
+defwinprop { class = "Firefox", instance = "firefox", name = "Firefox",
+    match = function(prop, cwin, id) return is_fullscreen(cwin); end,
+    switchto = false, flash_fullscreen = true, }
+defwinprop { class = "Prism", instance = "prism",
+    match = function(prop, cwin, id) return is_fullscreen(cwin); end,
+    switchto = false,
+    flash_fullscreen = true, }
+defwinprop { class = "Exe", instance = "exe", name = "exe",
+    match = function(prop, cwin, id) return is_fullscreen(cwin); end,
+    switchto = false, flash_fullscreen = true, }
+defwinprop{class="uim-toolbar-qt4", statusbar="uim"}
+
+ioncore.get_hook('clientwin_do_manage_alt'):add(
+    function(cwin, table)
+    ioncore.write_savefile("windowinfos", cwin:get_ident())
+end)
 -------------------------------------------------------------------------------------
 --[  KEY BINDINGS   ]------------------------------
 ---------------------
@@ -182,15 +212,10 @@ defbindings("WMPlex.toplevel", {
     kpress("Mod1+space",       "nop()"),
     kpress("Mod1+grave",       "mod_query.query_exec(_)"),
     kpress("Mod4+Shift+grave", "mod_query.query_lua(_)"),
-    -- kpress("Mod4+space",       "named_scratchpad(_, 'misc')"),
-    -- kpress("Mod1+G",           "mod_query.query_fuzzy_gotoclient(_)"),
     kpress("Mod1+G",           "mod_menu.menu(_, _sub, 'windowlist')"),
-    -- kpress("Mod1+G",           "mod_query.query_gotoclient(_)"),
-
     kpress("Mod4+G",           "mod_menu.menu(_, _sub, 'workspacelist')"),
 
     kpress("Mod4+Control+G",   "mod_query.query_workspace(_)"),
-    --kpress("Mod1+Control+G", "mod_query.query_gotoclient(_)"),
 
     kpress("Mod4+M",           "mod_query.query_menu(_, _sub, 'ctxmenu', 'Context menu:')"),
     kpress("Mod4+Shift+D", "ioncore.detach(_chld, 'toggle')", "_chld:non-nil"),
@@ -200,7 +225,6 @@ defbindings("WMPlex.toplevel", {
 --------------------
     kpress("Mod4+d",         "console(_)"),
     kpress("Mod4+p",         "named_scratchpad(_, 'float2')"),
-    kpress("Mod4+n",         "named_scratchpad(_, 'nicotine')"),
     kpress("Mod4+F",         "ncmpcpp(_)"),
     
     kpress("Mod4+E",         "named_scratchpad(_, 'im')"),
@@ -214,14 +238,11 @@ defbindings("WMPlex.toplevel", {
     kpress("XF86AudioNext",        "ioncore.exec_on(_, 'mpc next')"),
     kpress("XF86AudioPlay",        "ioncore.exec_on(_, 'mpc toggle')"),
     kpress("XF86AudioStop",        "ioncore.exec_on(_, 'mpc stop')"),
-    kpress("XF86AudioLowerVolume", "ioncore.exec_on(_, 'mpc volume -5')"),
-    kpress("XF86AudioRaiseVolume", "ioncore.exec_on(_, 'mpc volume +5')"),
+    kpress("XF86AudioLowerVolume", "ioncore.exec_on(_, 'mpc volume -1')"),
+    kpress("XF86AudioRaiseVolume", "ioncore.exec_on(_, 'mpc volume +1')"),
     kpress("Mod4+Control+T",       "ioncore.exec_on(_, 'dipser -S')"),
     kpress("Mod4+8",               "ioncore.exec_on(_, 'amixer -q set Master 0% mute')"),
     kpress("Mod4+8+Shift",         "ioncore.exec_on(_, 'amixer -q set Master 100% unmute ')"),
-
-    --kpress("Mod4+Shift+Z",       "ioncore.exec_on(_, 'amixer -c0 sset -q Master 5-')"),
-    --kpress("Mod4+Shift+X",       "ioncore.exec_on(_, 'amixer -c0 sset -q Master 5+')"),
     submap("Mod4+Control+R", {
         kpress("t", "ioncore.exec_on(_, 'xcalib -c')"),
         kpress("i", "ioncore.exec_on(_, 'xcalib -alter -invert')"),
@@ -236,22 +257,19 @@ defbindings("WMPlex.toplevel", {
 --[[ SLEEP ]]-----------------------------------------
 -------------
     kpress("XF86Sleep", "ioncore.exec_on(_, 'sudo pm-suspend')"),
-    -- kpress("Alt_R+l", "ioncore.exec_on(_, '')"),
-    --kpress("XF86Sleep", "ioncore.exec_on(_, 'sudo hibernate-ram')"),
-    --kpress("Mod4+Control+Shift+u", "ioncore.exec_on(_, 'sudo /usr/sbin/hibernate-ram')"),
+    kpress("Mod4+Control+Shift+u", "ioncore.exec_on(_, 'sudo systemctl suspend')"),
 -------------------------------------------------------------------------------------
 --[[  PROGS RUN by APP  ]]-----------------------------
 --------------------------
     kpress("Mod4+1", "app.byinstance('~/bin/wim', 'URxvt', 'wim')"),
     --kpress("Mod1+Control+a", "app.byinstance('~/bin/virt.sh', 'VirtualBox')"),
     kpress("Mod1+Control+a", "app.byinstance('', 'Vmware')"),
-    --kpress("Mod4+b", "app.byclass('', 'MPlayer')"),
-    --kpress("Mod4+b", "app.byclass('', 'mplayer2')"),
+    -- kpress("Mod4+b", "app.byclass('', '.*(mpv|MPlayer|mplayer2).*')"),
     kpress("Mod4+b", "app.byclass('', 'mpv')"),
     kpress("Mod4+x", "app.byinstance('~/bin/urxvt', 'URxvt','MainTerminal')"),
-    -- kpress("Mod4+w", "app.byinstance('dwb', 'Dwb','dwb')"),
     kpress("Mod4+w", "app.byinstance('firefox', 'Firefox','Navigator')"),
-    -- kpress("Mod4+w", "app.byinstance('icecat', 'Firefox','Navigator')"),
+    --kpress("Mod4+w", "app.byinstance('dwb', 'Dwb','dwb')"),
+    --kpress("Mod4+w", "app.byinstance('icecat', 'Firefox','Navigator')"),
     --kpress("Mod4+w", "app.byclass('firefox', 'Iceweasel')"),
     --kpress("Mod4+w", "app.byclass('chromium', 'Chromium')"),
     --kpress("Mod4+w", "app.byclass('chromium-browser', 'Chromium-browser')"),
@@ -269,12 +287,8 @@ defbindings("WMPlex.toplevel", {
 --------------
     kpress("Mod4+Control+Q", "WRegion.rqclose_propagate(_, _sub)"),
     kpress("Mod4+F4",        "ioncore.exec_on(_, 'eject')"),
-    -- kpress("Mod4+Control+P", "ioncore.exec_on(_, 'anamnesis --browser')"),
     kpress("Mod4+U",         "ioncore.exec_on(_, 'udiskie-umount -a')"),
     kpress("Mod4+Shift+F",   "app.byinstance('lowriter', 'VCLSalFrame', 'libreoffice-writer')"),
-    -- kpress("Mod4+Shift+F",   "app.class('libreoffice-writer', '')"),
-    -- kpress("Mod4+F12",       "ioncore.exec_on(_, 'switch-sound-output.sh')"),
-    --kpress("Mod4+F6",        "ioncore.exec_on(_, '~/bin/misc/music')"),
     -- Rename frame --
     kpress("Mod4+c",        "ioncore.exec_on(_, 'sh ~/bin/clip')"),
     submap("Mod1+E",{
@@ -288,6 +302,7 @@ defbindings("WMPlex.toplevel", {
         kpress("q", "ioncore.exec_on(_, '~/bin/mpd_sel.sh')"),
         kpress("i", "ioncore.exec_on(_, '~/bin/pls -output')"),
         kpress("o", "ioncore.exec_on(_, '~/bin/pls -sink')"),
+        kpress("n", "app.byinstance('nicotine.py || nicotine', 'Nicotine.py', 'nicotine.py')"),
         kpress("Shift+o", "ioncore.exec_on(_, '~/bin/pls -vol')"),
         kpress("Control+r", "app.byinstance('cr3', 'Cr3', 'cr3')"),
         kpress("Shift+r", "ranger(_)"),
@@ -296,12 +311,6 @@ defbindings("WMPlex.toplevel", {
         kpress("Shift+k", "ioncore.exec_on(_, '~/bin/scripts/toggle_keynav')"),
     }),
 })
-----------------------------[[ TRANPARENCY  ]]---------------------------------
---  submap("Mod4+J", {
---      kpress("j", "ioncore.exec_on(_, 'transset-df 0.9 ')"),
---      kpress("t", "ioncore.exec_on(_, 'transset-df 1   ')"),
---  }),
-
 -------------------------------------------------------------------------------------
 --[[  MENU  ]]---------------------------------------
 --------------
@@ -346,25 +355,18 @@ defbindings("WScreen", {
         kpress("AnyModifier+L", "WRegion.rqorder(_chld, 'front')","_chld:non-nil"),
         kpress("AnyModifier+Shift+L", "WRegion.rqorder(_chld, 'back')","_chld:non-nil"),
     }),
-    -- kpress("Mod1+Shift+s","WRegion.rqorder(_chld, 'front')","_chld:non-nil"),
-
     kpress("F12", "mod_query.query_menu(_, _sub, 'mainmenu', 'Main menu:')"),
     kpress("Mod4+grave", "ioncore.goto_next(_chld, 'right')", "_chld:non-nil"),
     kpress("Mod1+grave", "mod_query.query_exec(_)"),
-
---  kpress("KP_6", "MusicPD.next()"),
---  kpress("KP_4", "MusicPD.previous()"),
---  kpress("KP_2", "MusicPD.volume_down()"),
---  kpress("KP_8", "MusicPD.volume_up()"),
---  kpress("KP_5", "MusicPD.toggle_play()"),
---  kpress("KP_7", "MusicPD.toggle_repeat()"),
---  kpress("KP_9", "MusicPD.toggle_random()"),
     ---------------------------------------------------------
     kpress("Mod4+H", "_chld:focus_direction('left')", "_chld:non-nil"),
     kpress("Mod4+J", "_chld:focus_direction('down')", "_chld:non-nil"),
     kpress("Mod4+K", "_chld:focus_direction('up')", "_chld:non-nil"),
     kpress("Mod4+L", "_chld:focus_direction('right')", "_chld:non-nil"),
     ---------------------------------------------------------
+    -- Screenshots
+    kpress("Print", "make_screenshot()"),
+    kpress("Mod1+Print", "make_current_window_screenshot()"),
 })
 
 defbindings("WClientWin", {
@@ -374,7 +376,6 @@ defbindings("WClientWin", {
 })
 
 defbindings("WGroupCW", {
-    kpress_wait("Mod4+Return", "WGroup.set_fullscreen(_, 'toggle')"),
     kpress_wait("Mod4+Q", "WGroup.set_fullscreen(_, 'toggle')"),
 })
 defbindings("WMPlex",{
@@ -383,13 +384,11 @@ defbindings("WMPlex",{
 })
 -- Frames for transient windows ignore this bindmap
 defbindings("WFrame", {
-    kpress( "Mod4+S","mod_sp.set_shown(ioncore.lookup_region(_:name(), 'WFrame'), 'unset' )" ),
-    -- kpress( "Mod4+S","mod_sp.set_shown(ioncore.lookup_region(_:name(), 'WGroupWS'), 'unset' )" ),
+    kpress("Mod4+S","mod_sp.set_shown(ioncore.lookup_region(_:name(), 'WFrame'), 'unset' )" ),
     submap("Mod1+E", {
       kpress("H", "WFrame.maximize_horiz(_)"),
       kpress("V", "WFrame.maximize_vert(_)"),
     }),
-    --mpress("Button3", "mod_menu.pmenu(_, _sub, 'ctxmenu')"),
     kpress("Mod4+R", "WFrame.begin_kbresize(_)"),
     mclick("Button1@tab", "WFrame.p_switch_tab(_)"), mclick("Button2@tab", "WFrame.p_switch_tab(_)"),
     mdrag("Button1@border", "WFrame.p_resize(_)"),   mdrag(META.."Button3", "WFrame.p_resize(_)"),
@@ -403,15 +402,8 @@ defbindings("WFrame", {
 defbindings("WFrame.toplevel", {
     kpress("Mod4+A", "mod_query.query_attachclient(_)"),
     kpress("Mod4+backslash", "WFrame.switch_next(_)"),
-    --kpress("Mod4+V", "WFrame.switch_next(_)"),
     kpress("Mod1+A", "ioncore.tagged_attach(_)"),
     submap("Mod1+E", {
-        -- submap_wait("ioncore.tabnum.show(_)"),
-        -- kpress("Q", "WFrame.switch_nth(_, 0)"), kpress("W", "WFrame.switch_nth(_, 1)"),
-        -- kpress("E", "WFrame.switch_nth(_, 2)"), kpress("A", "WFrame.switch_nth(_, 3)"),
-        -- kpress("S", "WFrame.switch_nth(_, 4)"), kpress("D", "WFrame.switch_nth(_, 5)"),
-        -- kpress("Z", "WFrame.switch_nth(_, 6)"), kpress("X", "WFrame.switch_nth(_, 7)"),
-        -- kpress("C", "WFrame.switch_nth(_, 8)"),
         --Maximize the frame horizontally/vertically
         kpress("H", "WFrame.maximize_horiz(_)"),
         kpress("V", "WFrame.maximize_vert(_)"),
@@ -421,12 +413,11 @@ defbindings("WFrame.toplevel", {
 defbindings("WFrame.floating", {
     mpress("Button1@tab", "WRegion.rqorder(_, 'front')"),
     mpress("Button1@border", "WRegion.rqorder(_, 'front')"),
-    mclick(META.."Button1", "WRegion.rqorder(_, 'front')"),
-    mclick(META.."Button3", "WRegion.rqorder(_, 'back')"),
+    mclick("Mod1+Button1", "WRegion.rqorder(_, 'front')"),
+    mclick("Mod1+Button3", "WRegion.rqorder(_, 'back')"),
     kpress("Mod1+L", "WRegion.rqorder(_, 'back')" ),
     kpress("Mod1+U", "WRegion.rqorder(_, 'front')" ),
     mdrag("Button1@tab", "WFrame.p_move(_)"),
-    --mdblclick("Button1@tab", "WFrame.set_shaded(_, 'toggle')"),
     ---------------------------------------------------------
     kpress("Mod1+Control+H", "_:push_direction('left')"),
     kpress("Mod1+Control+J", "_:push_direction('down')"),
@@ -469,7 +460,6 @@ defbindings("WMoveresMode", {
 ---------------------------  [[ Tiling  ]]  ------------------------------------
 defbindings("WTiling", {
     --Split current frame vertically
-    --kpress(META.."S", "WTiling.split_at(_, _sub,'bottom', true)"),
     kpress("Mod4+Down", "ioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
     kpress("Mod4+Up",   "ioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
     kpress("Mod4+Left", "ioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
@@ -478,8 +468,6 @@ defbindings("WTiling", {
     kpress("Mod4+k",    "ioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
     kpress("Mod4+h",    "ioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
     kpress("Mod4+l",    "ioncore.goto_next(_sub, 'right', {no_ascend=_})"),
-    --kpress("Mod1+Tab",  "ioncore.goto_next(_sub, 'right')"),
-    --submap("Mod4+s",{
     kpress("Mod4+Control+H", "WTiling.split_at(_, _sub, 'right', true)"),
     kpress("Mod4+Control+L", "WTiling.split_at(_, _sub, 'left', true)"),
     kpress("Mod4+Control+K", "WTiling.split_at(_, _sub, 'bottom', true)"),
@@ -496,7 +484,7 @@ defbindings("WTiling", {
 })
 -- Frame bindings
 defbindings("WFrame.floating", {
-    submap(META.."E", { kpress("B", "mod_tiling.mkbottom(_)"), }),
+    submap("Mod1+E", { kpress("B", "mod_tiling.mkbottom(_)"), }),
 })
 
 
@@ -637,28 +625,7 @@ function move_scratch(x, y, w, h)
 end
 
 -- ioncore.get_hook("ioncore_post_layout_setup_hook"):add(resize_scratch)
---
--- submenu("Float split", {
---     menuentry("At left",
---               "WTiling.set_floating_at(_, _sub, 'toggle', 'left')"),
---     menuentry("At right",
---               "WTiling.set_floating_at(_, _sub, 'toggle', 'right')"),
---     menuentry("Above",
---               "WTiling.set_floating_at(_, _sub, 'toggle', 'up')"),
---     menuentry("Below",
---               "WTiling.set_floating_at(_, _sub, 'toggle', 'down')"),
--- }),
---
--- defctxmenu("WFrame.floating", "Floating frame", {
---     append=true,
---     menuentry("New tiling", "mod_tiling.mkbottom(_)"),
--- })
-
-
 -- ......................................................................................
--- dopath("mod_ionflux")
--- dopath("mpd")
---
 -- function get_hostname()
 --    local out = io.popen("hostname")
 --    return out:read()
