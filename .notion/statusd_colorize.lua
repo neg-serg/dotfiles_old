1f62850b72992077a1353fc412768c0e082c2e31
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-01-12 21:36:37 +0300

    delete statusd_colorize
    Former-commit-id: 83fd92fc4c08c166fb7583bc5702a20d46528464 [formerly 4d8b5b2a00a9e1222621f6e69b591d9dcf96abd5] [formerly 51b1c34f82321fff6e6fa256dc5bf470b7ce77bf [formerly 82c78a3c3afacb2f23e77639ae659928976b1422]]
    Former-commit-id: 3adcccef49fbe0c68909176785640fec2fa0dfa4 [formerly ac555f298f381438f1f54c44ecb9086dfff7b3fa]
    Former-commit-id: ba2d3b8e3027aaae391c81e087fefb8cdc6f8d4b

--- .notion/statusd_colorize.lua
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------------------------------
--- this is ugly hack to colorize some widgets
---
-
-statusd.inform("colorize", "")
-
-statusd.inform("foo", "yellow")
-statusd.inform("]", "yellow")
-
