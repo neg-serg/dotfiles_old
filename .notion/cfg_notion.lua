d6983705e31c1fbbfe3d80a319838ec8e9e79cd5
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-01-13 17:11:06 +0300

    try to use two dzen2 without synchronization
    Former-commit-id: 2c82235beecacbc3a7a7e54c0fb19e568fe78cbe [formerly d2f2723b71d09edae3bc3efe14a72f4d40e3e8f5] [formerly be5db215594a9ee84dd90c79bfc1d06758520e7b [formerly 3bb42d7fa1949aa59618020644a3c5ecbed8c375]]
    Former-commit-id: 7dec557533bbbacf498f599fc0078070cf3b14e2 [formerly 1b7395d77263028b2593ed15aaae908dace22a55]
    Former-commit-id: fd2e76e6c1a148851d192ac0c75304bf3b3b7190

--- .notion/cfg_notion.lua
+++ .notion/cfg_notion.lua
@@ -1,8 +1,19 @@
  --_:attach_new({type="WFloatWS", layer=2}):goto()
 --------------------------------[[ CONSTS ]]----------------------------------------
 META="Mod1+"
---------------------------------[[ IONCORE ]]---------------------------------------
 
+function dzen_delete()
+    os.execute("pkill dzen2")
+end
+
+hook_deinit = ioncore.get_hook("ioncore_deinit_hook")
+local hook_deinit = ioncore.get_hook("ioncore_deinit_hook")
+if hook_deinit then
+    hook_deinit:add(dzen_delete)
+end
+dzen_pipe = io.popen("dzen2 -dock -bg '#000000' -h 22 -tw 0 -x 0 -ta l -w 1000 -p -fn 'PragmataPro:style=bold:size=12' ", "w")
+dzen_pipe:setvbuf("line")
+--------------------------------[[ IONCORE ]]---------------------------------------
 ioncore.set{
     dblclick_delay=250,
     ioncore.defshortening("(.*)(<[0-9]+>)", "$1$2$|$1$<...$2"),
@@ -46,11 +57,11 @@ dopath("cfg_xrandr")
 dopath("mod_notionflux")
 dopath("vim_bindings")
 dopath("goto-by-tag")
-dopath("mod_statusbar")
 dopath("nest_ws")
 dopath("screenshot")
--- dopath("mod_dock")
--- dopath("cfg_dock")
+dopath("lua_repl")
+dopath("mod_dock")
+dopath("cfg_dzen")
 -------------------------------------[[ KLUDGES ]]----------------------------------
 defwinprop{lazy_resize=true}
 -------------------------------------[[ TERM ]]-------------------------------------
@@ -200,6 +211,8 @@ ioncore.get_hook('clientwin_do_manage_alt'):add(
     function(cwin, table)
     ioncore.write_savefile("windowinfos", cwin:get_ident())
 end)
+
+-- dzen_pipe = io.popen("dzen2 -dock -bg '#000000' -tw 0 -x 0 -ta l -w 1910 -p -fn '-hell-monobook-bold-r-normal--16-160-72-72-m-80-iso10646-1' ", "w")
 -------------------------------------------------------------------------------------
 --[  KEY BINDINGS   ]------------------------------
 ---------------------
@@ -214,6 +227,8 @@ defbindings("WMPlex.toplevel", {
     kpress("Mod4+Shift+grave", "mod_query.query_lua(_)"),
     kpress("Mod1+G",           "mod_menu.menu(_, _sub, 'windowlist')"),
     kpress("Mod4+G",           "mod_menu.menu(_, _sub, 'workspacelist')"),
+
+    kpress("Mod4+F2", "repl(_)"),
 
     kpress("Mod4+Control+G",   "mod_query.query_workspace(_)"),
 
@@ -365,7 +380,7 @@ defbindings("WScreen", {
     kpress("Mod4+L", "_chld:focus_direction('right')", "_chld:non-nil"),
     ---------------------------------------------------------
     -- Screenshots
-    kpress("Print", "make_screenshot()"),
+    kpress("Print", "make_root_screenshot()"),
     kpress("Mod1+Print", "make_current_window_screenshot()"),
 })
 
@@ -624,156 +639,37 @@ function move_scratch(x, y, w, h)
    ioncore.lookup_region("*scratchpad*"):rqgeom({x=x, y=y, w=w, h=h})
 end
 
--- ioncore.get_hook("ioncore_post_layout_setup_hook"):add(resize_scratch)
 -- ......................................................................................
--- function get_hostname()
---    local out = io.popen("hostname")
---    return out:read()
--- end
---
--- defbindings("WMPlex", {
--- 	       kpress("Mod4+r", "ioncore.exec_on(_, 'urxvtcd -e screen -dRR main')"),
---                kpress("Mod4+Mod1+r", 
---                       "ioncore.exec_on(_, 'urxvtcd -e ssh -tX slack LANG=ru_RU.UTF-8 screen -dRR')"),
--- 	       kpress("Mod4+e", "ioncore.exec_on(_, 'emacs')"),
--- 	       kpress("Mod4+f", "ioncore.exec_on(_, 'firefox')"),
--- 	       kpress("Mod4+o", "ioncore.exec_on(_, 'opera')"),
---                kpress("Mod4+c", "ioncore.exec_on(_, 'chrome')"),
--- 	       kpress("Mod4+d", "ioncore.exec_on(_, 'okular')"),
---
--- 	       kpress("Mod4+z", "dict_lookup(_)"),
---
---                kpress("Print", "ioncore.exec('sleep 0.1;  xset dpms force off')"),
---                kpress("Scroll_Lock", "toggle_display(_)"),
---                kpress("Control+F4", "ioncore.exec('susp')"),
---                kpress("Mod4+F2", "repl(_)"),
---                kpress("Mod4+F5", "start_all(_)"),
---                -- kpress("XF86WWW", "show_weather()")
--- 	    })
---
--- function start_all(ws)
---    ioncore.exec_on(ws, 'emacs')
---    ioncore.exec_on(ws, 'browser')
---    ioncore.exec_on(ws, 'gnus')
---    ioncore.exec_on(ws, 'urxvtcd -e screen -dRR main')
--- end
---
--- function dict_lookup (ws)
---    ioncore.request_selection(
---       function (str)
--- 	 local stream = io.popen("gosh ~/c/bin/idict \""..str.."\"")
--- 	 local string = ""
---
--- 	 for line in stream:lines() do
--- 	    string = string..line.."\n"
--- 	 end
--- 	 stream:close()
---
--- 	 mod_query.message(ws, string)
---       end)
--- end
---
--- function mod_query.query_lua(mplex)
---     local env=mod_query.create_run_env(mplex)
---     
---     local function complete(cp, code)
---         cp:set_completions(mod_query.do_complete_lua(env, code))
---     end
---     
---     local function handler(mplex, code)
---         return mod_query.do_handle_lua(mplex, env, code)
---     end
---     
---     mod_query.query(mplex, TR("Lua code:"), nil, handler, complete, "lua")
---  end
---
--- function do_handle_lua(mplex, env, code)
---     local print_res
---     local function collect_print(...)
---         local tmp=""
---         local arg={...}
---         local l=#arg
---         for i=1,l do
---             tmp=tmp..tostring(arg[i])..(i==l and "\n" or "\t")
---         end
---         print_res=(print_res and print_res..tmp or tmp)
---     end
---
---     local f, err=loadstring(code)
---     if not f then
---         mod_query.warn(mplex, err)
---         return
---     end
---     
---     env.print=collect_print
---     setfenv(f, env)
---
---     local result
---     err=collect_errors(function () result = f() end)
---
---     if err then
---         mod_query.warn(mplex, err)
---     elseif print_res then
---        mod_query.message(mplex, print_res .. "\nResult: "..tostring(result))
---     end
--- end
---
--- function repl(ws)
---    local env=mod_query.create_run_env(ws)
---
---    local function complete(cp, code)
---         cp:set_completions(mod_query.do_complete_lua(env, code))
---      end
---    
---    local function handler(mplex, code)
---       return do_handle_lua(mplex, env, code)
---    end
---
---    mod_query.query(ws, "LUA: ", nil, handler, complete, "lua")
--- end
---
---
--- function move_scratch(x, y, w, h)
---    ioncore.lookup_region("*scratchpad*"):rqgeom({x=x, y=y, w=w, h=h})
--- end
---
--- function get_display()
---    local out = io.popen("xrandr")
---    local line = out:read()
---    
---    while line do
---       local b, e, id = string.find(line, "^(%w.+) connected %d+x%d+")
---
---       if id then
---          return id
---       end
---
---       line = out:read()
---    end
--- end
---
--- function toggle_display(ws)
---    local status
---    local offset
---
---    if  get_display() == "HDMI-0" then
---       status = os.execute("xrandr --output HDMI-0 --off --output DVI-I-1 --auto && sleep 0.3 && xrandr --output DVI-D-0 --auto --output DVI-D-0 --left-of DVI-I-1 && xset dpms 600 600 600")
---    else
---       status = os.execute("xrandr --output DVI-I-1 --off && sleep 0.3 && xrandr --output DVI-D-0 --off --output HDMI-0 --auto && xset s off s noblank dpms 0 0 0 -dpms")
---    end
---    
---    if status == 0 then
---       move_scratch(0,0,20,30)
---       ioncore.restart()
---    end
--- end
---
--- function resize_scratch()
---    if get_display() == "HDMI-0" then
---       move_scratch(300, 160, 1361, 744)
---    else
---       move_scratch(2200, 260, 1361, 744)
---    end
--- end
---
--- ioncore.get_hook("ioncore_post_layout_setup_hook"):add(resize_scratch)
+function get_hostname()
+   local out = io.popen("hostname")
+   return out:read()
+end
+
+function move_scratch(x, y, w, h)
+   ioncore.lookup_region("*scratchpad*"):rqgeom({x=x, y=y, w=w, h=h})
+end
+
+function get_display()
+   local out = io.popen("xrandr")
+   local line = out:read()
+
+   while line do
+      local b, e, id = string.find(line, "^(%w.+) connected %d+x%d+")
+
+      if id then
+         return id
+      end
+
+      line = out:read()
+   end
+end
+
+function resize_scratch()
+   if get_display() == "HDMI-0" then
+      move_scratch(300, 160, 1361, 744)
+   else
+      move_scratch(2200, 260, 1361, 744)
+   end
+end
+
+ioncore.get_hook("ioncore_post_layout_setup_hook"):add(resize_scratch)
