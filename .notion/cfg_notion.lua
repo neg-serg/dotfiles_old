ce6bfb0f0122873a436a0f8aa75ddff3493bdca9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-01-14 22:55:58 +0300

    move sp_app to separate file
    Former-commit-id: a97e73286bda3e29ec118f648c5a1174ce4a3946 [formerly 5c5a83c86653217d6800a3c773789f599523c996] [formerly fe63dc1195bb6000a81e0167a792cf2e6bab361c [formerly 74aaf8ab6a59cfaa3081ac73e22a08938e55f9e5]]
    Former-commit-id: e82a421997956ba45d48dd5d9cce739ee8054954 [formerly 72bd8b2e4383f785f6b513ed97aee3b36a648254]
    Former-commit-id: 7305eebde450a60782f79fd149e27e25d48c9d78

--- .notion/cfg_notion.lua
+++ .notion/cfg_notion.lua
@@ -11,7 +11,8 @@ local hook_deinit = ioncore.get_hook("ioncore_deinit_hook")
 if hook_deinit then
     hook_deinit:add(dzen_delete)
 end
-dzen_pipe = io.popen("dzen2 -dock -bg '#000000' -h 22 -tw 0 -x 0 -ta l -w 1000 -p -fn 'PragmataPro:style=bold:size=12' ", "w")
+-- dzen_pipe = io.popen("dzen2 -dock -bg '#000000' -h 22 -tw 0 -x 0 -ta l -w 1000 -p -fn 'PragmataPro:style=bold:size=12' ", "w")
+dzen_pipe = io.popen("dzen2 -dock -bg '#000000' -h 19 -tw 0 -x 0 -ta l -w 1000 -p -fn 'PragmataPro:style=bold:size=12' ", "w")
 dzen_pipe:setvbuf("line")
 --------------------------------[[ IONCORE ]]---------------------------------------
 ioncore.set{
@@ -36,8 +37,8 @@ ioncore.set{
     autoraise=true,
 	edge_resistance=200,	-- The default is so unrestrictive that I wasn't even aware of it!
 }
-
 --------------------------------[[ DOPATH ]]-----------------------------------------
+
 dopath("cfg_layouts.lua")
 dopath("app")
 dopath("mod_sp")
@@ -513,99 +514,6 @@ defmenu("menuattach", {
     menuentry("Float WS", "_:attach_new({type=\"WGroupWS\", switchto=true})"),
     menuentry("Tiling WS", "_:attach_new({type=\"WGroupWS\", switchto=true}):attach_new({type=\"WTiling\", sizepolicy=\"full\", bottom=true})"),
 })
-
-sp_app = {}
-
-local sp_app_private = {}
-
-local sp_apps = {}
-local sp_launching = {}
-local sp_launched = {}
-
-local function add_to_apps (xid, spname, table)
-    sp_apps[xid] = spname
-    sp_launching[table] = nil
-    sp_launched[spname] = true
-end
-
-function sp_app_private.mapped_hook (clientwin)
-    local class = clientwin:get_ident().class
-    local instance = clientwin:get_ident().instance
-    local name = clientwin:name()
-    dbg.echo("Class: "..class.."\tInstance: "..instance..
-        "\t\tTitle: "..name.."\tXid: "..clientwin:xid())
-    dbg.echo(sp_launching)
-    dbg.echo(sp_launched)
-    for k, v in pairs(sp_launching) do
-        dbg.echo("c: "..k.class.." i: "..k.instance)
-        if k.instance then
-            dbg.echo("a")
-            if k.instance == instance and k.class == class then
-            dbg.echo("b")
-                add_to_apps(clientwin:xid(), v, k)
-            end
-        elseif k.class then
-            if k.class == class then
-                add_to_apps(clientwin:xid(), v, k)
-            end
-        elseif k.name == name then
-            add_to_apps(clientwin:xid(), v, k)
-        end
-    end
-end
-
-function sp_app_private.unmapped_hook (xid)
-    dbg.echo("Xid: "..xid)
-    dbg.echo(sp_apps)
-    if sp_apps[xid] then
-        local sp = ioncore.lookup_region(sp_apps[xid], "WFrame")
-        dbg.echo("sp")
-        dbg.echo(sp)
-        dbg.echo(sp_apps[xid])
-            mod_sp.set_shown(sp, "unset")
-        sp_launched[sp_apps[xid]]=nil
-        sp_apps[xid] = nil
-    end
-end
-
-function sp_app_private.init ()
-    ioncore.get_hook("clientwin_mapped_hook"):add(sp_app_private.mapped_hook)
-    ioncore.get_hook("clientwin_unmapped_hook"):add(sp_app_private.unmapped_hook)
-end
-
-function sp_app.deinit ()
-    ioncore.get_hook("clientwin_mapped_hook"):remove(sp_app_private.mapped_hook)
-    ioncore.get_hook("clientwin_unmapped_hook"):remove(sp_app_private.unmapped_hook)
-end
-
-function sp_app.toggle (reg, cmdline, mtable)
-    local spname;
-    local mtype;
-    if mtable.instance then
-        mtype="instance"
-        spname=mtable.instance
-    else
-        mtype="class"
-        spname=mtable.class
-    end
-
-    if reg:current():name() == spname then
-            named_scratchpad(reg, spname, "unset")
-    else
-            named_scratchpad(reg, spname, "set")
-        if not sp_launched[spname] then
-            sp_launching[mtable] = spname
-            if mtype=="class" then
-                    app.byclass(cmdline, mtable.class)
-            elseif mtype=="instance" then
-                app.byinstance(cmdline, mtable.class, mtable.instance)
-            end
-        end
-    end
-end
-
-sp_app_private.init()
-
 
 function ncmpcpp(ws)
     ioncore.exec_on(ws, '~/bin/scripts/ncmpcpp')
