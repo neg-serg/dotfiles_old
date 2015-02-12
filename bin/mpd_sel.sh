113684e5381f072a30188125089b9fa262d0cf44
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-02-12 23:01:27 +0300

    use rofi with mpd-sel
    Former-commit-id: 5e373d5f20a7a17649bd3d80e7de35137a446951 [formerly b6824c5daaea250bd64a6dc5cee7d09e5fa7aaab] [formerly eb4dfae1dc57966b8b815940a398937d2bc83343 [formerly a8e4e231fefaae02de03568713c5e20a354a2585]]
    Former-commit-id: d4a587f3e4942f1259ccbf9bb3ebee2fa314bf93 [formerly fc450d5f7d68004dce6a0bfbc4f474f5d7db5ddc]
    Former-commit-id: e08240dc8567fa2acd2f4c302112dfac8427b99f

--- bin/mpd_sel.sh
+++ bin/mpd_sel.sh
@@ -1,13 +1,11 @@
 #!/bin/zsh
 
-DMENU="${HOME}/bin/dmenu_select"
-
-ARTIST=$( mpc ls | $DMENU )
-
+ROFI='rofi -font "Pragmata Pro for Powerline bold 12" -yoffset -22 -location 6 -width 1850 -lines 10 -dmenu -p "[>>]"'
+ARTIST=$( mpc ls | eval $ROFI)
 if [[ $ARTIST == "" ]]; then
   exit 1
 else
-  ALBUM=$( echo -e "Play All\n$( mpc ls "$ARTIST" )" | $DMENU )
+  ALBUM=$( echo -e "Play All\n$( mpc ls "$ARTIST" )" | eval $ROFI)
 
   if [[ $ALBUM == "" ]]; then
     exit 1
