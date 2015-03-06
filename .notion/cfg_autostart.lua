3027f6ae12958d4cb4a66889dbed4f21df0b784d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-06 20:42:17 +0300

    disable levenshtein-sort for rofi in autostart

--- .notion/cfg_autostart.lua
+++ .notion/cfg_autostart.lua
@@ -2,8 +2,9 @@ function start_mpdstat()
     ioncore.exec('lua ~/.notion/mpd_dzen.lua')
 end
 function start_rofi()
-    ioncore.exec('rofi -levenshtein-sort -font \"Pragmata Pro for Powerline 12 bold\" -lines 10 -width 1850 -yoffset -22 -key mod1-g -location 6')
-    ioncore.exec('rofi -levenshtein-sort -font \"Pragmata Pro for Powerline 12 bold\" -lines 10 -width 1850 -yoffset -22 -rkey mod1-grave -location 6')
+    local rofi_font = '-font "Pragmata Pro for Powerline bold 12 "'
+    ioncore.exec('rofi ' .. rofi_font ..  ' -lines 10 -width 1850 -yoffset -22 -key mod1-g -location 6')
+    ioncore.exec('rofi ' .. rofi_font ..  ' -lines 10 -width 1850 -yoffset -22 -rkey mod1-grave -location 6')
 end
 start_mpdstat()
 start_rofi()
