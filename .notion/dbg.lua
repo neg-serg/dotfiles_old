4cb0c002e28875d70eb2938db9a40a9747c61bc6
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-08 20:44:18 +0300

    move lua_repl to dbg.lua
    Former-commit-id: f96eaaaf5788fabdcfca041f3d6ffc7f641fc4ed [formerly 62813d711ab284fbf5d8ecb9a640a43c725df90b] [formerly d45474f932cb2940c9f9a0656557691102f10e9a [formerly 5395c282e7fd9d3fc0a667ec9c0ba0dd8eec9d4a]]
    Former-commit-id: ef5ccd9397d832137553b816fe875d73dde332f2 [formerly 698206859c80213d41d2f7937943d500b9bea317]
    Former-commit-id: c74cbb9545f64aa3e505298988a5422540f1f221

--- .notion/dbg.lua
+++ .notion/dbg.lua
@@ -168,4 +168,63 @@ end
 -- close do
 end
 
--- ChillCode
+function mod_query.query_lua(mplex)
+    local env=mod_query.create_run_env(mplex)
+
+    local function complete(cp, code)
+        cp:set_completions(mod_query.do_complete_lua(env, code))
+    end
+
+    local function handler(mplex, code)
+        return mod_query.do_handle_lua(mplex, env, code)
+    end
+
+    mod_query.query(mplex, TR("Lua code:"), nil, handler, complete, "lua")
+ end
+
+function do_handle_lua(mplex, env, code)
+    local print_res
+    local function collect_print(...)
+        local tmp=""
+        local arg={...}
+        local l=#arg
+        for i=1,l do
+            tmp=tmp..tostring(arg[i])..(i==l and "\n" or "\t")
+        end
+        print_res=(print_res and print_res..tmp or tmp)
+    end
+
+    local f, err=loadstring(code)
+    if not f then
+        mod_query.warn(mplex, err)
+        return
+    end
+
+    env.print=collect_print
+    setfenv(f, env)
+
+    local result
+    err=collect_errors(function () result = f() end)
+
+    if err then
+        mod_query.warn(mplex, err)
+    elseif print_res then
+       mod_query.message(mplex, print_res .. "\nResult: "..tostring(result))
+    end
+end
+
+function repl(ws)
+   local env=mod_query.create_run_env(ws)
+
+   local function complete(cp, code)
+        cp:set_completions(mod_query.do_complete_lua(env, code))
+     end
+
+   local function handler(mplex, code)
+      return do_handle_lua(mplex, env, code)
+   end
+
+mod_query.query(ws, "[Lua] >> ", nil, handler, complete, "lua")
+end
+
+
