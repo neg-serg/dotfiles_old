ecf1d3794be9f20f3069125c54c6585c39c4d6f7
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-08 20:47:57 +0300

    clean main notion-conf a bit
    Former-commit-id: 2d9c5579b5e774de65f1cdc38d48a3957a8e242e [formerly dc8cdd9626447a93dc0b6baa78ae9cafba7a12a4] [formerly 104541c3039f43c21cff08e0a8baa1abbfa524a8 [formerly 95dfebe0f9ab36a0efaddaae2f03176badc8d52a]]
    Former-commit-id: 2f15cd1ca396ccbd6bea6f26f20a2fccf0c23722 [formerly db7ed79af070cc5fae098d5cdfa174441167df08]
    Former-commit-id: 53eb0d409bd173953f095cfe570204e48ff828db

--- .notion/cfg_notion.lua
+++ .notion/cfg_notion.lua
@@ -63,6 +63,7 @@ dopath("cfg_dzen")
 dopath("rofi_goto")
 dopath("cfg_autostart")
 dopath("mod_xkbevents")
+dopath("helpers")
 -------------------------------------[[ KLUDGES ]]----------------------------------
 defwinprop{lazy_resize=true}
 -------------------------------------[[ TERM ]]-------------------------------------
@@ -471,70 +472,3 @@ defmenu("menuattach", {
     menuentry("Float WS", "_:attach_new({type=\"WGroupWS\", switchto=true})"),
     menuentry("Tiling WS", "_:attach_new({type=\"WGroupWS\", switchto=true}):attach_new({type=\"WTiling\", sizepolicy=\"full\", bottom=true})"),
 })
-
-function ncmpcpp(ws)
-    ioncore.exec_on(ws, '~/bin/scripts/ncmpcpp')
-    named_scratchpad(ws, 'ncmpcpp')
-end
-
-function console(ws)
-    ioncore.exec_on(ws, '~/bin/scripts/console')
-    named_scratchpad(ws, 'console')
-end
-
-function ranger(ws)
-    ioncore.exec_on(ws, '~/bin/scripts/ranger')
-    named_scratchpad(ws, 'console')
-end
-
-function gdb(ws)
-    ioncore.exec_on(ws, '~/bin/scripts/gdb')
-    named_scratchpad(ws, 'gdb')
-end
-
-function radare2(ws)
-    ioncore.exec_on(ws, '~/bin/scripts/radare')
-    named_scratchpad(ws, 'radare2')
-end
-
-function nop()
-end
-
-function move_scratch(x, y, w, h)
-   ioncore.lookup_region("*scratchpad*"):rqgeom({x=x, y=y, w=w, h=h})
-end
-
--- ......................................................................................
-function get_hostname()
-   local out = io.popen("hostname")
-   return out:read()
-end
-
-function move_scratch(x, y, w, h)
-   ioncore.lookup_region("*scratchpad*"):rqgeom({x=x, y=y, w=w, h=h})
-end
-
-function get_display()
-   local out = io.popen("xrandr")
-   local line = out:read()
-
-   while line do
-      local b, e, id = string.find(line, "^(%w.+) connected %d+x%d+")
-
-      if id then
-         return id
-      end
-
-      line = out:read()
-   end
-end
-
-function resize_scratch()
-   if get_display() == "HDMI-0" then
-      move_scratch(300, 160, 1361, 744)
-   else
-      move_scratch(2200, 260, 1361, 744)
-   end
-end
-
-ioncore.get_hook("ioncore_post_layout_setup_hook"):add(resize_scratch)
