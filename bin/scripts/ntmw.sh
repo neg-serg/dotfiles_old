ba84edb9ff1c9fa905a9b3c367c5b6da4b967768
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-20 23:05:28 +0300

    delete nottomuch wrapper

--- bin/scripts/ntmw.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# nottoomuch-addresses.sh wrapper
-
-$HOME/.mutt/nottoomuch-addresses.sh "$1" \
-    |sed -s 's/\(.*\) \(<.*\)/\2\   \1/'\
-    |sed -s 's/\"//g'\
-    |sed -s '/buzz+.*/d'\
