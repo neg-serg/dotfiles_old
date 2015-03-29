1c70fd6c789c9c7dc46d9d99f0f0d10335b847be
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-03-29 16:24:07 +0300

    new rofi

--- .notion/cfg_autostart.lua
+++ .notion/cfg_autostart.lua
@@ -3,8 +3,7 @@ function start_mpdstat()
 end
 function start_rofi()
     local rofi_font = '-font "Pragmata Pro for Powerline bold 12 "'
-    ioncore.exec('rofi ' .. rofi_font ..  ' -lines 10 -width 1850 -yoffset -22 -key mod1-g -location 6')
-    ioncore.exec('rofi ' .. rofi_font ..  ' -lines 10 -width 1850 -yoffset -22 -rkey mod1-grave -location 6')
+    ioncore.exec('/usr/bin/rofi -auto-select ' .. rofi_font ..  ' -lines 10 -width 1850 -yoffset -22 -key-window mod1-g -key-run mod1-grave -location 6')
 end
 start_mpdstat()
 start_rofi()
