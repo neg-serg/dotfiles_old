c8a368bdf0e9799716c691e16696bbb2624667f2
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-04-01 23:43:18 +0300

    I have missed a mod_query call

--- .notion/rofi_goto.lua
+++ .notion/rofi_goto.lua
@@ -49,17 +49,17 @@ function rofi_attach_win(frame, str)
     tbl = {}
     local rofi_font = '-font "Pragmata Pro for Powerline bold 12 "'
     local rofi_width = 1850
-    rofi_cmd='rofi -auto-select -dmenu -opacity "90" -lines "10" -yoffset -22 '.. rofi_font .. ' -fg' ..
+    rofi_cmd='rofi -dmenu -opacity "90" -lines "10" -yoffset -22 '.. rofi_font .. ' -fg' ..
     '"#666666" -bg "#000" -hlfg "#aaaaaa" -hlbg "#194558" -bc "#202020" -bw 2 -location 6' ..
     ' -padding 2 -width ' .. rofi_width
     prefix = ' -p "[attach] >> "'
     rofi_pipe = io.popen(rofi_cmd .. prefix .. "> /tmp/attach_win", "w")
     rofi_pipe:setvbuf("line")
     tbl = windowlist_for_rofi()
-    function PrintSomething(i)
+    function print_to_rofi(i)
         rofi_pipe:write(tbl[i].name,'\n')
     end
-    table.foreachi( tbl, PrintSomething  )
+    table.foreachi( tbl, print_to_rofi  )
     rofi_pipe:close() 
     fp = io.open("/tmp/attach_win")
     str = fp:read("*l")
@@ -67,7 +67,6 @@ function rofi_attach_win(frame, str)
     
     local cwin=ioncore.lookup_clientwin(str)
     if not cwin then
-        -- mod_query.warn(frame, TR("Could not find client window %s.", str))
         return
     end
     local reg=cwin:groupleader_of()
@@ -76,10 +75,10 @@ function rofi_attach_win(frame, str)
         frame:attach(reg, { switchto = true })
     end
     if frame:rootwin_of()~=reg:rootwin_of() then
-        -- mod_query.warn(frame, TR("Cannot attach: different root windows."))
+        
     elseif reg:manager()==frame then
         reg:goto_focus()
     else
-        -- mod_query.call_warn(frame, attach)
+        ioncore.defer(function () attach() end)
     end
 end
