347202471cfae3f701401135be1b0d5bb5d53f6f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-04-11 11:37:42 +0300

    add ws goto_or_create
    Former-commit-id: 5bf886c7b19119249323cb9dc5f676dfeea98511 [formerly 918e5743b395680d6e7e9a56c9e3da4026897d5b] [formerly 1ff65513b8859355212cf53109f0349f18588f95 [formerly 3b27433fc5bb3a5577d2b07a2dff8ca17acab20c]]
    Former-commit-id: 0756c3978164b925e375d8e486ee93240a07ef4a [formerly 535282a2fe55398862e5a8e4667d9bd836c2b38c]
    Former-commit-id: 45620056662e94f7b2f08394c44b71d2a5ba060c

--- .notion/rofi_goto.lua
+++ .notion/rofi_goto.lua
@@ -2,6 +2,10 @@ function complete_clientwin()
     return complete_name(ioncore.clientwin_i)
 end
 
+function complete_ws()
+    return workspacelist(ioncore.region_i)
+end
+
 local function mk_completion_add(entries)
     return function(s) 
                if s then
@@ -10,9 +14,41 @@ local function mk_completion_add(entries)
            end
 end
 
+local function sort(entries)
+    table.sort(entries, function(a, b) return a.name < b.name end)
+    return entries
+end
+
+function workspacelist(iter)
+    local entries={}
+    local ws_add=mk_completion_add(entries)
+    
+    iter(function(obj)
+             if obj_is(obj, "WGroupWS") then
+                ws_add(obj:name())
+             end
+             return true
+         end)
+    
+    return entries
+end
+
+
+function workspace_handler(reg,name)
+    local ws=ioncore.lookup_region(name, "WGroupWS")
+    if ws then
+        ws:goto_focus()
+    else
+        local tmpl={
+            name=(name~="" and name),
+            switchto=true
+        } 
+        layout = "full"
+        ioncore.create_ws(reg:screen_of(),tmpl,layout)
+    end
+end
+
 function complete_name(iter)
-    local sub_ok_first=true
-    local casei_ok=true
     local entries={}
     local tst_add=mk_completion_add(entries)
     
@@ -47,6 +83,26 @@ function rofi_goto_win()
     if win then
         ioncore.defer(function () win:goto_focus() end)
     end
+end
+
+function rofi_goto_or_create_ws(reg)
+    local tbl = {}
+    local rofi_font = '-font "Pragmata Pro for Powerline bold 12 "'
+    local rofi_width = 1850
+    local goto_win_file = "/tmp/goto_ws"
+    local rofi_cmd='rofi -auto-select -dmenu -opacity "90" -lines "10" -yoffset -22 '.. rofi_font .. ' -fg' ..
+    '"#666666" -bg "#000" -hlfg "#aaaaaa" -hlbg "#194558" -bc "#202020" -bw 2 -location 6' ..
+    ' -padding 2 -width ' .. rofi_width
+    local rofi_prefix = ' -p "[ws] >> "'
+    rofi_pipe = io.popen(rofi_cmd .. rofi_prefix .. "> " .. goto_win_file, "w")
+    rofi_pipe:setvbuf("line")
+    tbl = complete_ws()
+    for i in pairs(tbl) do rofi_pipe:write(tbl[i],'\n') end
+    rofi_pipe:close() 
+    fp = io.open(goto_win_file)
+    name = fp:read("*l")
+    workspace_handler(reg,name)
+    fp:close()
 end
 
 function rofi_attach_win(frame, str)
