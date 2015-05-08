c0a731685fd874fd2510c98ee73bd74ca3d8fa4b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-05-08 23:06:57 +0300

    add real-transparency support
    Former-commit-id: 9c45d54b1788a4fe112944964e2ce9372c32a023 [formerly 96d8063b1ccd28066baae610aa7189fda5c24f6f] [formerly 7ae3cd1179dacb9eaa0ec540d97b7ca94fb1dafc [formerly 43e53a92d8b340918d3f3ca0de7b8a8e3c99cd3e]]
    Former-commit-id: f2f7e956c65b135954c03a98dc1468c2de32df93 [formerly c47dbe4eb4a6883c820fe4e3488b3821e5b75d46]
    Former-commit-id: 3bfd9d729349b7b9956f9498c9862b3753a89267

---
+++ .notion/transparency.lua
@@ -0,0 +1,64 @@
+local transparent_table = {}
+local nontransparent_table = {}
+
+
+local function mk_completion_add(entries)
+    return function(s) 
+               if s then
+                   table.insert(entries, s)
+               end
+           end
+end
+
+function complete_frame()
+    return framelist(ioncore.region_i)
+end
+
+function framelist(iter)
+    transparent_table = {}
+    nontransparent_table = {}
+
+    local entries={}
+    local ws_add=mk_completion_add(entries)
+
+    local name=""
+
+    iter(function(obj)
+             if obj_is(obj, "WFrame") then
+                 if obj:mx_count() == 0 then
+                    table.insert(transparent_table, obj)
+                    ws_add(obj:xid())
+                 else
+                    table.insert(nontransparent_table, obj)
+                 end
+             end
+             return true
+         end)
+    return entries
+end
+
+function maketransparent(reg)
+    local atom_client_opacity = ioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
+    local opacity_level = 3435973836
+
+    local atom_client_opacity = ioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
+    local cur=ioncore.find_manager(ioncore.current(), "WFrame")
+
+    complete_frame()
+
+    for _,reg in ipairs(transparent_table) do
+        notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {opacity_level})
+    end
+    
+    for _,reg in ipairs(nontransparent_table) do
+        notioncore.x_delete_property(reg:xid(), atom_client_opacity)
+    end
+end
+
+local function hookhandler(reg, how)
+    ioncore.defer(function() maketransparent() end)
+end
+ioncore.get_hook("clientwin_mapped_hook"):add(hookhandler)
+ioncore.get_hook("clientwin_unmapped_hook"):add(hookhandler)
+ioncore.get_hook("region_notify_hook"):add(hookhandler)
+
