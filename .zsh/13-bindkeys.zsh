147164654b31e74352ee22b6210ce5fd64f043ec
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-05-23 01:54:38 +0300

    [zsh] correct history search

--- .zsh/13-bindkeys.zsh
+++ .zsh/13-bindkeys.zsh
@@ -79,8 +79,12 @@ bindkey '\C-x\C-e' edit-command-line
 
 bindkey . rationalise-dot
 
-bindkey '\e[A'  up-line-or-search       # cursor up
-bindkey '\e[B'  down-line-or-search     # <ESC>-
+autoload up-line-or-beginning-search
+autoload down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
 
 if zrcautoload insert-files && zle -N insert-files ; then
     #k# Insert files and test globbing
