148ab767a4f4417b29037fce625c4540db190bfd
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-07-02 22:11:31 +0300

    update fzf bindings
    Former-commit-id: 1399c5c297496bbf2a92aba7d1040e3c7b8865b1 [formerly 627ad1f6563df222ac985aed4d102890af2ea0c6] [formerly e61d5c7253d4d07eb5881f953173356685818da6 [formerly 9394ee60c060f6d2f67b329f7c9d1ff3cc3f2826]]
    Former-commit-id: ce2778c04cb9890d90241ade6c61771ced1e42ed [formerly dc62ca2fe363a10add4a57f65f9d5bca9fa98644]
    Former-commit-id: 1918456cea897dde9b095988b62508c651be7e68

--- .zsh/98-fzf.zsh
+++ .zsh/98-fzf.zsh
@@ -1,19 +1,11 @@
-# fe [FUZZY PATTERN] - Open the selected file with the default editor
-#   - Bypass fuzzy finder if there's only one match (--select-1)
-#   - Exit if there's no match (--exit-0)
 fe() {
-  zle -I
-  local file
-  file=$(fzf --query="$1" --select-1 --exit-0)
-  [ -n "$file" ] && ${EDITOR:-vim} "$file"
-}
-
-# fd - cd to selected directory
-fd() {
-  local dir
-  dir=$(find ${1:-*} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
+  local out file key
+  out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && xdg-open "$file" || ${EDITOR:-vim} "$file"
+  fi
 }
 
 # fh - repeat history
@@ -29,6 +21,57 @@ fkill() {
 }
 
 
+# fco - checkout git commit
+fco() {
+  local commits commit
+  commits=$(git log --pretty=oneline --abbrev-commit --reverse) &&
+  commit=$(echo "$commits" | fzf --tac +s +m -e) &&
+  git checkout $(echo "$commit" | sed "s/ .*//")
+}
+
+# fshow - git commit browser (enter for show, ctrl-d for diff, backtick ` toggles sort)
+fshow() {
+  local out shas sha q k
+  while out=$(
+      git log --graph --color=always \
+          --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" "$@" |
+      fzf --ansi --multi --no-sort --reverse --query="$q" \
+          --print-query --expect=ctrl-o,ctrl-d --toggle-sort=\`); do
+    q=$(head -1 <<< "$out")
+    k=$(head -2 <<< "$out" | tail -1)
+    shas=$(sed '1,2d;s/^[^a-z0-9]*//;/^$/d' <<< "$out" | awk '{print $1}')
+    [ -z "$shas" ] && continue
+    if [ "$k" = 'ctrl-d' ]; then
+      git diff --color=always $shas | vimpager
+    elif [ "$k" = 'ctrl-o' ]; then
+      git checkout $shas 
+    else
+      for sha in $shas; do
+        git show --color=always $sha | vimpager
+      done
+    fi
+  done
+}
+
+# ftpane - switch pane
+ftpane () {
+  local panes current_window target target_window target_pane
+  panes=$(tmux list-panes -s -F '#I:#P - #{pane_current_path} #{pane_current_command}')
+  current_window=$(tmux display-message  -p '#I')
+
+  target=$(echo "$panes" | fzf) || return
+
+  target_window=$(echo $target | awk 'BEGIN{FS=":|-"} {print$1}')
+  target_pane=$(echo $target | awk 'BEGIN{FS=":|-"} {print$2}' | cut -c 1)
+
+  if [[ $current_window -eq $target_window ]]; then
+    tmux select-pane -t ${target_window}.${target_pane}
+  else
+    tmux select-pane -t ${target_window}.${target_pane} &&
+    tmux select-window -t $target_window
+  fi
+}
+
 zle -N fe 
 bindkey "^Xe" fe
 
@@ -37,4 +80,3 @@ bindkey "^Xh" fh
 
 zle -N fkill 
 bindkey "^Xq" fkill
-
