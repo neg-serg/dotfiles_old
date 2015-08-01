212d76ccc97b7349e18689a73b36bf879ac669e4
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-08-01 16:44:08 +0300

    [zsh] better colors for vim-interaction
    Former-commit-id: ff2d3e190bf74323149204b16e5a369efb7e2f65 [formerly dcbacc9a5a711822c3717ae6608398f4372ab33a] [formerly 9b27ff90389c6d0a2922dc1e625ef84f6f449d41 [formerly 00a592f0e2842dedeedc3bd4c015e3b9a50fed6a]]
    Former-commit-id: 84af2ac8f6a35d265ed500a2bfcf590dc4d5601d [formerly b418f1311cf0d6039ff1469174a3788b4363a1b2]
    Former-commit-id: 594b5745ed2ad04870f0fdee175a35eeea44afb7

--- .zsh/89-vim-interation.plugin.zsh
+++ .zsh/89-vim-interation.plugin.zsh
@@ -58,13 +58,20 @@ EOH
 
 function vim_file_open() {
     local file_name="$(resolve_file $line)"
-    local file_size=$(stat -c%s "$file_name" 2>/dev/null| numfmt --to=iec-i --suffix=B)
+    local FG24="[38;5;24m"
+    local FG29="[38;5;29m"
+    local FG237="[38;5;237m"
+    local file_size=$(stat -c%s "$file_name" 2>/dev/null| numfmt --to=iec-i --suffix=B|sed "s/\([KMGT]iB\|B\)/${FG29}&/")
     local file_length="`wc -l $file_name 2>/dev/null|grep -owE '[0-9]* '|tr -d ' '`"
+    local sz_msg="$fg[blue][$fg[white]sz ${FG237}~$fg[white] $file_size$fg[blue]]"
+    local len_msg="$fg[blue][$fg[white]len ${FG237}=$fg[white] $file_length$fg[blue]]"
+    local new_file_msg="$fg[blue][$fg[white] new_file $fg[blue]]"
+    local msg_delim="${FG24}::"
     file_name=$(bash -c "printf %q '$file_name'")
     if [ -f "$file_name" ]; then
-        echo "$fg[blue][$fg[white]>>$fg[blue]] -> $fg[white]$file_name :: [sz = $file_size] :: [len = $file_length]"
+        echo "$fg[blue][$fg[white]>>$fg[blue]] -> $fg[white]$file_name $msg_delim $sz_msg $msg_delim $len_msg"
     else
-        echo "$fg[blue][$fg[white]>>$fg[blue]] -> $fg[white]$file_name :: [ new_file ]"
+        echo "$fg[blue][$fg[white]>>$fg[blue]] -> $fg[white]$file_name $msg_delim $new_file_msg"
     fi
     eval $(echo tmux -S ~/1st_level/vim.socket run \'"$(echo vim --servername VIM --remote-silent "${file_name}")"\')
     file_name=
