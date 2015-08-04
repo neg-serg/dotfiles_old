04c7de80863d613f8dae1a46aabd0adbd6f25743
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-08-04 17:40:40 +0300

    [zsh] vim-interaction works corrent without sleep for now
    Former-commit-id: fa4bf64802550b20c89f06ac181974cb6c3ea48d [formerly 51af83953a72a668d0ffa7e78eb4184b001254a9] [formerly c5cf9851bddb9c8436b6f4afb9fbd849d6eedcc8 [formerly 5683ed897fbd5901e74dc2751cdcf6aa80a8c219]]
    Former-commit-id: 2e3c9afe131327728aad4c9f317cf6235a2a94fd [formerly 915dfc5c8f2263edcce4c0b8331ad32ce7bb619b]
    Former-commit-id: dc694a065f5e53767d7a1ec486cbaafab82951a7

--- .zsh/89-vim-interation.plugin.zsh
+++ .zsh/89-vim-interation.plugin.zsh
@@ -58,26 +58,39 @@ EOH
 
 function vim_file_open() {
     local file_name="$(resolve_file $line)"
+    file_name=$(bash -c "printf %q '$file_name'")
+
+    eval $(echo tmux -S ~/1st_level/vim.socket run \'"$(echo vim --servername VIM --remote-silent "${file_name}")"\')
+    
     local FG237="[38;5;237m"
     local file_size=$(stat -c%s "$file_name" 2>/dev/null| numfmt --to=iec-i --suffix=B|sed "s/\([KMGT]iB\|B\)/$fg[green]&/")
     local file_length="`wc -l $file_name 2>/dev/null|grep -owE '[0-9]* '|tr -d ' '`"
     local sz_msg="$fg[blue][$fg[white]sz ${FG237}~$fg[white] $file_size$fg[blue]]"
     local len_msg="$fg[blue][$fg[white]len ${FG237}=$fg[white] $file_length$fg[blue]]"
     local new_file_msg="$fg[blue][$fg[white] new_file $fg[blue]]"
+    local dir_msg="$fg[blue][$fg[white] directory $fg[blue]]"
     local msg_delim="[38;5;24m::"
     local prefix="$fg[blue][$fg[white]>>$fg[blue]]"
     local decoration="$fg[green]‒$fg[white]"
-    file_name=$(bash -c "printf %q '$file_name'")
-    local fancy_name="$(echo ${file_name}|sed "s|^${HOME}|$fg[green]~|;s|/|$fg[blue]&$fg[white]|g")"
-    local fancy_name_decorated="${decoration} $fg[white]${fancy_name} ${decoration}"
-    if [ -f "$file_name" ]; then
-        echo "${prefix} ${fancy_name_decorated} ${msg_delim} ${sz_msg} ${msg_delim} ${len_msg}"
+    local tmp_name="$(echo ${file_name}|sed "s|^${HOME}|$fg[green]~|;s|/|$fg[blue]&$fg[white]|g")"
+    local fancy_name="${decoration} $fg[white]${tmp_name} ${decoration}"
+    if [ -f "${file_name}" ] && [ ! -d "${file_name}" ]; then
+        local current_syntax=$(vim --servername "VIM" --remote-expr "b:current_syntax" 2>/dev/null)
+        if [[ ! $(echo ${current_syntax}|tr -d '[:blank:]') == "" ]]; then
+            local syn_msg=" ${msg_delim} $fg[blue][$fg[white] ft ${FG237}=$fg[white] ${current_syntax} $fg[blue]]$fg[white]"
+        else
+            syn_msg=""
+        fi
+        echo "${prefix} ${fancy_name} ${msg_delim} ${sz_msg} ${msg_delim} ${len_msg}${syn_msg}"
     else
-        echo "${prefix} ${fancy_name_decorated} ${msg_delim} ${new_file_msg}"
+        if [ ! -d "${file_name}"  ]; then
+            echo "${prefix} ${fancy_name} ${msg_delim} ${new_file_msg}"
+        else
+            echo "${prefix} ${fancy_name} ${msg_delim} ${dir_msg}"
+        fi
     fi
-    eval $(echo tmux -S ~/1st_level/vim.socket run \'"$(echo vim --servername VIM --remote-silent "${file_name}")"\')
+
     file_name=
-    sleep .17s
 }
 
 function process_list() {
