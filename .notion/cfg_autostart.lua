5c93a06bbd88ba3edabe83d244168401867e3759
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-08-09 14:28:03 +0300

    [notion] fix rofi

--- .notion/cfg_autostart.lua
+++ .notion/cfg_autostart.lua
@@ -3,7 +3,7 @@ function start_mpdstat()
 end
 function start_rofi()
     local rofi_font = '-font "Pragmata Pro for Powerline bold 12 "'
-    ioncore.exec('/usr/bin/rofi ' .. rofi_font ..  ' -lines 10 -width 1850 -yoffset -22 -key-run mod1-grave -location 6')
+    ioncore.exec('/usr/bin/rofi ' .. rofi_font ..  ' -b -disable-history -lines 10 -width 1850 -yoffset -22 -key-run mod1-grave -location 6')
 end
 start_mpdstat()
 start_rofi()
