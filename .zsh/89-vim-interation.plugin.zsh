5ce90520e6b795465b547ffca9601dcaa994437b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-08-09 12:43:14 +0300

    [zsh] vim-interaction: initial support for remote commands

--- .zsh/89-vim-interation.plugin.zsh
+++ .zsh/89-vim-interation.plugin.zsh
@@ -10,9 +10,10 @@ function resolve_file {
 
 function vim_file_open() (
     local file_name="$(resolve_file $line)"
+    local srv_name="VIM"
     file_name=$(bash -c "printf %q '$file_name'")
     
-    eval $(echo tmux -S ~/1st_level/vim.socket run \'"$(echo vim --servername VIM --remote-silent "${file_name}")"\')
+    eval $(echo tmux -S ~/1st_level/vim.socket run \'"$(echo vim --servername ${srv_name} --remote-silent "${file_name}")"\')
     
     local FG237="[38;5;237m"
     local file_size=$(stat -c%s "$file_name" 2>/dev/null| numfmt --to=iec-i --suffix=B|sed "s/\([KMGT]iB\|B\)/$fg[green]&/")
@@ -22,23 +23,23 @@ function vim_file_open() (
     local new_file_msg="$fg[blue][$fg[white] new_file $fg[blue]]"
     local dir_msg="$fg[blue][$fg[white] directory $fg[blue]]"
     local msg_delim="[38;5;24m::"
-    local prefix="$fg[blue][$fg[white]>>$fg[blue]]"
+    local pref="$fg[blue][$fg[white]>>$fg[blue]]"
     local decoration="$fg[green]‒$fg[white]"
     local tmp_name="$(echo ${file_name}|sed "s|^${HOME}|$fg[green]~|;s|/|$fg[blue]&$fg[white]|g")"
     local fancy_name="${decoration} $fg[white]${tmp_name} ${decoration}"
     if [[ -f "${file_name}" ]] && [[ ! -d "${file_name}" ]]; then
-        local current_syntax=$(vim --servername "VIM" --remote-expr "b:current_syntax" 2>/dev/null)
-        if [[ ! $(echo ${current_syntax}|tr -d '[:blank:]') == "" ]]; then
-            local syn_msg=" ${msg_delim} $fg[blue][$fg[white] ft ${FG237}=$fg[white] ${current_syntax} $fg[blue]]$fg[white]"
+        local ft_syntax=$(vim --servername "${srv_name}" --remote-expr "b:ft_syntax" 2>/dev/null)
+        if [[ ! $(echo ${ft_syntax}|tr -d '[:blank:]') == "" ]]; then
+            local syn_msg=" ${msg_delim} $fg[blue][$fg[white] ft ${FG237}=$fg[white] ${ft_syntax} $fg[blue]]$fg[white]"
         else
             syn_msg=""
         fi
-        echo "${prefix} ${fancy_name} ${msg_delim} ${sz_msg} ${msg_delim} ${len_msg}${syn_msg}"
+        echo "${pref} ${fancy_name} ${msg_delim} ${sz_msg} ${msg_delim} ${len_msg}${syn_msg}"
     else
         if [[ ! -d "${file_name}"  ]]; then
-            echo "${prefix} ${fancy_name} ${msg_delim} ${new_file_msg}"
+            echo "${pref} ${fancy_name} ${msg_delim} ${new_file_msg}"
         else
-            echo "${prefix} ${fancy_name} ${msg_delim} ${dir_msg}"
+            echo "${pref} ${fancy_name} ${msg_delim} ${dir_msg}"
         fi
     fi
 
@@ -54,42 +55,39 @@ function process_list() {
 function v {
     wid=$(xdotool search --classname wim)
     local wim_font="PragmataPro for Powerline"
-#   local wim_font_s="Mensch:size=14"
+    local font_size=20
+    local wim_font_s="Mensch:size=14"
     local sock_path="${HOME}/1st_level/vim.socket"
     local srv_name="VIM"
-    #----------------------------------------------------------------
-    # Examples :
-    #----------------------------------------------------------------
-    #  -b':vsp' -b':sp'
-    #  -b':wincmd k' -b':wincmd j'
-    #  -b':wincmd l' -b':wincmd h'
-    #  -b':sp<cr>:wincmd k'
-    #  -b':sp<cr>:wincmd j'
-    #  -b':vsp<cr>:wincmd h'
-    #  -b':vsp<cr>:wincmd l'
-    local cmd=""
-    # local to_normal="<c-\\><c-n>"
-    local to_normal=""
-    local before=""
-    local after=""
-    while getopts ":b:a:" option; do
-        case $option in
-            a) after="$OPTARG" ;;
-            b) before="$before$OPTARG" ;;
+    
+    readonly to_normal="--remote-send <C-\><C-N>:call<SPACE>foreground()<CR>"
+    while getopts ":b:a:" opt; do
+        #  -b':vsp' -b':sp'
+        #  -b':wincmd k' -b':wincmd j'
+        #  -b':wincmd l' -b':wincmd h'
+        #  -b':sp<cr>:wincmd k'
+        #  -b':sp<cr>:wincmd j'
+        #  -b':vsp<cr>:wincmd h'
+        #  -b':vsp<cr>:wincmd l'
+        case ${opt} in
+            a) after="${OPTARG}" ;;
+            b) before="${before}${OPTARG}" ;;
         esac
     done
     shift $((OPTIND-1))
-    [[ ${after#:} != $after && ${after%<cr>} == $after ]] && after="$after<cr>"
-    [[ ${before#:} != $before && ${before%<cr>} == $before ]] && before="$before<cr>"
-    local files=""
-    for f in "$@"; do files="$files $(resolve_file $f)"; done
-    [[ -n $files ]] && files=':args! '"$files<cr>"
-    cmd="${to_normal}${before}${files}${after}"
-    # vim --servername ${srv_name} --remote-send "$cmd"
-    # echo cmd="${to_normal}${before}${files}${after}"
+    [[ ${after#:} != $after && ${after%<CR>} == ${after} ]] && after="${after}<CR>"
+    [[ ${before#:} != $before && ${before%<CR>} == ${before} ]] && before="${before}<CR>"
+    local mfiles=""
+    for f; do
+        f="$(resolve_file ${f})"
+        mfiles="${mfiles} $(bash -c "printf %q '${f}'")"
+    done
+    [[ -n ${mfiles} ]] && mfiles=':args! '"${mfiles}<CR>"
+    local cmd="${to_normal}${before}${after}"
+    vim --servername ${srv_name} --remote-send "${cmd}"
     #----------------------------------------------------------------
-    if [[ -z "$wid" ]]; then
-        st -f "${wim_font}:pixelsize=20" -c 'wim' -e bash -c "tmux -S ${sock_path} new \"vim --servername ${srv_name}\" && \
+    if [[ -z "${wid}" ]]; then
+        st -f "${wim_font}:pixelsize=${font_size}" -c 'wim' -e bash -c "tmux -S ${sock_path} new \"vim --servername ${srv_name}\" && \
                 tmux -S ${sock_path} switch-client -t vim" &
         process_list ".6s" "$@"
     else  
