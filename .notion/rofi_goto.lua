a47b1f6d61190b6532e7d25f46ff50e343623841
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-08-13 17:33:02 +0300

    [notion] make rofi-goto more simple

--- .notion/rofi_goto.lua
+++ .notion/rofi_goto.lua
@@ -1,16 +1,42 @@
-rf={
+dopath("rofi_handlers")
+
+local rf={
     font = '-font "' .. 'Pragmata Pro for Powerline' .. ' '.. 'bold' ..' ' .. 12 .. '"',
     common = ' -auto-select -dmenu -opacity 95 -yoffset -22 ',
     colors = ' -fg "#666666" -bg "#000" -hlfg "#aaaaaa" -hlbg "#194558" -bc "#202020"',
     width = 1850,
 }
 
-function rofi_template(rofi_prefix,ipc_file,lines)
+local function rofi_template(pref,file_name,lines,fn)
+------------------------------------------
+    local function new_ipc_file(file_name)
+        local shm_dir = "/tmp"
+        return shm_dir .. "/" .. file_name
+    end
+------------------------------------------
+    local function handle_input(func)
+        if func ~= nil then
+            local t = func()
+            for i in pairs(t) do rofi_pipe:write(t[i],'\n') end
+        end
+        rofi_pipe:close() 
+    end
+------------------------------------------
+    local function new_rofi_prefix(prefix)
+        local function quote(str)
+            return '"' .. str:gsub('\\', '\\\\'):gsub('"', '\\"') .. '"'
+        end
+        local lb = '[' rb = ']'
+        local prompt = ' >> '
+        local str = lb .. prefix .. rb .. prompt
+        return " -p " .. quote(str)
+    end
+------------------------------------------
+    local rofi_prefix = new_rofi_prefix(pref)
     local columns = 0
     local columns_str = ""
-    if lines == nil then
-        lines = 10
-    else
+    local ipc_file = new_ipc_file(file_name)
+    if lines == nil then lines = 10 else
         columns = 10
         columns_str = ' -columns ' .. columns
     end
@@ -18,169 +44,80 @@ function rofi_template(rofi_prefix,ipc_file,lines)
     ' -padding 2 -width ' .. rf.width
     rofi_pipe = io.popen(rofi_cmd .. rofi_prefix .. "> " .. ipc_file, "w")
     rofi_pipe:setvbuf("line")
+    handle_input(fn)
+    local fp = io.open(ipc_file)
+    answer = fp:read("*l")
+    fp:close()
+    return answer
 end
 
-dopath("rofi_handlers")
-
-local function new_ipc_file(file_name)
-    shm_dir = "/tmp"
-    return shm_dir .. "/" .. file_name
-end
-
-local function new_rofi_prefix(prefix)
-    local function quote(str)
-        return '"' .. str:gsub('\\', '\\\\'):gsub('"', '\\"') .. '"'
-    end
-    local lb = '['
-    local rb = ']'
-    local prompt = ' >> '
-    str = lb .. prefix .. rb .. prompt
-    return " -p " .. quote(str)
-end
-
-function complete_clientwin()
-    return complete_name(ioncore.clientwin_i)
-end
-
-function complete_ws()
-    return workspacelist(ioncore.region_i)
-end
-
-local function mk_completion_add(entries)
-    return function(s) 
-               if s then
-                   table.insert(entries, s)
-               end
-           end
-end
-
-local function sort(entries)
-    table.sort(entries, function(a, b) return a.name < b.name end)
-    return entries
-end
-
-function workspacelist(iter)
-    local entries={}
-    local ws_add=mk_completion_add(entries)
-    iter(function(obj)
+local function complete_ws()
+    local t={}
+    local ws_add=(function(s) if s then table.insert(t, s) end end)
+    ioncore.region_i(function(obj)
              if obj_is(obj, "WGroupWS") then
                 ws_add(obj:name())
              end
              return true
          end)
-    return entries
+    return t
 end
 
-function complete_name(iter)
-    local entries={}
-    local list_add=mk_completion_add(entries)
-    
-    iter(function(reg)
+local function complete_name()
+    local t={}
+    local list_add=(function(s) if s then table.insert(t, s) end end)
+    ioncore.clientwin_i(function(reg)
              if not string.match(reg:name(), "dzen.*title") then
                 list_add(reg:name())
              end
              return true
          end)
-    
-    return entries
+    return t
 end
 
 function rofi_renameworkspace(mplex,ws)
     if not mplex then
-        assert(ws)
-        mplex=ioncore.find_manager(ws, "WMPlex")
+        assert(ws) mplex=ioncore.find_manager(ws, "WMPlex")
     elseif not ws then
-        assert(mplex)
-        ws=ioncore.find_manager(mplex, "WGroupWS")
+        assert(mplex) ws=ioncore.find_manager(mplex, "WGroupWS")
     end
-    
     assert(mplex and ws)
-
-    local tbl = {}
-    local ws_file = new_ipc_file("rename_ws")
-    local rofi_prefix = new_rofi_prefix("main_menu")
-    rofi_template(rofi_prefix,ws_file)
-    rofi_pipe:write(ws:name(),'\n')
-    rofi_pipe:close() 
-    fp = io.open(ws_file)
-    wsname = fp:read("*l")
-    if not (wsname == nil or wsname == '') then
-        ws:set_name(wsname)
-    end
-    fp:close()
+    local wsname = rofi_template("new_ws_name :: "..ws:name(),"rename_ws",_,_)
+    rename_workspace_handler(wsname,ws)
 end
 
-function complete_mainmenu(entries)
-    table.insert(entries, "save")  --  ioncore.snapshot()
-    table.insert(entries, "restart") --  ioncore.restart()
-    table.insert(entries, "ratpoison-restart") -- ioncore.restart_other('ratpoison')
+local function complete_mainmenu()
+    local t = {"save", "restart", "ratpoison-restart", "xrandr-set" }
     local str='ctd'
     for i = 1, #str do
         local c = str:sub(i,i)
-        table.insert(entries, c .. "wm-restart")
+        table.insert(t, c .. "wm-restart")
     end
+    return t
 end
 
-function complete_mpd_menu(entries)
-    table.insert(entries, "title_copy")
-    table.insert(entries, "artist_copy")
-    table.insert(entries, "mpd_show")
-    table.insert(entries, "resolution-set")
+local function complete_mpd_menu()
+    local t = { "title_copy", "artist_copy", "mpd_show" }
+    return t
 end
 
 function rofi_renameframe(frame)
-    local file_name = new_ipc_file("rename_frame")
-    local prefix =  new_rofi_prefix("frame_name")
-    rofi_template(prefix,file_name)
-    rofi_pipe:write(frame:name(),'\n')
-    rofi_pipe:close() 
-    local fp = io.open(file_name)
-    frame_name = fp:read("*l")
-    if not (frame_name == nil or frame_name == '') then
-        frame:set_name(frame_name)
-    end
-    fp:close()
+    local frame_name = rofi_template("frame_name :: "..frame:name(),"rename_frame",_,_)
+    rename_frame_handler(frame_name,frame)
 end
 
 function rofi_mainmenu()
-    local tbl = {}
-    local mainmenu_file = new_ipc_file("mainmenu")
-    local rofi_prefix = new_rofi_prefix("mainmenu")
-    rofi_template(rofi_prefix,mainmenu_file)
-    complete_mainmenu(tbl)
-    for i in pairs(tbl) do rofi_pipe:write(tbl[i],'\n') end
-    rofi_pipe:close() 
-    fp = io.open(mainmenu_file)
-    x = fp:read("*l")
+    local x = rofi_template("mainmenu","mainmenu",_,complete_mainmenu)
     mainmenu_handler(x)
-    fp:close()
 end
 
 function rofi_mpdmenu()
-    local tbl = {}
-    local mpdmenu_file = new_ipc_file("mpdmenu")
-    local rofi_prefix = new_rofi_prefix("mpd")
-    rofi_template(rofi_prefix,mpdmenu_file)
-    complete_mpd_menu(tbl)
-    for i in pairs(tbl) do rofi_pipe:write(tbl[i],'\n') end
-    rofi_pipe:close() 
-    fp = io.open(mpdmenu_file)
-    x = fp:read("*l")
+    local x = rofi_template("mpdmenu","mpd",_,complete_mpd_menu)
     mpdmenu_handler(x)
-    fp:close()
 end
 
 function rofi_goto_win()
-    local tbl = {}
-    local goto_win_file = new_ipc_file("goto_win")
-    local rofi_prefix = new_rofi_prefix("go")
-    rofi_template(rofi_prefix,goto_win_file)
-    tbl = complete_clientwin()
-    for i in pairs(tbl) do rofi_pipe:write(tbl[i],'\n') end
-    rofi_pipe:close() 
-    fp = io.open(goto_win_file)
-    x = fp:read("*l")
-    fp:close(rofi_prefix, goto_win_file)
+    local x = rofi_template("goto_win","go",_,complete_name)
     local win = ioncore.lookup_clientwin(x)
     if win then
         ioncore.defer(function () win:goto_focus() end)
@@ -188,46 +125,11 @@ function rofi_goto_win()
 end
 
 function rofi_goto_or_create_ws(reg)
-    local tbl = {}
-    local ws_file = new_ipc_file("goto_ws")
-    local rofi_prefix = new_rofi_prefix("ws")
-    rofi_template(rofi_prefix,ws_file,1)
-    tbl = complete_ws()
-    for i in pairs(tbl) do rofi_pipe:write(tbl[i],'\n') end
-    rofi_pipe:close() 
-    fp = io.open(ws_file)
-    name = fp:read("*l")
-    if not (name == nil or name == '') then
-        workspace_handler(reg,name)
-    end
-    fp:close()
+    local name = rofi_template("goto_ws","ws",1,complete_ws)
+    goto_or_create_ws_handler(name,reg)
 end
 
-function rofi_attach_win(frame, str)
-    tbl = {}
-    attach_win_file = new_ipc_file("attach_win")
-    prefix = new_rofi_prefix("attach")
-    rofi_template(prefix,attach_win_file)
-    tbl = complete_clientwin()
-    for i in pairs(tbl) do rofi_pipe:write(tbl[i],'\n') end
-    rofi_pipe:close() 
-    fp = io.open(attach_win_file)
-    str = fp:read("*l")
-    fp:close()
-
-    local cwin=ioncore.lookup_clientwin(str)
-    if not cwin then
-        return
-    end
-    local reg=cwin:groupleader_of()
-    
-    local function attach()
-        frame:attach(reg, { switchto = true })
-    end
-    if frame:rootwin_of()~=reg:rootwin_of() then
-    elseif reg:manager()==frame then
-        reg:goto_focus()
-    else
-        ioncore.defer(function () attach() end)
-    end
+function rofi_attach_win(frame,str)
+    local str = rofi_template("attach","attach_win",_,complete_name)
+    attach_win_handler(str,frame)
 end
