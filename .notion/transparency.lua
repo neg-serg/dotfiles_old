37c150a242fd6b556db50f79b6848cad2e51326f
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-08-23 17:11:14 +0300

    [notion] update session
    Former-commit-id: 763cd104bf7a73386642f04f76eda7db9a8b9764 [formerly 7f949ed2b8a3bf6fc3f57c8b2a0bf5c891fc65f9] [formerly 540698428557292ff72a8a3593e52025d2d39a49 [formerly 6fa085affd90ca7726f87b7a948c053889e47ebd]]
    Former-commit-id: bd5bc1ec79d80ea181c3af78f604531cabf8bcc9 [formerly 5e7de0e822c609f0129baaf81c7166d0362136e1]
    Former-commit-id: 1dc93c24536f244ae7b03e81cc81508cf9223d49

--- .notion/transparency.lua
+++ .notion/transparency.lua
@@ -1,13 +1,9 @@
-local transparent_table = {}
-local nontransparent_table = {}
-
-function framelist(iter)
+local function framelist(iter)
     transparent_table = {}
     nontransparent_table = {}
 
     local entries={}
     local ws_add=function(s) if s then table.insert(entries, s) end end
-
     local name=""
 
     iter(function(obj)
@@ -24,12 +20,13 @@ function framelist(iter)
     return entries
 end
 
-function maketransparent(reg)
+local function maketransparent(reg)
     local atom_client_opacity = ioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
-    local opacity_level = 3435973836
+    local atom_client_opacity = ioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
 
-    local atom_client_opacity = ioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
+    local opacity_level = 3435973836
     local cur=ioncore.find_manager(ioncore.current(), "WFrame")
+
     framelist(ioncore.region_i)
     for _,reg in ipairs(transparent_table) do
         notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {opacity_level})
