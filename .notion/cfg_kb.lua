6839b7a9095ba8c375f011324aee1040f46ee534
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-09-12 00:31:02 +0300

    [notion] s/ioncore/notioncore and another adds
    Former-commit-id: e496539b2cdb3964d7e8b5d287f81f87d158f634 [formerly fce3561ecfcfe6315ebc6a261712f62385d4f86d] [formerly b8bd1865cf4bf225fa3e41ceed802fee13edf366 [formerly e35c6547bb22b788f85b96874efddf646eab86f1]]
    Former-commit-id: ca51c8a821c32015646aadee27c925bbe3202626 [formerly 13e7c9f89ede03584841b42076701e6232b178d1]
    Former-commit-id: ef192520e16c37f40e82354a8f3c36d0374a71e9

--- .notion/cfg_kb.lua
+++ .notion/cfg_kb.lua
@@ -4,13 +4,13 @@ Neg_kb.WMPlex_toplevel = {
     ---------------------
     -- WMPlex toplevel --
     ---------------------
-    kpress("M4+slash",   "ioncore.goto_previous()"),
-    kpress("M1+Tab",     "ioncore.goto_previous()"),
+    kpress("M4+slash",   "notioncore.goto_previous()"),
+    kpress("M1+Tab",     "notioncore.goto_previous()"),
     kpress("M1+space",   "nop()"),
     kpress("M4+F2",      "repl(_)"),
     kpress("M4+F11",     "rofi.mainmenu()"),
     kpress("M4+Sh+L", "notioncore.exec_on(_, notioncore.lookup_script('notion-lock'))"),
-    kpress("M4+Sh+D", "ioncore.detach(_chld, 'toggle')", "_chld:non-nil"),
+    kpress("M4+Sh+D", "notioncore.detach(_chld, 'toggle')", "_chld:non-nil"),
     -------------------------------------------------------------------------------------
     --[[ SCRATCHPADS  ]]--------------------------------
     --------------------
@@ -24,34 +24,34 @@ Neg_kb.WMPlex_toplevel = {
     kpress("M4+Ct+4",    "named_scratchpad(_, 'etc')"),
     kpress("M4+Ct+G",    "rofi.goto_or_create_ws(_)"),
     kpress("M1+G",       "rofi.goto_win(_)"),
-    kpress("M4+F9",      "ioncore.create_ws(_)"),
+    kpress("M4+F9",      "notioncore.create_ws(_)"),
     -------------------------------------------------------------------------------------
     --[[  PLAYER CONTROL ]]---------------------------------
     -----------------------
-    kpress("XF86AudioPrev",        "ioncore.exec_on(_, 'mpc prev')"),
-    kpress("XF86AudioNext",        "ioncore.exec_on(_, 'mpc next')"),
-    kpress("XF86AudioPlay",        "ioncore.exec_on(_, 'mpc toggle')"),
-    kpress("XF86AudioStop",        "ioncore.exec_on(_, 'mpc stop')"),
-    kpress("XF86AudioLowerVolume", "ioncore.exec_on(_, 'mpc volume -1')"),
-    kpress("XF86AudioRaiseVolume", "ioncore.exec_on(_, 'mpc volume +1')"),
-    kpress("M4+Ct+T",              "ioncore.exec_on(_, 'dipser -S')"),
-    kpress("M4+8",                 "ioncore.exec_on(_, 'mpc volume 0 || amixer -q set Master 0% mute')"),
-    kpress("M4+8+Sh",              "ioncore.exec_on(_, 'mpc volume 100 || amixer -q set Master 100% unmute ')"),
+    kpress("XF86AudioPrev",        "notioncore.exec_on(_, 'mpc prev')"),
+    kpress("XF86AudioNext",        "notioncore.exec_on(_, 'mpc next')"),
+    kpress("XF86AudioPlay",        "notioncore.exec_on(_, 'mpc toggle')"),
+    kpress("XF86AudioStop",        "notioncore.exec_on(_, 'mpc stop')"),
+    kpress("XF86AudioLowerVolume", "notioncore.exec_on(_, 'mpc volume -1')"),
+    kpress("XF86AudioRaiseVolume", "notioncore.exec_on(_, 'mpc volume +1')"),
+    kpress("M4+Ct+T",              "notioncore.exec_on(_, 'dipser -S')"),
+    kpress("M4+8",                 "notioncore.exec_on(_, 'mpc volume 0 || amixer -q set Master 0% mute')"),
+    kpress("M4+8+Sh",              "notioncore.exec_on(_, 'mpc volume 100 || amixer -q set Master 100% unmute ')"),
     submap("M4+Ct+R", {
-        kpress("t", "ioncore.exec_on(_, 'xcalib -c')"),
-        kpress("i", "ioncore.exec_on(_, 'xcalib -alter -invert')"),
-        kpress("r", "ioncore.exec_on(_, 'xcalib -red 0.1 0.0 1 -a')"),
+        kpress("t", "notioncore.exec_on(_, 'xcalib -c')"),
+        kpress("i", "notioncore.exec_on(_, 'xcalib -alter -invert')"),
+        kpress("r", "notioncore.exec_on(_, 'xcalib -red 0.1 0.0 1 -a')"),
     }),
 
     -------------------------------------------------------------------------------------
     --[[ RESTART ]]----------------------------------------
     ---------------
-    kpress("M4+apostrophe", "ioncore.restart()"),
+    kpress("M4+apostrophe", "notioncore.restart()"),
     -------------------------------------------------------------------------------------
     --[[ SLEEP ]]-----------------------------------------
     -------------
-    kpress("XF86Sleep", "ioncore.exec_on(_, 'sudo pm-suspend')"),
-    kpress("M4+Ct+Sh+u", "ioncore.exec_on(_, 'sudo systemctl suspend')"),
+    kpress("XF86Sleep", "notioncore.exec_on(_, 'sudo pm-suspend')"),
+    kpress("M4+Ct+Sh+u", "notioncore.exec_on(_, 'sudo systemctl suspend')"),
     -------------------------------------------------------------------------------------
     --[[  PROGS RUN by APP  ]]-----------------------------
     --------------------------
@@ -59,56 +59,57 @@ Neg_kb.WMPlex_toplevel = {
     kpress("M4+x",     "app.byinstance('~/bin/urxvt', 'URxvt','MainTerminal')"),
     kpress("M4+Ct+a",  "app.byclass_withtag(nil, 'Vmware',nil, 'virt')"),
     kpress("M4+b",     "app.byclass_withtag(nil, 'mpv',nil, 'video')"),
-    kpress("M4+w",     "app.byinstance_withtag('/usr/bin/firefox||firefox-nightly', 'Firefox','Navigator',nil,'www')"),
+    kpress("M4+w",     "app.byinstance_withtag('/usr/bin/firefox-beta||/usr/bin/firefox||firefox-nightly', 'Firefox','Navigator',nil,'www')"),
     kpress("M4+o",     "app.byclass_withtag('zathura','Zathura','pdf')"),
     kpress("M4+Ct+C",  "app.byclass('~/bin/sx ~/dw/*', 'Sxiv')"),
     -------------------------------------------------------------------------------------
     --[[  MISC  ]]---------------------------------------
     --------------
     kpress("M4+Ct+Q",  "WRegion.rqclose_propagate(_, _sub)"),
-    kpress("M4+U",     "ioncore.exec_on(_, 'udiskie-umount -a')"),
-    kpress("M4+M",     "ioncore.exec_on(_, '~/bin/scripts/rofi_xprop')"),
-    kpress("M4+Sh+U",  "ioncore.exec_on(_, 'eject')"),
+    kpress("M1+grave", "notioncore.exec_on(_, '~/bin/dmenu_run2')"),
+    kpress("M4+U",     "notioncore.exec_on(_, 'udiskie-umount -a')"),
+    kpress("M4+M",     "notioncore.exec_on(_, '~/bin/scripts/rofi_xprop')"),
+    kpress("M4+Sh+U",  "notioncore.exec_on(_, 'eject -T')"),
     kpress("M4+Sh+F",  "app.byinstance('lowriter', 'VCLSalFrame', 'libreoffice-writer')"),
-    kpress("M4+c",     "ioncore.exec_on(_, 'sh ~/bin/clip')"),
+    kpress("M4+c",     "notioncore.exec_on(_, 'sh ~/bin/clip')"),
     kpress("M4+Sh+m",   "rofi.mpdmenu()"),
     submap("M1+E",{
         kpress("Ct+r", "app.byinstance('cr3', 'Cr3', 'cr3')"),
-        kpress("d",    "ioncore.exec_on(_, 'stardict')"),
-        kpress("i",    "ioncore.exec_on(_, '~/bin/pls -output')"),
+        kpress("d",    "notioncore.exec_on(_, 'stardict')"),
+        kpress("i",    "notioncore.exec_on(_, '~/bin/pls -output')"),
         kpress("l",    "radare2(_)"),
         kpress("m",    "app.byinstance('BROWSER=firefox /bin/urxvtc -name mutt -e mutt', 'URxvt', 'mutt')"),
         kpress("n",    "app.byinstance('nicotine.py || nicotine', 'Nicotine.py', 'nicotine.py')"),
-        kpress("o",    "ioncore.exec_on(_, '~/bin/pls -sink')"),
-        kpress("p",    "ioncore.exec_on(_, 'pavucontrol')"),
-        kpress("q",    "ioncore.exec_on(_, '~/bin/mpd_sel.sh')"),
-        kpress("Sh+d", "ioncore.exec_on(_, '~/bin/scripts/exec_demo.sh')"),
+        kpress("o",    "notioncore.exec_on(_, '~/bin/pls -sink')"),
+        kpress("p",    "notioncore.exec_on(_, 'pavucontrol')"),
+        kpress("q",    "notioncore.exec_on(_, '~/bin/mpd_sel.sh')"),
+        kpress("Sh+d", "notioncore.exec_on(_, '~/bin/scripts/exec_demo.sh')"),
         kpress("Sh+g", "gdb(_)"),
-        kpress("Sh+k", "ioncore.exec_on(_, '~/bin/scripts/toggle_keynav')"),
-        kpress("Sh+o", "ioncore.exec_on(_, '~/bin/pls -vol')"),
+        kpress("Sh+k", "notioncore.exec_on(_, '~/bin/scripts/toggle_keynav')"),
+        kpress("Sh+o", "notioncore.exec_on(_, '~/bin/pls -vol')"),
         kpress("Sh+r", "ranger(_)"),
-        kpress("Sh+t", "ioncore.exec_on(_, '~/bin/scripts/toggle_stalonetray')"),
-        kpress("s",    "ioncore.exec_on(_, 'skype')"),
-        kpress("t",    "ioncore.exec_on(_, 'urxvt')"),
-        kpress("v",    "ioncore.exec_on(_, 'vmware')"),
-        kpress("w",    "ioncore.exec_on(_, '~/bin/webcam')"),
+        kpress("Sh+t", "notioncore.exec_on(_, '~/bin/scripts/toggle_stalonetray')"),
+        kpress("s",    "notioncore.exec_on(_, 'skype')"),
+        kpress("t",    "notioncore.exec_on(_, 'urxvt')"),
+        kpress("v",    "notioncore.exec_on(_, 'vmware')"),
+        kpress("w",    "notioncore.exec_on(_, '~/bin/webcam')"),
     }),
 }
 
 Neg_kb.WScreen = {
     submap("M1+E", {
-            kpress("Ct+A", "ioncore.goto_activity()"),
-            kpress("Ct+t", "ioncore.tagged_clear()"),
+            kpress("Ct+A", "notioncore.goto_activity()"),
+            kpress("Ct+t", "notioncore.tagged_clear()"),
             }),
-    kpress("M4+Sh+1",      "ioncore.goto_nth_screen(0)"),
-    kpress("M4+Sh+2",      "ioncore.goto_nth_screen(1)"),
-    kpress("M4+Sh+comma",  "ioncore.goto_prev_screen()"),
-    kpress("M4+Sh+period", "ioncore.goto_next_screen()"),
+    kpress("M4+Sh+1",      "notioncore.goto_nth_screen(0)"),
+    kpress("M4+Sh+2",      "notioncore.goto_nth_screen(1)"),
+    kpress("M4+Sh+comma",  "notioncore.goto_prev_screen()"),
+    kpress("M4+Sh+period", "notioncore.goto_next_screen()"),
     submap("M1+E", {
         kpress("AnyModifier+L",    "WRegion.rqorder(_chld, 'front')","_chld:non-nil"),
         kpress("AnyModifier+Sh+L", "WRegion.rqorder(_chld, 'back')","_chld:non-nil"),
     }),
-    kpress("M4+grave", "ioncore.goto_next(_chld, 'right')", "_chld:non-nil"),
+    kpress("M4+grave", "notioncore.goto_next(_chld, 'right')", "_chld:non-nil"),
     ---------------------------------------------------------
     kpress("M4+H", "_chld:focus_direction('left')", "_chld:non-nil"),
     kpress("M4+J", "_chld:focus_direction('down')", "_chld:non-nil"),
@@ -137,7 +138,7 @@ Neg_kb.WMPlex = {
 }
 
 Neg_kb.WFrame = {
-    kpress("M4+S","mod_sp.set_shown(ioncore.lookup_region(_:name(), 'WFrame'), 'unset' )" ),
+    kpress("M4+S","mod_sp.set_shown(notioncore.lookup_region(_:name(), 'WFrame'), 'unset' )" ),
     kpress("M4+6","rofi.renameframe(_)"),
     kpress("M4+backslash", "WFrame.switch_next(_)"),
     kpress("M4+3", "WFrame.switch_next(_)"),
@@ -155,7 +156,7 @@ Neg_kb.WFrame = {
 
 Neg_kb.WFrame_toplevel = {
     kpress("M4+A",         "rofi.attach_win(_)"),
-    kpress("M1+A",         "ioncore.tagged_attach(_)"),
+    kpress("M1+A",         "notioncore.tagged_attach(_)"),
     submap("M1+E", {
         kpress("H", "WFrame.maximize_horiz(_)"),
         kpress("V", "WFrame.maximize_vert(_)"),
@@ -208,14 +209,14 @@ Neg_kb.WMoveresMode = {
 }
 
 Neg_kb.Tiling = {
-    kpress("M4+Up",   "ioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
-    kpress("M4+k",    "ioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
-    kpress("M4+Down", "ioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
-    kpress("M4+j",    "ioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
-    kpress("M4+Left", "ioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
-    kpress("M4+h",    "ioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
-    kpress("M4+Right","ioncore.goto_next(_sub, 'right', {no_ascend=_})"),
-    kpress("M4+l",    "ioncore.goto_next(_sub, 'right', {no_ascend=_})"),
+    kpress("M4+Up",   "notioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
+    kpress("M4+k",    "notioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
+    kpress("M4+Down", "notioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
+    kpress("M4+j",    "notioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
+    kpress("M4+Left", "notioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
+    kpress("M4+h",    "notioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
+    kpress("M4+Right","notioncore.goto_next(_sub, 'right', {no_ascend=_})"),
+    kpress("M4+l",    "notioncore.goto_next(_sub, 'right', {no_ascend=_})"),
     kpress("M4+Ct+H", "WTiling.split_at(_, _sub, 'right', true)"),
     kpress("M4+Ct+L", "WTiling.split_at(_, _sub, 'left', true)"),
     kpress("M4+Ct+K", "WTiling.split_at(_, _sub, 'bottom', true)"),
