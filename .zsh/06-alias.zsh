8df9e356c2a9792905c3bd4142091f3546533212
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-09-15 00:22:22 +0300

    [zsh] update aliases

--- .zsh/06-alias.zsh
+++ .zsh/06-alias.zsh
@@ -1,55 +1,68 @@
 (){
-    if [[ -x `which cope_path 2> /dev/null` ]]; then
-        local p=$(cope_path)
-        for i in $p/*; alias $i=\"$i\"
-        alias df="$p/df -hT"
+    if [[ -x $(which cope_path 2> /dev/null) ]]; then
+        # to prevent slow cope_path evaluation
+        local _cope_path=$(cope_path)
+        for i in ${_cope_path}/*; alias $i=\"$i\"
+        alias df="${_cope_path}/df -hT"
     else
         alias df="df -hT"
     fi
+    unset _cope_path
 }
+
+# # un-smart function for viewing sectioned partitions:
+# function dfu() {
+#   local fs_types
+#   fs_types=(nilfs2 btrfs ext2 ext3 ext4 jfs xfs zfs reiserfs reiser4 minix ntfs ntfs-3g fat vfat fuse)
+#   df -hTP -x rootfs -x devtmpfs -x tmpfs -x none ; print
+#   df -hTP $(for f in $FSTYPES; { print - " -x $f" })
+# }
+
 alias pstree="pstree -U "$@" | sed '
 	s/[-a-zA-Z]\+/\x1B[32m&\x1B[0m/g
 	s/[{}]/\x1B[31m&\x1B[0m/g
 	s/[─┬─├─└│]/\x1B[34m&\x1B[0m/g'"
 
 alias '?=bc -l <<<'
-alias stderred="LD_PRELOAD=${HOME}/bin/lib/libstderred.so${LD_PRELOAD:+:\$LD_PRELOAD}"
+alias stderred="LD_PRELOAD=${BIN_HOME}/lib/libstderred.so${LD_PRELOAD:+:\$LD_PRELOAD}"
 
 # If noglob (for zsh) is not available, just make it a noop
 if ! type noglob >/dev/null 2>&1; then
     alias noglob=''
 fi
-NOGLOB_LIST=( \
-    fc find ftp sftp lftp history locate rake rsync scp \
-    eix mmv wget clive clivescan youtube-dl \
-    translate links links2 xlinks2 lynx \
-    you-get bower pip
-)
-for i in ${NOGLOB_LIST[@]}; alias ${i}="noglob ${i}";
+noglob_list=( \
+    fc find {,s,l}ftp history locate rake rsync scp \
+    eix {z,m}mv wget clive{,scan} youtube-{dl,viewer} \
+    translate links{,2} lynx you-get bower pip sp)
+rlwrap_list=( bigloo clisp irb )
+sudo_list=({u,}mount ch{mod,own}) 
+[[ -x /usr/bin/systemctl ]] && sysctl_pref="systemctl"
+sys_sudo_list=(reboot halt poweroff)
 
-alias "zmv=noglob zmv -v"
+for i in ${sudo_list[@]}; alias "${i}=sudo ${i}";
+for i in ${noglob_list[@]}; alias "${i}=noglob ${i}";
+for i in ${rlwrap_list[@]}; alias "${i}=rlwrap ${i}";
+for i in ${sys_sudo_list[@]}; alias "${i}=sudo ${sysctl_pref} ${i}"
+unset noglob_list rlwrap_list sudo_list sys_sudo_list
+
 alias fevil='find . -regextype posix-extended -regex'
 
 function sp() {
   setopt extendedglob bareglobqual
-  du -sch -- ${~^@:-"*"}(D) | sort -h
+  du -sch -- ${~^@:-"*"}(D) | sort -h| distribution --size=small
 }
-alias sp='noglob sp'
-alias cdu=cdu -idh
+alias cdu='cdu -idh'
 
 if [[ $UID != 0 ]]; then
     if [ -f "$HOME/.ssh/config" ]; then
         for host in $( perl -ne 'print "$1\n" if /\A[Hh]ost\s+(.+)$/' ${HOME}/.ssh/config);
             alias $host="ssh $host '$@'";
     fi
-    alias bigloo='rlwrap bigloo'
-    alias clisp= 'rlwrap clisp'
-    alias irb=   'rlwrap irb-1.8'
 fi
 
 alias ls="ls --color=auto"   # do we have GNU ls with color-support?
-if [[ ! -x "${HOME}/bin/l" ]]; then
-    if  [[ -x "${HOME}/bin/lsp" ]]; then
+if [[ ! -x "${BIN_HOME}/l" ]]; then
+    if  [[ -x "${BIN_HOME}/lsp" ]]; then
         alias l="lsp"
         if [[ -x "/usr/bin/vendor_perl/ls++" ]]; then 
             alias l="ls++"; 
@@ -62,9 +75,6 @@ alias l.='ls -d .*'
 
 alias primusrun="vblank_mode=0 primusrun"
 alias gps='ps -eo cmd,fname,pid,pcpu,time --sort=-pcpu | head -n 11 && echo && ps -eo cmd,fname,pid,pmem,rss --sort=-rss | head -n 9'
-# see http://www.cl.cam.ac.uk/~mgk25/unicode.html#term for details
-alias term2iso="echo 'Setting terminal to iso mode' ; print -n '\e%@'"
-alias term2utf="echo 'Setting terminal to utf-8 mode'; print -n '\e%G'"
 
 alias magnet2torrent="aria2c -q --bt-metadata-only --bt-save-metadata"
 
@@ -76,21 +86,8 @@ alias grep="grep --color=auto"
 
 alias mutt="dtach -A ${HOME}/.mutt/mutt.session mutt"
 
-alias u="sudo umount"
-function do_sudo() { 
-    integer glob=1
-    while (($#)); do
-        case $1 in
-            command|exec|-) shift; break;;
-            nocorrect) shift; continue;;
-            noglob) glob=0; shift; continue;;
-            *) break;;
-        esac
-    done
-    (($# == 0)) && 1=zsh
-    if ((glob)); then command sudo $~==*; else command sudo $==*; fi
-}
-alias s="noglob do_sudo "
+alias u="umount"
+alias s="sudo"
 alias e="open"
 alias rd="rmdir"
 
@@ -102,20 +99,6 @@ alias ple='perl -wlne'
 alias ftp="lftp"
 alias now="date +'[%H:%M] %A %e %B %G'"
 alias mv="mv -vi"
-alias mvv="\mv"
-
-sudo_list=(mount umount chmod chown) 
-for i in ${sudo_list[@]}; alias ${i}="sudo ${i}";
-
-if [[ -x /usr/bin/systemctl ]]; then
-    alias reboot="sudo systemctl reboot"
-    alias poweroff="sudo systemctl poweroff"
-    alias halt="sudo systemctl halt"
-else
-    alias reboot="sudo reboot"
-    alias halt="sudo halt"
-    alias poweroff="sudo poweroff"
-fi
 
 alias tree="tree --dirsfirst -C"
 alias acpi="acpi -V"
@@ -145,26 +128,25 @@ alias hi='_v'
 
 alias wine="LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine"
 
-alias ym="${HOME}/bin/scripts/yandex.mount > /dev/null"
-alias td='[ -z $(pidof transmission-daemon) ] && transmission-daemon'
+alias ym="${BIN_HOME}/scripts/yandex.mount > /dev/null"
+alias td="[ -z $(pidof transmission-daemon) ] && transmission-daemon"
 
 alias awk="$(whence gawk || whence awk)"
 alias history='history 0'
 alias sniff='sudo ngrep -d "enp6s0" -t "^(GET|POST) " "tcp and port 80"'
 
-alias wd="${HOME}/bin/wd.sh"
-
+alias wd="${BIN_HOME}/wd.sh"
 alias pastebinit='pastebinit -a "Neg" -b "http://paste2.org" -t "Neg is here"'
 
 alias objdump='objdump -M intel -d'
 alias glog="git log --graph --pretty=format:'%Cgreen%h%Creset -%C(yellow)%d%Creset %s %Cred(%cr)%Creset%C(yellow)<%an>'"
 alias memgrind='valgrind --tool=memcheck "$@" --leak-check=full'
 
-alias cal='~/bin/scripts/dzen-time-date'
-alias lk="[[ -x `which glances`  ]] && glances"
+alias cal="${BIN_HOME}/scripts/dzen-time-date"
+alias lk="{[[ -x $(which glances)  ]] && glances} || htop || top"
 
 function resolve_file {
-  if [ -f "$1" ]; then
+  if [[ -f "$1" ]]; then
     echo $(readlink -f "$1")
   elif [[ "${1#/}" == "$1" ]]; then
     echo "$(pwd)/$1"
@@ -174,18 +156,19 @@ function resolve_file {
 }
 
 function ta {
-    tmp_list=/tmp/torr_list
-    sleep .2s
-    for i in $@; echo $i >> $tmp_list
+    local tmp_list=/tmp/torr_list_$$
+    local torrent_dir=${HOME}/torrent
+    local torrent_handler=transmission-remote-cli
+    for i in $@; echo $i >> ${tmp_list}
         while read line; do
-            file_name="$(resolve_file $line)"
-            base_name="$(basename $file_name)"
-            \mv $file_name ~/torrent/$base_name && \
-            transmission-remote-cli ~/torrent/$base_name > /dev/null &&
-                echo "$fg[blue][$fg[white]>>$fg[blue]] -> $fg[white] $base_name $fg[blue]added $fg[green]"
-        done < $tmp_list
-    rm $tmp_list
-    file_name=
+            local file_name="$(resolve_file ${line})"
+            local base_name="$(basename ${file_name})"
+            command mv ${file_name} ${torrent_dir}/${base_name} && \
+            ${torrent_handler} ${torrent_dir}/${base_name} > /dev/null &&
+            echo "$fg[blue][$fg[white]>>$fg[blue]] -> $fg[white] ${base_name} $fg[blue]added $fg[green]"
+        done < ${tmp_list}
+    rm ${tmp_list}
+    unset file_name tmp_list
 }
 
 function w7run {
@@ -202,7 +185,7 @@ function w7run {
     -chardev spicevmc,id=spicechannel0,name=vdagent \
     -qmp unix:${HOME}/1st_level/qmp.socket,server --monitor stdio \
     -boot d
-    # ${HOME}/bin/scripts/qmp/qmp-shell ${HOME}/1st_level/qmp.socket
+    ${BIN_HOME}/scripts/qmp/qmp-shell ${HOME}/1st_level/qmp.socket
 }
 
 user_commands=(
@@ -214,8 +197,8 @@ sudo_commands=(
   reset-failed enable disable reenable preset mask unmask
   link load cancel set-environment unset-environment)
 
-for c in $user_commands; do; alias sc-$c="systemctl $c"; done
-for c in $sudo_commands; do; alias sc-$c="sudo systemctl $c"; done
+for c in ${user_commands}; do; alias sc-${c}="systemctl ${c}"; done
+for c in ${sudo_commands}; do; alias sc-${c}="sudo systemctl ${c}"; done
 
 if [ -z "\${which tree}" ]; then
   tree () {
@@ -227,8 +210,9 @@ fi
 d0() { find "$(retval $1)" -type f -size 0 -exec rm -rf {} \; }
 
 alias cpv="rsync -poghb --backup-dir=/tmp/rsync -e /dev/null --progress --"
+alias unison="unison -log=false -auto -ui=text -times"
 
-g() {
+function g() {
   if [[ $# > 0 ]]; then
     git $@
   else
