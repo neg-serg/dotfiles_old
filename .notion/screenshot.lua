82e3ed85eaf05537e5f49b07eb7e56849b811af8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-11-02 11:47:59 +0300

    [notion] fix screenshot notification
    Former-commit-id: dd5abcb827974a9e8ac740b7ff8de03f98b0db37 [formerly 7564ec99ebcd053f8ddff2002f6dbb8273662854] [formerly 1bba7305d717e43fbc04b53fa2a04847e0af2dcc [formerly 46999a820f87f9d742a45689c499ac68dcba0061]]
    Former-commit-id: 4eb9077d2fd27e091c2eb052266785d64e11ab47 [formerly 68a9ee794776a2125ef13603c4e4d62cf4635e91]
    Former-commit-id: 1e786f8d42f2fcffd965e936b8e6737f152e60f1

--- .notion/screenshot.lua
+++ .notion/screenshot.lua
@@ -1,20 +1,21 @@
+local prefix = "scr_"
+local extension = ".png"
+local screenshot_path = "/tmp"
+local datetime = ""
+
 function screenshot_filename(postfix)
-   local prefix = "scr_"
-   local datetime = os.date("%Y_%m_%d-%H_%M_%S")
-   -- local home_path = os.getenv("HOME")
-   -- local screenshot_path = home_path .. "/pic/shots"
-   local screenshot_path = "/tmp"
-   local extension = ".png"
+   datetime = os.date("%Y_%m_%d-%H_%M_%S")
    local filename = screenshot_path .. '/' .. prefix .. datetime .. postfix .. extension
    local echo_str = '[ Scr :: {' ..  prefix .. datetime .. postfix .. extension ..'} ]'
    local fd = io.popen("zsh -c \"echo '".. filename .."'|xsel -i\"") fd:close()
-   local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
    return filename
 end
 
 function make_screenshot(windowname, filename)
    notioncore.exec('/usr/bin/import -window "' .. windowname .. '"' ..
                    ' \'' .. filename .. '\'')
+   local echo_str = '[ Scr :: { scr_' .. datetime .. '.png} ]'
+   local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
 end
 
 function make_root_screenshot()
