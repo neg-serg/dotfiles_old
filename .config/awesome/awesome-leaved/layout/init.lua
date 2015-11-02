09b4cb9d74f9106ad37e1b7a6c166ccc1b9981aa
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-11-02 13:54:25 +0300

    remove awesome wm configs

--- .config/awesome/awesome-leaved/layout/init.lua
+++
@@ -1,6 +0,0 @@
---import all builders as separate layouts into suit
-local utils = require "awesome-leaved.utils"
-local layout = require "awesome-leaved.layout.layout"
-layout.suit = require "awesome-leaved.layout.suit"
-
-return layout
