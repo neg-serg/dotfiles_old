85622e72ec151138dc3e2c922f3c5a5846648996
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-11-10 02:22:16 +0300

    [zsh] delete dirjumps rehash-hook gpg-agent and title
    Former-commit-id: d00a18fd6330738669199f468fa40fc3ee27069e [formerly 774c6f1a3674ca8ea717ade19d8bc6d04f022b27] [formerly abbebc0dcecc078b1a78d6fe78539d5b728c7c1b [formerly 3c5797b817b2421fdfcc90cdfdea7b04add728c9]]
    Former-commit-id: e904d049934493f63ea4368c7eb742eef2600c20 [formerly e939d9e859bd636babb5f98cb286745e2b7102bb]
    Former-commit-id: d5165aeeb75492bc3a6f766b4395f35908be3474

--- .zsh/14-dirjumps.zsh
+++
@@ -1,13 +0,0 @@
-jump_dirs=( 
-                ~/1st_level  
-                ~/dw         
-                ~/dev        
-                ~/pic        
-                ~/vid        
-                ~/trash      
-            )
-
-for index in $(seq 1 $((${#jump_dirs[@]} )))
-do
-    bindkey -s "${index}" "cd ${jump_dirs[$index]}"
-done
