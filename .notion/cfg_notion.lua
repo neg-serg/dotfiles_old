1f40ca33cdfc77390266ae4adfdbfe24b46883b3
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-11-10 02:17:58 +0300

    [notion] add experimental stuff that should be added to dzen2, better dzen2 configuration

--- .notion/cfg_notion.lua
+++ .notion/cfg_notion.lua
@@ -1,5 +1,6 @@
 notioncore.set{
     dblclick_delay=250,
+    
     notioncore.defshortening("(.*)(<[0-9]+>)", "$1$2$|$1$<...$2"),
     notioncore.defshortening("[^:]+: (.*)(<[0-9]+>)", "$1$2$|$1$<...$2"),
     notioncore.defshortening("[^:]+: (.*)", "$1$|$1$<..."),
@@ -7,6 +8,7 @@ notioncore.set{
     notioncore.defshortening("(.*) - Mozilla(<[0-9]+>)", "$1$2$|$1$<...$2"),
     notioncore.defshortening("(.*) - Mozilla", "$1$|$1$<..."),
     notioncore.defshortening("XMMS - (.*)", "$1$|...$>$1"),
+
     ignore_net_active_window=false,
     float_placement_padding=2,
     warp=false,
