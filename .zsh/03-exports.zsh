8e0efba16ca8c161a65190fa75f8fbe47a1e3c7c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-12-24 14:38:50 +0300

    [zsh] suppress java env messages

--- .zsh/03-exports.zsh
+++ .zsh/03-exports.zsh
@@ -119,6 +119,8 @@ export OSSLIBDIR=/usr/lib/oss
 export JAVA_FONTS=/usr/share/fonts/TTF
 export _JAVA_AWT_WM_NONREPARENTING=1
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+_SILENT_JAVA_OPTIONS="$_JAVA_OPTIONS"
+unset _JAVA_OPTIONS
 
 export FZF_DEFAULT_COMMAND='find'
 
