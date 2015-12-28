40ea8f55a39756617e6cb16fb4dcf97343f2000c
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-12-28 09:47:57 +0300

    [scripts] do not change wallpaper
    Former-commit-id: a27c485d044752f811c34b62aa7dc64e3d51c7c4 [formerly eadfd16fb9e5181dec0134100ed10686183b6b3a] [formerly 1147bc2bab827b88bf9cc22141d4c23aac6b5ecb [formerly fc1ff4406e284550718a677e11b6b7f4b98bb935]]
    Former-commit-id: b0766ad6ed6ea8758c9c15528f28a74e002713ef [formerly 10550e3defdc6471b7782b840eee7d772df3b1e6]
    Former-commit-id: 7be5729af2a4dda80bc067daaa70f93e96dfccfc

--- bin/scripts/wall_set.sh
+++ bin/scripts/wall_set.sh
@@ -3,12 +3,12 @@
 shopt -s nullglob
 cd /home/neg/pic/wl
 
-while true; do
-	files=()
-	for i in *.{jpg,png}; do
-		[[ -f $i ]] && files+=("$i")
-	done
-	range=${#files[@]}
-	((range)) && hsetroot -fill "${files[RANDOM % range]}"
-	sleep 120m
+# while true; do
+files=()
+for i in *.{jpg,png}; do
+    [[ -f $i ]] && files+=("$i")
 done
+range=${#files[@]}
+((range)) && hsetroot -fill "${files[RANDOM % range]}"
+	# sleep 120m
+# done
