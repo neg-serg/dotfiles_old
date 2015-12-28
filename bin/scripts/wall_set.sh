7d562d69c12459da7e00bee4a393fa4997564a36
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-12-28 07:27:23 +0300

    [scripts] add wallpaper setter
    Former-commit-id: ae9526484bd016b1ac1ad2a0281cfa2188a03e5a [formerly 94c28d05666725172fce4f263e0243c0ff04ac37] [formerly 44f3eabc5f9ab4d452b72bb8fa22fdaaef412a14 [formerly 20d12d2d2ee3ce449c58ac341d091f21918d1889]]
    Former-commit-id: 1e530ac210c0c210a90d353e4e723502ecbba3f7 [formerly 68c58825f9bf5583a7e0291d2923f27e3969cfea]
    Former-commit-id: 88a976b1fc0a18823f4e48e437a6bf6eddd26ca7

---
+++ bin/scripts/wall_set.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+shopt -s nullglob
+cd /home/neg/pic/wl
+
+while true; do
+	files=()
+	for i in *.{jpg,png}; do
+		[[ -f $i ]] && files+=("$i")
+	done
+	range=${#files[@]}
+	((range)) && hsetroot -fill "${files[RANDOM % range]}"
+	sleep 120m
+done
