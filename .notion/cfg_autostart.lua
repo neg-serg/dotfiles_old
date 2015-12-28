8fd250e376b62457d58571d1cac37138e9fdf6a8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-12-28 06:07:07 +0300

    [notion] better rofi autostart

--- .notion/cfg_autostart.lua
+++ .notion/cfg_autostart.lua
@@ -11,7 +11,14 @@ function start_mpdstat()
 end
 
 function start_rofi()
-    notioncore.exec('/usr/bin/rofi ' .. rofi.font ..  ' -b -disable-history -columns 10 -lines 1 -width '..rofi.width..' -yoffset -22 -key-run Alt-grave -location 6')
+    local colors = ' -fg '..neg.rofi.fg..' -bg '..neg.rofi.bg..' -hlfg '..neg.rofi.hlfg..' -hlbg '..neg.rofi.hlbg..' -bc '..neg.rofi.bc
+    notioncore.exec('/usr/bin/rofi ' .. rofi.font ..  
+                                        colors .. 
+                                        ' -b -disable-history -columns 10 -lines 1 -width '
+                                        ..rofi.width..
+                                        ' -yoffset -22 -key-run Alt-grave -location 6'
+                                        .. rofi.pid
+                   )
 end
 
 start_rofi()
