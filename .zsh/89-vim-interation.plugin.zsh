6c5a4c34d4608be8116eb5f92dcc054584e4ec63
Sergey Miroshnichenko <serg.zorg@gmail.com>
2015-12-28 05:11:46 +0300

    [zsh] fix new file case and add command sender
    Former-commit-id: 81008fca52ec94117fcd67d7fb73bb95566ef51e [formerly 911a1844088c7d03a17dd43f9d8fb6cb812d452a] [formerly c6fcf502487bf66315fe9d9a8462e65d4ee33ca5 [formerly 73cfc1cdeed6726846e65a4359c225a880ed9aee]]
    Former-commit-id: 341a9f7248bb20135f210a3c751f49c09cc95b4f [formerly 109be13ca46f1d2cc02c2c968644556e44b8a4c3]
    Former-commit-id: 63cdcbe6e472d3d84bc847171ef8a587b4cf6032

--- .zsh/89-vim-interation.plugin.zsh
+++ .zsh/89-vim-interation.plugin.zsh
@@ -1,8 +1,10 @@
+readonly to_normal="<C-\><C-N>:call<SPACE>foreground()<CR>"
+
 function vim_file_open() (
     local file_name="$(resolve_file ${line})"
     file_name=$(bash -c "printf %q '${file_name}'")
     { vim --servername ${vim_server_name} --remote-send "${to_normal}:silent edit ${file_name}<CR>" 2>/dev/null \
-        || { while [[ $(vim --servername VIM --remote-expr "g:vim_is_started") != "on" ]]; do
+        || { while [[ $(vim --servername VIM --remote-expr "g:vim_is_started" 2>/dev/null) != "on" ]]; do
             sleep "0.1s"
         done \
         && vim --servername ${vim_server_name} --remote-send "${to_normal}:silent edit ${file_name}<CR>" 2>/dev/null } } && {
@@ -18,12 +20,12 @@ function vim_file_open() (
         local decoration="$fg[green]‒$fg[white]"
         local tmp_name="$(echo ${file_name}|sed "s|^${HOME}|$fg[green]~|;s|/|$fg[blue]&$fg[white]|g")"
         local fancy_name="${decoration} $fg[white]${tmp_name} ${decoration}"
-        if [[ -f "${file_name}" ]] && [[ ! -d "${file_name}" ]]; then
+        if [[ ! -e "${file_name}" ]]; then
+            echo "${pref} ${fancy_name} ${msg_delim} ${new_file_msg}"
+        elif [[ -f "${file_name}" ]] && [[ ! -d "${file_name}" ]]; then
             echo "${pref} ${fancy_name} ${msg_delim} ${sz_msg} ${msg_delim} ${len_msg}${syn_msg}"
         else
-            if [[ -z "${file_name}"  ]]; then
-                echo "${pref} ${fancy_name} ${msg_delim} ${new_file_msg}"
-            elif [[ -d "${file_name}" ]]; then
+            if [[ -d "${file_name}" ]]; then
                 if [[ $(readlink -f ${file_name}) == $(pwd) ]]; then
                     local spec_fancy_name="${decoration} $fg[white]current dir ${decoration}"
                     echo "${pref} ${spec_fancy_name} ${msg_delim} ${dir_msg}"
@@ -40,23 +42,36 @@ function process_list() {
     notionflux -e "app.byclass('', 'wim')" > /dev/null
     sleep "$1"; shift
     for line; do vim_file_open; done
-    # local cmd="${to_normal}${before}${after}"
-    # vim --servername ${vim_server_name} --remote-send "${cmd}"
 }
 
 function v {
     local wid=$(xdotool search --classname wim)
-    
-    # readonly to_normal="--remote-send <C-\><C-N>:call<SPACE>foreground()<CR>"
-    readonly to_normal="<C-\><C-N>:call<SPACE>foreground()<CR>"
+    local mfiles=""
+    for f; do
+        f="$(resolve_file ${f})"
+        mfiles="${mfiles} $(bash -c "printf %q '${f}'")"
+    done
+    [[ -n ${mfiles} ]] && mfiles=':args! '"${mfiles}<CR>"
+    if [[ -z "${wid}" ]]; then
+        st -f "${wim_font}:pixelsize=${wim_font_size}" -c 'wim' -e bash -c "tmux -S ${wim_sock_path} new -s vim -n vim \"vim --servername ${vim_server_name}\" && \
+            tmux -S ${wim_sock_path} switch-client -t vim" 2>/dev/null &!
+        process_list ".1s" "$@"
+    else  
+        process_list ".1s" "$@"
+    fi
+}
+
+
+function vprocess_list() {
+    notionflux -e "app.byclass('', 'wim')" > /dev/null
+    local cmd="${to_normal}${before}${after}"
+    vim --servername ${vim_server_name} --remote-send "${cmd}"
+}
+
+
+function vv {
+    local wid=$(xdotool search --classname wim)
     while getopts ":b:a:" opt; do
-        #  -b':vsp' -b':sp'
-        #  -b':wincmd k' -b':wincmd j'
-        #  -b':wincmd l' -b':wincmd h'
-        #  -b':sp<cr>:wincmd k'
-        #  -b':sp<cr>:wincmd j'
-        #  -b':vsp<cr>:wincmd h'
-        #  -b':vsp<cr>:wincmd l'
         case ${opt} in
             a) after="${OPTARG}" ;;
             b) before="${before}${OPTARG}" ;;
@@ -71,12 +86,11 @@ function v {
         mfiles="${mfiles} $(bash -c "printf %q '${f}'")"
     done
     [[ -n ${mfiles} ]] && mfiles=':args! '"${mfiles}<CR>"
-    #----------------------------------------------------------------
     if [[ -z "${wid}" ]]; then
         st -f "${wim_font}:pixelsize=${wim_font_size}" -c 'wim' -e bash -c "tmux -S ${wim_sock_path} new -s vim -n vim \"vim --servername ${vim_server_name}\" && \
-            tmux -S ${wim_sock_path} switch-client -t vim" &!
-        process_list ".1s" "$@"
+            tmux -S ${wim_sock_path} switch-client -t vim" 2>/dev/null &!
+        vprocess_list ".1s" "$@"
     else  
-        process_list ".1s" "$@"
+        vprocess_list ".1s" "$@"
     fi
 }
