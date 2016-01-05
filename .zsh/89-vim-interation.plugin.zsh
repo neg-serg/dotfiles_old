8fcd38cf167070026987029f96a8850aa134ccd8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-05 16:02:17 +0300

    [zsh] simplify vim-interaction code

--- .zsh/89-vim-interation.plugin.zsh
+++ .zsh/89-vim-interation.plugin.zsh
@@ -9,9 +9,9 @@ function wim_run(){
     if [[ -z "${wid}" ]]; then
         st -f "${wim_font}:pixelsize=${wim_font_size}" -c 'wim' -e bash -c "tmux -S ${wim_sock_path} new -s vim -n vim \"vim --servername ${vim_server_name}\" && \
             tmux -S ${wim_sock_path} switch-client -t vim" 2>/dev/null &!
-        eval ${proc} ".1s" "$@"
+        eval ${proc} ${wim_timer} "$@"
     else  
-        eval ${proc} ".1s" "$@"
+        eval ${proc} ${wim_timer} "$@"
     fi
 }
 
@@ -29,32 +29,27 @@ function vim_file_open() (
     file_name=$(bash -c "printf %q '${file_name}'")
     { vim --servername ${vim_server_name} --remote-send "${to_normal}:silent edit ${file_name}<CR>" 2>/dev/null \
         || { while [[ $(vim --servername ${vim_server_name} --remote-expr "g:vim_is_started" 2>/dev/null) != "on" ]]; do
-            sleep ".1s"
+            sleep ${wim_timer}
         done \
         && vim --servername ${vim_server_name} --remote-send "${to_normal}:silent edit ${file_name}<CR>" 2>/dev/null } } && {
-        local FG237="[38;5;237m"
-        local file_size=$(stat -c%s "$file_name" 2>/dev/null| numfmt --to=iec-i --suffix=B|sed "s/\([KMGT]iB\|B\)/$fg[green]&/")
-        local file_length="$(wc -l $file_name 2>/dev/null|grep -owE '[0-9]* '|tr -d ' ')"
-        local sz_msg=$(_zsh_wrap "sz${FG237}~$fg[white]$file_size")
-        local len_msg=$(_zsh_wrap "len${FG237}=$fg[white]$file_length")
+        local file_size=$(stat -c%s "${file_name}" 2>/dev/null| numfmt --to=iec-i --suffix=B|sed "s/\([KMGT]iB\|B\)/$fg[green]&/")
+        local file_length="$(wc -l ${file_name} 2>/dev/null|grep -owE '[0-9]* '|tr -d ' ')"
+        local sz_msg=$(_zsh_wrap "sz$(_zfg 237)~$fg[white]${file_size}")
+        local len_msg=$(_zsh_wrap "len$(_zfg 237)=$fg[white]${file_length}")
         local new_file_msg=$(_zsh_wrap new_file)
         local dir_msg=$(_zsh_wrap directory)
-        local msg_delim="[38;5;24m::"
         local pref=$(_zsh_wrap ">>")
-        local decoration="$fg[green]‒$fg[white]"
-        local tmp_name="$(echo ${file_name}|sed "s|^${HOME}|$fg[green]~|;s|/|$fg[blue]&$fg[white]|g")"
-        local fancy_name="${decoration} $fg[white]${tmp_name} ${decoration}"
         if [[ ! -e "${file_name}" ]]; then
-            echo "${pref} ${fancy_name} ${msg_delim} ${new_file_msg}"
+            <<< "${pref} $(_zsh_filename_wrap ${file_name}) $(_zdelim) ${new_file_msg}"
         elif [[ -f "${file_name}" ]] && [[ ! -d "${file_name}" ]]; then
-            echo "${pref} ${fancy_name} ${msg_delim} ${sz_msg} ${msg_delim} ${len_msg}${syn_msg}"
+            <<< "${pref} $(_zsh_filename_wrap ${file_name}) $(_zdelim) ${sz_msg} $(_zdelim) ${len_msg}${syn_msg}"
         else
             if [[ -d "${file_name}" ]]; then
                 if [[ $(readlink -f ${file_name}) == $(readlink -f $(pwd)) ]]; then
                     local spec_fancy_name="${decoration} $fg[white]current dir ${decoration}"
-                    echo "${pref} ${spec_fancy_name} ${msg_delim} ${dir_msg}"
+                    <<< "${pref} ${spec_fancy_name} ${msg_delim} ${dir_msg}"
                 else
-                    echo "${pref} ${fancy_name} ${msg_delim} ${dir_msg}"
+                    <<< "${pref} ${fancy_name} ${msg_delim} ${dir_msg}"
                 fi
             fi
         fi
