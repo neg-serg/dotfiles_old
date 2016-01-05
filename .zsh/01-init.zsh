ffe488aa525f1042c5c45126fc7148fd52c3fb1e
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-05 17:09:49 +0300

    [zsh] fix control-c bug
    Former-commit-id: 30c503707f6805c4c432096576d3bd4407f2ec21 [formerly 4908f46fb2517a4461f244ed44f9cdbead2af237] [formerly 286926b913191e05cdd63dfe10123fd474dbb274 [formerly 59e46bcb48e293daabc1743257ecedaa63477d9c]]
    Former-commit-id: e083e900f55b7e52b60aa0cb01ef8d217fd4ff46 [formerly 0dbcd6b033c13d76889e98fba1c21721f73e0daa]
    Former-commit-id: 122e28cdf550a933c6f6e3b4325926337ae3a8de

--- .zsh/01-init.zsh
+++ .zsh/01-init.zsh
@@ -67,20 +67,20 @@ function stty_setup(){
     stty eof  2> /dev/null  # stty eof ''
     stty ixany
     stty ixoff -ixon # Disable XON/XOFF flow control; this is required to make C-s work in Vim.
-    stty intr "^C" 2> /dev/null
-    stty erase "^?" 2> /dev/null
-    stty start "undef" 2> /dev/null
-    stty stop "undef" 2> /dev/null
-    stty susp "^Z" 2> /dev/null
-    stty rprnt "^R" 2> /dev/null
-    stty werase "^W" 2> /dev/null
-    stty lnext "^B" 2> /dev/null
-    stty flush "undef" 2> /dev/null
-    #stty eol "undef" 2> /dev/null
-    #stty eol2 "undef" 2> /dev/null
-    #stty swtch "undef" 2> /dev/null
-    #stty kill "undef" 2> /dev/null
-    #stty quit "undef" 2> /dev/null
+    #stty intr "^C" 2> /dev/null
+    #stty erase "^?" 2> /dev/null
+    #stty start "undef" 2> /dev/null
+    #stty stop "undef" 2> /dev/null
+    #stty susp "^Z" 2> /dev/null
+    #stty rprnt "^R" 2> /dev/null
+    #stty werase "^W" 2> /dev/null
+    #stty lnext "^B" 2> /dev/null
+    #stty flush "undef" 2> /dev/null
+    ##stty eol "undef" 2> /dev/null
+    ##stty eol2 "undef" 2> /dev/null
+    ##stty swtch "undef" 2> /dev/null
+    ##stty kill "undef" 2> /dev/null
+    ##stty quit "undef" 2> /dev/null
     stty time 0 2> /dev/null
     stty min 0 2> /dev/null
     stty line 6 2> /dev/null
