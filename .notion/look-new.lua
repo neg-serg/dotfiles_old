638fb8593fc42b7763647ff512298cc19470d709
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-06 11:46:19 +0300

    [notion] create dzen2 symb tables

--- .notion/look-new.lua
+++ .notion/look-new.lua
@@ -1,26 +1,173 @@
-neg = {}
+neg      = {}
 neg.dzen = {}
 neg.rofi = {}
 
-neg.basefont = "xft:PragmataPro for Powerline-12:bold"
+neg.basefont   = "xft:PragmataPro for Powerline-12:bold"
 neg.statusfont = "xft:PragmataPro for Powerline-12:bold"
 
-neg.padding_color="#000000"
-neg.def_bg="#000000"
-neg.def_fg="#666666"
-neg.scratchpad_border="#1F3B4F"
-neg.frame_def_border="#000000"
+neg.padding_color     = "#000000"
+neg.def_bg            = "#000000"
+neg.def_fg            = "#666666"
+neg.scratchpad_border = "#1F3B4F"
+neg.frame_def_border  = "#000000"
 
-neg.rofi.bg="\"#000000\""
-neg.rofi.fg="\"#666666\""
-neg.rofi.hlfg="\"#aaaaaa\""
-neg.rofi.hlbg="\"#194558\""
-neg.rofi.bc="\"#202020\""
+neg.rofi.bg   = "\"#000000\""
+neg.rofi.fg   = "\"#666666\""
+neg.rofi.hlfg = "\"#aaaaaa\""
+neg.rofi.hlbg = "\"#194558\""
+neg.rofi.bc   = "\"#202020\""
 
-neg.dzen.panel_font_= "'PragmataPro for Powerline:style=bold:size=12'"
-neg.dzen.bg_='"#000000"'
-neg.dzen.h_=19
-neg.dzen.main_w_="1000"
+neg.dzen.panel_font_ = "'PragmataPro for Powerline:style = bold:size = 12'"
+neg.dzen.bg_         = '"#000000"'
+neg.dzen.h_          = 19
+neg.dzen.main_w_     = "1000"
+
+neg.dzen.fancy_table1 = {
+    "",  "",
+    "",  "",
+    " data", " gtd",
+    " gtd", " code",
+    " text", " play",
+    " pics", " game",
+    " adm", " news",
+    "         "
+
+}
+
+neg.dzen.fancy_table2 = {"", "", "", "", "", "", "", "", "", ""}
+neg.dzen.powersym = {
+    right         = "", -- \xe0b0
+    left          = "", -- \xe0b2
+    right_bracket = "", -- \xe0b1
+    left_bracket  = "", -- \xe0b3
+    branch        = "", -- \xe0a0
+    lock          = "" -- \xe0a2
+}
+
+--  font = "Ionicons" -- http://ionicons.com/
+neg.dzen.ioniconssym = {
+    battery_charge = "\xf111",
+    battery_low    = "\xf112",
+    battery_mid    = "\xf115",
+    battery_high   = "\xf114",
+    battery_full   = "\xf113",
+    volume_high    = "\xf257",
+    volume_low     = "\xf258",
+    volume_medium  = "\xf259",
+    volume_mute    = "\xf25a",
+    close          = "\xf128"
+}
+
+--  font = "octicons" -- https://octicons.github.com/
+neg.dzen.octiconsym = {
+    bug           = "\xf091",
+    browser       = "\xf0c5",
+    question      = "\xf02c",
+    mirror        = "\xf024",
+    unfold        = "\xf039",
+    screen_full   = "\xf066",
+    screen_normal = "\xf067",
+    puzzle        = "\xf0c0",
+    rocket        = "\xf033",
+    light_bulb    = "\xf000",
+    dashboard     = "\xf07d",
+    mail          = "\xf03b",
+    mail_read     = "\xf03c"
+}
+
+--  font = "Typicons" -- http://typicons.con (battery)
+neg.dzen.typiconssym = {
+    typcn_battery_charge = "\xe02a",
+    typcn_battery_low    = "\xe02d",
+    typcn_battery_mid    = "\xe02e",
+    typcn_battery_high   = "\xe02c",
+    typcn_battery_full   = "\xe02b",
+    typcn_volume_down    = "\xe132",
+    typcn_volume_mute    = "\xe133",
+    typcn_volume_up      = "\xe134",
+    typcn_volume         = "\xe135",
+    typcn_flag           = "\xe074",
+    typcn_plug           = "\xe0cd",
+    typcn_time           = "\xe120"
+}
+
+--  font = "Climacons\\-Font" -- http://adamwhitcroft.com/climacons
+neg.dzen.climaconssym = {
+     climacon_cloud                     = "\xe000",
+     climacon_cloud_sun                 = "\xe001",
+     climacon_cloud_moon                = "\xe002",
+     climacon_rain                      = "\xe003",
+     climacon_rain_sun                  = "\xe004",
+     climacon_rain_moon                 = "\xe005",
+     climacon_showers                   = "\xe006",
+     climacon_showers_sun               = "\xe007",
+     climacon_showers_moon              = "\xe008",
+     climacon_downpour                  = "\xe009",
+     climacon_downpour_sun              = "\xe00a",
+     climacon_downpour_moon             = "\xe00b",
+     climacon_drizzle                   = "\xe00c",
+     climacon_drizzle_sun               = "\xe00d",
+     climacon_drizzle_moon              = "\xe00e",
+     climacon_sleet                     = "\xe00f",
+     climacon_sleet_sun                 = "\xe010",
+     climacon_sleet_moon                = "\xe011",
+     climacon_hail                      = "\xe012",
+     climacon_hail_sun                  = "\xe013",
+     climacon_hail_moon                 = "\xe014",
+     climacon_flurries                  = "\xe015",
+     climacon_flurries_sun              = "\xe016",
+     climacon_flurries_moon             = "\xe017",
+     climacon_snow                      = "\xe018",
+     climacon_snow_sun                  = "\xe019",
+     climacon_snow_moon                 = "\xe01a",
+     climacon_fog                       = "\xe01b",
+     climacon_fog_sun                   = "\xe01c",
+     climacon_fog_moon                  = "\xe01d",
+     climacon_haze                      = "\xe01e",
+     climacon_haze_sun                  = "\xe01f",
+     climacon_haze_moon                 = "\xe020",
+     climacon_wind                      = "\xe021",
+     climacon_wind_cloud                = "\xe022",
+     climacon_wind_sun                  = "\xe023",
+     climacon_wind_moon                 = "\xe024",
+     climacon_lightning                 = "\xe025",
+     climacon_lightning_sun             = "\xe026",
+     climacon_lightning_moon            = "\xe027",
+     climacon_sun                       = "\xe028",
+     climacon_sunset                    = "\xe029",
+     climacon_sunrise                   = "\xe02a",
+     climacon_sun_low                   = "\xe02b",
+     climacon_sun_lower                 = "\xe02c",
+     climacon_moon                      = "\xe02d",
+     climacon_moon_new                  = "\xe02e",
+     climacon_moon_first_crescent       = "\xe02f",
+     climacon_moon_first_half           = "\xe030",
+     climacon_moon_first_three_quarter  = "\xe031",
+     climacon_moon_full                 = "\xe032",
+     climacon_moon_last_three_quarter   = "\xe033",
+     climacon_moon_last_half            = "\xe034",
+     climacon_moon_last_crescent        = "\xe035",
+     climacon_snowflake                 = "\xe036",
+     climacon_tornado                   = "\xe037",
+     climacon_thermometer_empty         = "\xe038",
+     climacon_thermometer_low           = "\xe039",
+     climacon_thermometer_medium_low    = "\xe03a",
+     climacon_thermometer_medium_high   = "\xe03b",
+     climacon_thermometer_high          = "\xe03c",
+     climacon_thermometer_full          = "\xe03d",
+     climacon_celcius                   = "\xe03e",
+     climacon_farenheit                 = "\xe03f",
+     climacon_compass                   = "\xe040",
+     climacon_compass_north             = "\xe041",
+     climacon_compass_east              = "\xe042",
+     climacon_compass_south             = "\xe043",
+     climacon_compass_west              = "\xe044",
+     climacon_umbrella                  = "\xe045",
+     climacon_sunglasses                = "\xe046",
+     climacon_cloud_refresh             = "\xe047",
+     climacon_cloud_down                = "\xe048",
+     climacon_cloud_up                  = "\xe049"
+}
 
 if not gr.select_engine("de") then return end
 de.reset()
