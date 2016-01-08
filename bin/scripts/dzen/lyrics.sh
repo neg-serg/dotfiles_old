12a090c7a603ca5c7ca202539f8643a3b994a805
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-08 12:22:04 +0300

    [scripts/dzen] refactoring lyrics and music scripts

--- bin/scripts/dzen/lyrics.sh
+++ bin/scripts/dzen/lyrics.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-source $(dirname $0)/config.sh
-XPOS=$((950 + $XOFFSET))
-WIDTH="400"
+source ./config.sh
+xpos=$[950 + ${xoffset}]
+width="400"
 
 url="http://makeitpersonal.co/lyrics?artist=$(mpc current -f %artist% | sed 's/ /%20/g')&title=$(mpc current -f %title% | sed 's/ /%20/g')"
 lyrics=$(curl -s "$url" | fold -sw 60)
 lyricslines=$(curl -s "$url" | fold -sw 60 | wc -l)
-LINES=$(( $lyricslines + 3 ))
+lines=$[${lyricslines} + 3]
 
-if [[ $LINES -gt 60 ]]; then
-	LINES=60
-fi
+[[ ${lines} -gt 60 ]] && lines=60
 
-(echo "^fg($highlight)Music Info "; echo "^fg($highlight) Lyrics"; echo "$lyrics"; echo " "; sleep 15) | dzen2 -fg $foreground -bg $background -fn "$FONT" -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button3=exit;button4=scrollup;button5=scrolldown;'
+(echo "^fg(${hi})Music Info "; echo "^fg(${hi}) Lyrics"; echo "${lyrics}"; echo " "; sleep 15) | dzen2 -fg ${fg} -bg ${bg} -fn "${font}" -x ${xpos} -y ${ypos} -w ${width} -l ${lines} -e 'onstart=uncollapse,hide;button1=exit;button3=exit;button4=scrollup;button5=scrolldown;'
--- bin/scripts/dzen/music.sh
+++ bin/scripts/dzen/music.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-source $(dirname $0)/config.sh
-XPOS=$((1180 + $XOFFSET))
-WIDTH="380"
-LINES="8"
+source ./config.sh
+xpos=$((1180 + ${xoffset}))
+width="380"
+lines="8"
 
 playing=$(mpc current)
 artist=$(mpc current -f  %artist%)
@@ -17,13 +17,10 @@ date=$(mpc current -f  %date%)
 #prev=$(mpc playlist | sed -n ""$prevnum"p")
 #art=$(ls ~/.config/ario/covers | grep SMALL | grep $album)
 art="${HOME}/.config/ario/covers/$(ls ~/.config/ario/covers | grep -v SMALL | grep "$(mpc current -f %album% | sed 's/:/ /g')")"
-perc=`mpc | awk 'NR == 2 {gsub(/[()%]/,""); print $4}'`
- 
-percbar=`echo -e "$perc" | gdbar -bg $bar_bg -fg $foreground -h 1 -w $(($WIDTH-20))`
+perc=$(mpc | awk 'NR == 2 {gsub(/[()%]/,""); print $4}')
+percbar=$(echo -e "${perc}" | gdbar -bg ${bar_bg} -fg ${foreground} -h 1 -w $[${width}-20])
 
-#84x84
-feh -x -B black -^ "" -g 108x108+$(($XPOS-108))+$(($YPOS+12)) -Z "$art" &
-(echo "^fg($highlight)Music"; echo "       ";echo "^ca(1,${HOME}/bin/scripts/dzen/dzen_lyrics.sh)  Track:  ^fg($highlight)$track^ca()"; echo "^ca(1,/home/sunn/.xmonad/scripts/dzen_artistinfo.sh)^fg()  Artist: ^fg($highlight)$artist^ca()";echo "^ca(1,/home/sunn/.xmonad/scripts/dzen_albuminfo.sh)^fg()  Album:  ^fg($highlight)$album^ca()"; echo "^ca(1,/home/sunn/.xmonad/scripts/dzen_lyrics.sh)  Year:   ^fg($highlight)$date^ca()"; echo "  $percbar"; echo "      ^ca(1, ncmpcpp prev)  ^fg($white0)^i(/home/sunn/.xmonad/dzen2/prev.xbm) ^ca()  ^ca(1, ncmpcpp pause) ^i(/home/sunn/.xmonad/dzen2/pause.xbm) ^ca()  ^ca(1, ncmpcpp play) ^i(/home/sunn/.xmonad/dzen2/play.xbm) ^ca()   ^ca(1, ncmpcpp next) ^i(/home/sunn/.xmonad/dzen2/next.xbm) ^ca()"; echo " "; sleep 15) | dzen2 -fg $foreground -bg $background -fn "$FONT" -x $XPOS -y $YPOS -w $WIDTH -l $LINES -e 'onstart=uncollapse,hide;button1=exit;button3=exit' & 
-#notionflux -e "named_scratchpad(_, 'feh')"
+feh -x -B black -^ "" -g 108x108+$[${xpos}-108]+$[$ypos+12] -Z "${art}" &
+(echo "^fg(${hi})Music"; echo "       ";echo "^ca(1,${HOME}/bin/scripts/dzen/dzen_lyrics.sh)  Track:  ^fg(${hi})${track}^ca()"; echo "^ca(1,/home/sunn/.xmonad/scripts/dzen_artistinfo.sh)^fg()  Artist: ^fg(${hi})${artist}^ca()";echo "^ca(1,/home/sunn/.xmonad/scripts/dzen_albuminfo.sh)^fg()  Album:  ^fg(${hi})${album}^ca()"; echo "^ca(1,/home/sunn/.xmonad/scripts/dzen_lyrics.sh)  Year:   ^fg(${hi})${date}^ca()"; echo "  ${percbar}"; echo "      ^ca(1, ncmpcpp prev)  ^fg(${white0})^i(/home/sunn/.xmonad/dzen2/prev.xbm) ^ca()  ^ca(1, ncmpcpp pause) ^i(/home/sunn/.xmonad/dzen2/pause.xbm) ^ca()  ^ca(1, ncmpcpp play) ^i(/home/sunn/.xmonad/dzen2/play.xbm) ^ca()   ^ca(1, ncmpcpp next) ^i(/home/sunn/.xmonad/dzen2/next.xbm) ^ca()"; echo " "; sleep 15) | dzen2 -fg ${fg} -bg ${bg} -fn "${font}" -x ${xpos} -y ${ypos} -w ${width} -l ${lines} -e 'onstart=uncollapse,hide;button1=exit;button3=exit' & 
 sleep 15
 killall feh
