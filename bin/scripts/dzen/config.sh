846d155253b100c1b20dbf79e6091cb4481e0f85
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-08 12:21:50 +0300

    [scripts/dzen] update dzen config

--- bin/scripts/dzen/config.sh
+++ bin/scripts/dzen/config.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-background="#000000"
-foreground="#666666"
-highlight="#aaaabb"
+bg="#000000"
+fg="#666666"
+hi="#aaaabb"
 
 #XPOS=$(xdotool getmouselocation | awk -F " " '{print $1}' | cut -d ":" -f 2)
-YPOS="11"
-HEIGHT="12"
-XOFFSET=554
-if [[ -z `xrandr | grep " connected" | grep 'VGA'` ]]; then
-	XOFFSET="0"
+ypos="11"
+height="12"
+xoffset=554
+if [[ -z $(xrandr | grep " connected" | grep 'VGA') ]]; then
+	xoffset="0"
 fi
 
-FONT="PragmataPro for Powerline:style=bold:size=10"
+font="PragmataPro for Powerline:style=bold:size=10"
 white0="#775759"
 
-bar_bg="#1f1f1f"
-bar_fg="#d17b49"
-notify="#d17b49"
-warning="#d17b49"
+bar_bg="#1f1f1f" bar_fg="#d17b49"
+notify="#d17b49" warning="#d17b49"
