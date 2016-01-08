1eb7dda10f3e7dada96a404da3cfdab612ea6439
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-08 11:05:32 +0300

    [vim]  update ycm flags

--- .vim/.ycm_extra_conf.py
+++ .vim/.ycm_extra_conf.py
@@ -35,66 +35,99 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
-'-Wall',
-'-Wextra',
-'-Werror',
-'-Wno-long-long',
-'-Wno-variadic-macros',
-'-fexceptions',
-'-DNDEBUG',
-# You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
-# source code needs it.
-'-DUSE_CLANG_COMPLETER',
-# THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-# language to use when compiling headers. So it will guess. Badly. So C++
-# headers will be compiled as C headers. You don't want that so ALWAYS specify
-# a "-std=<something>".
-# For a C project, you would set this to something like 'c99' instead of
-# 'c++11'.
-'-std=c++11',
-# ...and the same thing goes for the magic -x option which specifies the
-# language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
-# For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-isystem',
-'../BoostParts',
-'-isystem',
-# This path will only work on OS X, but extra paths that don't exist are not
-# harmful
-'/System/Library/Frameworks/Python.framework/Headers',
-'-isystem',
-'../llvm/include',
-'-isystem',
-'../llvm/tools/clang/include',
-'-I',
-'.',
-'-I',
-'./include',
-'../include',
-'-I',
-'./ClangCompleter',
-'-isystem',
-'./tests/gmock/gtest',
-'-isystem',
-'./tests/gmock/gtest/include',
-'-isystem',
-'./tests/gmock',
-'-isystem',
-'./tests/gmock/include',
-'-isystem',
-'/usr/include',
-'-isystem',
-'/usr/local/include',
-'-isystem',
-'-I',
-'/usr/include/c++/4.9.1',
-'-I',
-'/usr/include/c++/5.1.0',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/c++/v1',
-'-isystem',
-'/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+    '-Wall', '-Wextra', '-Werror',
+    '-Wno-long-long', '-Wno-variadic-macros',
+    '-fexceptions', '-DNDEBUG',
+    # You 100% do NOT need -DUSE_CLANG_COMPLETER in your flags; only the YCM
+    # source code needs it.
+    '-DUSE_CLANG_COMPLETER',
+    # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
+    # language to use when compiling headers. So it will guess. Badly. So C++
+    # headers will be compiled as C headers. You don't want that so ALWAYS specify
+    # a "-std=<something>".
+    # For a C project, you would set this to something like 'c99' instead of
+    # 'c++11'.
+    '-std=c++11',
+    # ...and the same thing goes for the magic -x option which specifies the
+    # language that the files to be compiled are written in. This is mostly
+    # relevant for c++ headers.
+    # For a C project, you would set this to 'c' instead of 'c++'.
+    '-x', 'c++',
+    # Qt-support
+    '-DQT_CORE_LIB', '-DQT_GUI_LIB',
+    '-DQT_NETWORK_LIB', '-DQT_QML_LIB',
+    '-DQT_QUICK_LIB', '-DQT_SQL_LIB',
+    '-DQT_WIDGETS_LIB', '-DQT_XML_LIB',
+    '-isystem', '../BoostParts',
+    # This path will only work on OS X, but extra paths that don't exist are not
+    # harmful
+    '-isystem', '/System/Library/Frameworks/Python.framework/Headers',
+    '-isystem', '../llvm/include',
+    '-isystem', '../llvm/tools/clang/include',
+    '-I', '.', '-I', './include', '../include',
+    '-I', './ClangCompleter',
+
+    #GTK includes
+    '-isystem', '/usr/include/gtk-3.0',
+    '-isystem', '/usr/include/glib-2.0',
+    '-isystem', '/usr/include/glib-2.0/glib',
+    '-isystem', '/usr/lib/i386-linux-gnu/glib-2.0/include',
+    '-isystem', '/usr/include/pango-1.0',
+    '-isystem', '/usr/include/cairo',
+    '-isystem', '/usr/include/gdk-pixbuf-2.0',
+    '-isystem', '/usr/include/atk-1.0',
+    '-isystem', '-I/usr/include/gio-unix-2.0',
+    '-isystem', '-I/usr/include/freetype2',
+    '-isystem', '-I/usr/include/pixman-1',
+    '-isystem', '-I/usr/include/libpng12',
+
+    #QT includes
+    '-I', '/usr/lib/qt/mkspecs/linux-clang',
+    '-I', '/usr/include/qt',
+    '-I', '/usr/include/qt/QtConcurrent',
+    '-I', '/usr/include/qt/QtCore',
+    '-I', '/usr/include/qt/QtDBus',
+    '-I', '/usr/include/qt/QtGui',
+    '-I', '/usr/include/qt/QtHelp',
+    '-I', '/usr/include/qt/QtMultimedia',
+    '-I', '/usr/include/qt/QtMultimediaWidgets',
+    '-I', '/usr/include/qt/QtNetwork',
+    '-I', '/usr/include/qt/QtOpenGL',
+    '-I', '/usr/include/qt/QtPlatformSupport',
+    '-I', '/usr/include/qt/QtPositioning',
+    '-I', '/usr/include/qt/QtScript',
+    '-I', '/usr/include/qt/QtScriptTools',
+    '-I', '/usr/include/qt/QtSql',
+    '-I', '/usr/include/qt/QtSvg',
+    '-I', '/usr/include/qt/QtTest',
+    '-I', '/usr/include/qt/QtUiTools',
+    '-I', '/usr/include/qt/QtV8',
+    '-I', '/usr/include/qt/QtWebKit',
+    '-I', '/usr/include/qt/QtWebKitWidgets',
+    '-I', '/usr/include/qt/QtWidgets',
+    '-I', '/usr/include/qt/QtXml',
+    '-I', '/usr/include/qt/QtXmlPatterns',
+    '-I', '/usr/include/boost/spirit'
+
+
+    '-isystem', './tests/gmock/gtest',
+    '-isystem', './tests/gmock/gtest/include',
+    '-isystem', './tests/gmock',
+    '-isystem', './tests/gmock/include',
+    '-isystem', '/usr/include',
+    '-isystem', '/usr/local/include',
+
+    '-isystem', '-I', '/usr/include/c++/4.9.1',
+    '-isystem', '-I', '/usr/include/c++/5.1.0',
+    '-isystem', '-I', '/usr/include/c++/5.3.0',
+
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/../lib/c++/v1',
+    '-isystem',
+    '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include',
+
+    '-I', 'Tests',
+    '-I', 'build',
+    '-I', 'build/Tests',
 ]
 
 
