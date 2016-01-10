d9c18f942673ba93498c82855d96fa0db09e922b
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-11 02:02:31 +0300

    [notion] cleanup autostart

--- .notion/cfg_autostart.lua
+++ .notion/cfg_autostart.lua
@@ -1,15 +1,3 @@
-function start_mpdstat()
-    local fd1 = io.popen("pgrep -f 'dzen2 -dock -bg #000000 -h 19 -tw 0 -x 0 -ta l -w 1000 -p -fn PragmataPro for Powerline:style=bold:size=12'","r")
-    local exists = fd1:read("*l")
-    fd1:close()
-    local fd2 = io.popen("pidof mpd","r")
-    local mpd_run = fd2:read("*l")
-    fd2:close()
-    if (exists ~= nil) and (mpd_run ~= nil) then
-        notioncore.exec('lua ~/.notion/mpd_dzen.lua')
-    end
-end
-
 function start_rofi()
     local colors = ' -fg '..neg.rofi.fg..' -bg '..neg.rofi.bg..' -hlfg '..neg.rofi.hlfg..' -hlbg '..neg.rofi.hlbg..' -bc '..neg.rofi.bc
     notioncore.exec('/usr/bin/rofi ' .. rofi.font ..  
@@ -20,6 +8,4 @@ function start_rofi()
                                         .. rofi.pid
                    )
 end
-
 start_rofi()
-start_mpdstat()
