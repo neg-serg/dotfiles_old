b8bfb4107ddef778f5504560a47d12128bdae837
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-11 02:01:56 +0300

    [notion] fix dzen_bg bugs
    Former-commit-id: a342ee3345a42960e5ec26560e3054ce44eaa201 [formerly a4ee90497fbdf0fca24246e83f844f3664f8b332] [formerly fbb7f707c0c29e2ba29e49b231c8edccbcb0b598 [formerly 8d08ee89d22b05e3b8e8c5f7cd2f6d94455774f3]]
    Former-commit-id: cdcae89da005e996e25464bb3294434b74c7bdaa [formerly 25c15ae1050b6ddba8721de34c7a22781a89c1d8]
    Former-commit-id: b72e2b7d0ad45ecb2dd1a4693865fb0a3745e3a4

--- .notion/dzen_bg.lua
+++ .notion/dzen_bg.lua
@@ -1,24 +1,22 @@
 --------------------------------------------------------------------------------------------------------------------------------------------
-mpd_pipe = io.popen("dzen2 -dock -bg '#000000' -h 19 -tw 0  -x 0 -ta r -w 910 -p -fn 'PragmataPro for Powerline:style=bold:size=12' ", "w")
-mpd_pipe:setvbuf("line")
-
-local mpd_defaults={
-    update_interval=500, -- 500 or less makes seconds increment relatively smoothly while playing
-    address="localhost", -- mpd server info (localhost:6600 are mpd defaults)
-    port=6600,
-    password=nil, -- mpd password (if any)
+local mpd = {
+    update_interval = 500,  -- 500 or less makes seconds increment relatively smoothly while playing
+    address  = "localhost", -- mpd server info (localhost:6600 are mpd defaults)
+    port     = 6600,        -- default mpd port
+    password = nil,         -- mpd password (if any)
 }
-
+local timers    = {}
+local callbacks = {}
 --------------------------------------------------------------------------------------------------------------------------------------------
-
 if not dzen_tab then
     dzen_tab = {
-        { t = 'dateb', prog = 'lua ~/.notion/mpd.lua', delay = 1 * 1000, },
+        { 
+            t = 'mpd_writter', 
+            prog = 'mpd.lua', 
+            delay = mpd.update_interval, 
+        },
     }
 end
-
-local timers = {}
-local callbacks = {}
 
 -- Just print out stderr for now.
 local function flush_stderr(partial_data)
@@ -47,44 +45,47 @@ function start_execute(key)
                 partial_data = coroutine.yield()
             end
         
+            -- remove all but the last line
+            data = string.gsub(data, "\n$", "")
+            data = string.gsub(data, ".*\n", "");
+        
+            local sets = dzen_tab[key]
+            local t_key = sets.t
+            local t_length = sets.meter_length
+        
+            if not t_length or t_length < 1 then
+                t_length = string.len(data)
+            end
+
+            -- mod_statusbar.inform( t_key, data )
+            -- mod_statusbar.inform(t_key .. "_template", string.rep( ' ', t_length))
+
+            if mpd_pipe == nil then
+                mpd_pipe = io.popen("dzen2 -dock -bg '#000000' -h 19 -tw 0  -x 0 -ta r -w 910 -p -fn 'PragmataPro for Powerline:style=bold:size=12' ", "w")
+                mpd_pipe:setvbuf("line")
+            end
+            mpc = data
+            mpd_dzen_update()
+
             -- If no updates, then just schedule another run
             if not data or data == "" then
                 return timers[key]:set(dzen_tab[key].delay, callbacks[key])
             end
         
-            -- remove all but the last line
-            data = string.gsub( data, "\n$", "" )
-            data = string.gsub( data, ".*\n", "" );
-        
-            local sets = dzen_tab[key]
-            local t_key = sets.t
-            local t_length = sets.meter_length
-            local h_regexp = sets.hint_regexp
-        
-            if not tlength or t_length < 1 then
-                t_length = string.len( data )
-            end
-
-            mpc = data
-            -- mod_statusbar.inform( t_key, data )
-            -- mod_statusbar.inform(t_key .. "_template", string.rep( ' ', t_length))
-        
-            if not h_regexp then h_regexp = {} end
-        
-            -- mod_statusbar.update()
-            mpd_dzen_update()
-        
             return timers[key]:set(
                     dzen_tab[key].delay,
                     callbacks[key])
                     end
-                )
+        )
 
-            -- We need to pass it the key before calling a background call.
-            coroutine.resume(handle_output_changes, key)
-            notioncore.popen_bgread(dzen_tab[key].prog,
-                                    function(cor) coroutine.resume(handle_output_changes, cor) end,
-                                    coroutine.wrap(flush_stderr))
+        -- We need to pass it the key before calling a background call.
+        coroutine.resume(handle_output_changes, key)
+        notioncore.popen_bgread(dzen_tab[key].prog,
+                                function(cor) 
+                                    coroutine.resume(handle_output_changes, cor) 
+                                end,
+                                coroutine.wrap(flush_stderr)
+                                )
 end
 
 for key in pairs(dzen_tab) do
