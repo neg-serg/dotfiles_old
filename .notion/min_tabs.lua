492af97d358277c68479aa4ae1b53631e1b1b934
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-11 12:54:43 +0300

    [notion] min_tabs cleanup
    Former-commit-id: ab3e1188c5101c604839548a3829d9f3116467f1 [formerly b64c09e11b74a6e5a2c87cbfae125e5e040ce961] [formerly d64a63080ade9b69214c7f67bd3127b97639f6bd [formerly e3dea309c8ff2cfa41fd753ab2903b154eac73f7]]
    Former-commit-id: 538182b4a8cdf282f42fa4ea4a0f1f8ae3234fc4 [formerly 26f4201bdf4cf8f92eed6c78edb5d0c7202eb1d6]
    Former-commit-id: b46f6ad5d16f0855b7c0c4620ec641307365bf84

--- .notion/min_tabs.lua
+++ .notion/min_tabs.lua
@@ -1,43 +1,8 @@
---[[
-	min_tabs.lua
-
-	lua code to auto show/hide tabs as the number of windows in an ion frame
-	changes from/to 1
-	this affects windows on WIonWS and WPaneWS workspaces but leave
-	windows on WFloatWS's alone
-
-	Originally created by David Tweed
-
-	22 Feb 2007 John Schreiner: ignore transients to prevent empty frames
-        14 Dec 2006 David Roundy: modified to work with latest ion3
-	11 Feb 2005 David Tweed: modified to work with ion3 svn versions
-	15 Feb 2005 Cas Cremers: only frames with one window hide the tabbar
-	(as opposed to <= 2)
-	01 Sep 2005 Cas Cremers: single client windows that are tagged still
-	show the tabbar.
-
-	- Easier to work with using a style with slightly wider frame
-	  borders and more vivid "active frame" colours.
-	- The keybinding META..T is defined at the end of the file: this
-	  should override the normal binging, thus min_tabs should be
-	  loaded *after* the normal definition of META..T. It is not
-	  appropriate to have key bindings hard-coded in extensions, but I
-	  don't currently know of a better way.
-	- Currently supports ion3 with old toggling as well as new
-	  toggling function conventions; this can be removed later on.
-
-	one way to enable this is by adding
-	dopath("min_tabs")
-	to cfg_ion.lua
-
-]]
-
 function show_only_necessary_tabs_in_frame(fp)
     if WFrame.mode(fp) == 'floating' then
 	-- Escape: floatws thing should not be handled
         return
     end
-
 
     -- First the logic, then the propagation back to ion
 
@@ -47,7 +12,7 @@ function show_only_necessary_tabs_in_frame(fp)
 
     -- Assume the tabbar should be shown.
     local show_bar = true
-  -- if WMPlex.mx_count(fp) != 1 then
+    -- if WMPlex.mx_count(fp) != 1 then
     local rg = fp:mx_nth(0)
     if rg == nil then show_bar = false
     else
@@ -55,7 +20,7 @@ function show_only_necessary_tabs_in_frame(fp)
             show_bar = false
         end
     end
-  -- end
+    -- end
 
     -- Propagate choice
     notioncore.defer(function()
@@ -70,53 +35,25 @@ function show_only_necessary_tabs_in_frame(fp)
      end)
 end
 
-
-
 function show_only_necessary_tabs_in_frame_wrapper(ftable)
     show_only_necessary_tabs_in_frame(ftable.reg)
 end
-
-
 
 function min_tabs_setup_hook()
     local hk=notioncore.get_hook("frame_managed_changed_hook")
     hk:add(show_only_necessary_tabs_in_frame_wrapper)
 end
 
-
-
 function min_tabs_tag_wrapper(fr,reg)
-
     -- Note the ugly code: this actually caters for two versions of ion3
     -- I am only including this because I like Ubuntu [CC]
-
     local oldversion = (reg["toggle_tag"] ~= nil)
     if oldversion then
-
-        -- old version (for Ubuntu, can be removed later on)
-        reg:toggle_tag()
-
+        reg:toggle_tag() -- old version (for Ubuntu, can be removed later on)
     else
-
-        -- new version
-        reg:set_tagged("toggle")
-
+        reg:set_tagged("toggle") -- new version
     end
-
     -- recompute tabbar state
     show_only_necessary_tabs_in_frame(fr)
 end
-
-
---[[
-	Special keybinding override for this extension
-]]
-
-defbindings("WMPlex.toplevel", {
-    bdoc("Tag current object within the frame."),
-    kpress("Mod4+T", "min_tabs_tag_wrapper(_,_sub)", "_sub:non-nil"),
-})
-
-
-
 min_tabs_setup_hook()
