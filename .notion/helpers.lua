7959222ca56052fb03f479c477112de82eabe1f9
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-11 12:58:31 +0300

    [notion] move dynamic_view to helpers
    Former-commit-id: df8e82ae619337ae79e012bfdbb24e1492bac585 [formerly ea316d8acbe7e7313eff50678628f70db1f5f255] [formerly eef4310d07310cebc4fd491041f8099922342687 [formerly b4a4ccfd3d1ee22f3b50e07d5d06601c22a897e4]]
    Former-commit-id: ae837f593fc1f72ad3484a2aa0dff8a9bb0856d0 [formerly c72340ed3ccef57e519b43bbaee38265a9191dcc]
    Former-commit-id: e2f3488cfc0bfd164d29098ddb7f84482884b3dd

--- .notion/helpers.lua
+++ .notion/helpers.lua
@@ -86,6 +86,38 @@ function move_current.move(ws, dir)
     cwin:goto_focus()
 end
 
+dynamic_view = {}
+dynamic_view.cache = {}
+
+function dynamic_view.toggle (active_ws, target_ws_name, direction)
+    local target_ws = notioncore.lookup_region(target_ws_name, 'WGroupWS')
+    if not target_ws then return end
+    local target_frame = target_ws:current():current()
+    local key = active_ws:name()
+    if not dynamic_view.cache[key] then dynamic_view.cache[key] = {} end
+    local grabbed = dynamic_view.cache[key]
+    
+    if not grabbed[target_ws_name] then
+        if target_frame:mx_count() == 0 then return end
+        local temp_frame = active_ws:split_at(active_ws:current(), direction, false)
+        dynamic_view.move_clients(target_frame, temp_frame)
+        grabbed[target_ws_name] = temp_frame
+    else
+        temp_frame = grabbed[target_ws_name]
+        dynamic_view.move_clients(temp_frame, target_frame)
+        active_ws:unsplit_at(active_ws:current())
+        grabbed[target_ws_name] = nil
+    end
+end
+
+function dynamic_view.move_clients (from_frame, to_frame)
+    from_frame:mx_i(function (cwin)
+        notioncore.defer(function ()
+            to_frame:attach(cwin)
+        end)
+        return true
+    end)
+end
 -- ......................................................................................
 function get_hostname()
    local out = io.popen("hostname")
