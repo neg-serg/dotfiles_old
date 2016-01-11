6eb111bc2c5f41873533d8689c35e65851158c57
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-11 12:54:56 +0300

    [notion] update keybindings
    Former-commit-id: 9b0695438f4858496b234bfd9e2c5d3f423b63ee [formerly 1573dc1d058cae356b43b34aa639e7f2c5f91a84] [formerly c73b1b82d216bb8b8a6533057fae447570b1e48e [formerly a4b4f5a66c1d747c611c64201b63d9e477801cec]]
    Former-commit-id: d5138f892797535ac646a0a0db9410a8debc3a45 [formerly 222d5ee69b77f10187e3c08c96c8b3867fe41493]
    Former-commit-id: 374a05c6791a6bea1aeff7809da0f8eb1a906612

--- .notion/cfg_kb.lua
+++ .notion/cfg_kb.lua
@@ -5,26 +5,26 @@ Neg_kb.WMPlex_toplevel = {
     -- WMPlex toplevel --
     ---------------------
     kpress("M1+space", nil),
-    kpress("M4+F2",    "repl(_)"),
     kpress("M4+M",     "notioncore.exec_on(ioncore.current(), '~/bin/scripts/rofi_xprop' .. ' ' .. _chld:xid())"),
     kpress("M4+Sh+L",  "notioncore.exec_on(_, notioncore.lookup_script('notion-lock'))"),
     kpress("M4+Sh+D",  "notioncore.detach(_chld, 'toggle')", "_chld:non-nil"),
     kpress("M4+F11",   "rofi.mainmenu()"),
     kpress("M4+slash", "notioncore.goto_previous()"),
     kpress("M1+Tab",   "notioncore.goto_previous()"),
+    kpress("M4+T",     "min_tabs_tag_wrapper(_,_sub)", "_sub:non-nil"),
     -------------------------------------------------------------------------------------
     --[[ SCRATCHPADS  ]]--------------------------------
     --------------------
-    kpress("M4+d",       "console(_)"),
-    kpress("M4+p",       "named_scratchpad(_, 'float2')"),
-    kpress("M4+F",       "ncmpcpp(_)"),
-    kpress("M4+E",       "named_scratchpad(_, 'im')"),
-    kpress("M4+Ct+9",    "named_scratchpad(_, 'wicd')"),
-    kpress("M4+Ct+2",    "named_scratchpad(_, 'alsa')"),
-    kpress("M4+Ct+4",    "named_scratchpad(_, 'etc')"),
-    kpress("M4+Ct+G",    "rofi.goto_or_create_ws(_)"),
-    kpress("M1+G",       "rofi.goto_win(_)"),
-    kpress("M4+F9",      "notioncore.create_ws(_)"),
+    kpress("M4+d",       "console(ioncore.current())"),
+    kpress("M4+p",       "named_scratchpad(ioncore.current(), 'float2')"),
+    kpress("M4+F",       "ncmpcpp(ioncore.current())"),
+    kpress("M4+E",       "named_scratchpad(ioncore.current(), 'im')"),
+    kpress("M4+Ct+9",    "named_scratchpad(ioncore.current(), 'wicd')"),
+    kpress("M4+Ct+2",    "named_scratchpad(ioncore.current(), 'alsa')"),
+    kpress("M4+Ct+4",    "named_scratchpad(ioncore.current(), 'etc')"),
+    kpress("M4+Ct+G",    "rofi.goto_or_create_ws(ioncore.current())"),
+    kpress("M1+G",       "rofi.goto_win(ioncore.current())"),
+    kpress("M4+F9",      "notioncore.create_ws(ioncore.current())"),
     -------------------------------------------------------------------------------------
     --[[  PROGS RUN by APP  ]]-----------------------------
     --------------------------
@@ -40,39 +40,40 @@ Neg_kb.WMPlex_toplevel = {
     --------------
     kpress("M4+Ct+Q",  "WRegion.rqclose_propagate(_, _sub)"),
     kpress("M4+Sh+F",  "app.byinstance('lowriter', 'VCLSalFrame', 'libreoffice-writer')"),
-    kpress("M4+Sh+t",  "named_scratchpad(_, 'stalonetray')"),
+    kpress("M4+Sh+t",  "named_scratchpad(ioncore.current(), 'stalonetray')"),
     submap("M1+E",{
         kpress("Ct+r", "app.byinstance('cr3', 'Cr3', 'cr3')"),
         kpress("Ct+w", "autoprop(_sub, _, true)", "_sub:WGroupCW"),
-        kpress("d",    "notioncore.exec_on(_, 'stardict')"),
-        kpress("i",    "notioncore.exec_on(_, '~/bin/pls -output')"),
-        kpress("l",    "radare2(_)"),
+        kpress("d",    "notioncore.exec_on(notioncore.current, 'stardict')"),
+        kpress("i",    "notioncore.exec_on(notioncore.current, '~/bin/pls -output')"),
+        kpress("l",    "radare2(notioncore.current())"),
         kpress("m",    "app.byclass('~/bin/scripts/run_mutt', 'mutt')"),
         kpress("n",    "app.byinstance('nicotine.py || nicotine', 'Nicotine.py', 'nicotine.py')"),
-        kpress("o",    "notioncore.exec_on(_, '~/bin/pls -sink')"),
-        kpress("p",    "notioncore.exec_on(_, 'st pulsemixer')"),
-        kpress("q",    "notioncore.exec_on(_, '~/bin/mpd_sel.sh')"),
-        kpress("Sh+d", "notioncore.exec_on(_, '~/bin/scripts/exec_demo.sh')"),
-        kpress("Sh+g", "gdb(_)"),
-        kpress("Sh+k", "notioncore.exec_on(_, '~/bin/scripts/toggle_keynav')"),
-        kpress("Sh+o", "notioncore.exec_on(_, '~/bin/pls -vol')"),
-        kpress("Sh+r", "ranger(_)"),
-        kpress("s",    "notioncore.exec_on(_, 'skype')"),
-        kpress("t",    "notioncore.exec_on(_, 'urxvt')"),
-        kpress("v",    "notioncore.exec_on(_, 'vmware')"),
-        kpress("w",    "notioncore.exec_on(_, '~/bin/webcam')"),
+        kpress("o",    "notioncore.exec_on(notioncore.current(), '~/bin/pls -sink')"),
+        kpress("p",    "notioncore.exec_on(notioncore.current(), 'st pulsemixer')"),
+        kpress("q",    "notioncore.exec_on(notioncore.current(), '~/bin/mpd_sel.sh')"),
+        kpress("Sh+d", "notioncore.exec_on(notioncore.current(), '~/bin/scripts/exec_demo.sh')"),
+        kpress("Sh+g", "gdb(notioncore.current())"),
+        kpress("Sh+k", "notioncore.exec_on(notioncore.current(), '~/bin/scripts/toggle_keynav')"),
+        kpress("Sh+o", "notioncore.exec_on(notioncore.current(), '~/bin/pls -vol')"),
+        kpress("Sh+r", "ranger(notioncore.current())"),
+        kpress("s",    "notioncore.exec_on(notioncore.current(), 'skype')"),
+        kpress("t",    "notioncore.exec_on(notioncore.current(), 'urxvt')"),
+        kpress("v",    "notioncore.exec_on(notioncore.current(), 'vmware')"),
+        kpress("w",    "notioncore.exec_on(notioncore.current(), '~/bin/webcam')"),
     }),
 }
 
 Neg_kb.WScreen = {
+    ---------------------
+        -- WScreen --    
+    ---------------------
     submap("M1+E", {
             kpress("Ct+A", "notioncore.goto_activity()"),
             kpress("Ct+t", "notioncore.tagged_clear()"),
             }),
     kpress("M4+Sh+1",      "notioncore.goto_nth_screen(0)"),
     kpress("M4+Sh+2",      "notioncore.goto_nth_screen(1)"),
-    kpress("M4+Sh+comma",  "notioncore.goto_prev_screen()"),
-    kpress("M4+Sh+period", "notioncore.goto_next_screen()"),
     submap("M1+E", {
         kpress("AnyModifier+L",    "WRegion.rqorder(_chld, 'front')","_chld:non-nil"),
         kpress("AnyModifier+Sh+L", "WRegion.rqorder(_chld, 'back')","_chld:non-nil"),
@@ -90,12 +91,18 @@ Neg_kb.WScreen = {
 }
 
 Neg_kb.WClientWin = {
+    ---------------------
+      -- WClientWin --   
+    ---------------------
     submap("M1+E", {
         kpress("C",        "WklientWin.kill(_)"),}),
         kpress("M1+comma", "WClientWin.quote_next(_)"),
 }
 
 Neg_kb.WGroupCW = {
+    ---------------------
+       -- WGroupCW --   
+    ---------------------
     kpress_wait("M4+Q", "WGroup.set_fullscreen(_, 'toggle')"),
     kpress("M4+0",      "rofi.renameworkspace(_)"),
     kpress("M4+9",      "comp_man_please()"),
@@ -143,14 +150,17 @@ Neg_kb.floating = {
     kpress("M1+Ct+J", "_:push_direction('down')"),
     kpress("M1+Ct+K", "_:push_direction('up')"),
     kpress("M1+Ct+L", "_:push_direction('right')"),
+
     kpress("M1+Ct+F", "_:maximize_fill_toggle('vh')"),
     kpress("M1+Ct+V", "_:maximize_fill_toggle('vert')"),
+
     submap("M1+E", { kpress("B", "mod_tiling.mkbottom(_)"), }),
 }
 
 Neg_kb.WMoveresMode = {
     kpress("AnyModifier+Escape", "WMoveresMode.cancel(_)"),
     kpress("AnyModifier+Return", "WMoveresMode.finish(_)"),
+    kpress("Ct+C", "WMoveresMode.finish(_)"),
 
     kpress("Left",     "WMoveresMode.resize(_, 1, 0, 0, 0)"),
     kpress("h",        "WMoveresMode.resize(_, 1, 0, 0, 0)"),
@@ -177,25 +187,30 @@ Neg_kb.WMoveresMode = {
 }
 
 Neg_kb.Tiling = {
-    kpress("M4+Up",   "notioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
-    kpress("M4+k",    "notioncore.goto_next(_sub, 'up',    {no_ascend=_})"),
-    kpress("M4+Down", "notioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
-    kpress("M4+j",    "notioncore.goto_next(_sub, 'down',  {no_ascend=_})"),
-    kpress("M4+Left", "notioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
-    kpress("M4+h",    "notioncore.goto_next(_sub, 'left',  {no_ascend=_})"),
-    kpress("M4+Right","notioncore.goto_next(_sub, 'right', {no_ascend=_})"),
-    kpress("M4+l",    "notioncore.goto_next(_sub, 'right', {no_ascend=_})"),
-    kpress("M4+Ct+H", "WTiling.split_at(_, _sub, 'right', true)"),
-    kpress("M4+Ct+L", "WTiling.split_at(_, _sub, 'left', true)"),
-    kpress("M4+Ct+K", "WTiling.split_at(_, _sub, 'bottom', true)"),
-    kpress("M4+Ct+J", "WTiling.split_at(_, _sub, 'top', true)"),
-    kpress("M4+2",    "WTiling.transpose_at(_, _sub)"),
-    kpress("M4+3",    "WTiling.flip_at(_, _sub)"),
-    kpress("M4+Ct+X", "WTiling.unsplit_at(_, _sub)"),
+    kpress("M4+Up",   "goto_dir('up')"),
+    kpress("M4+k",    "goto_dir('up')"),
+    kpress("M4+Down", "goto_dir('down')"),
+    kpress("M4+j",    "goto_dir('down')"),
+    kpress("M4+Left", "goto_dir('left')"),
+    kpress("M4+h",    "goto_dir('left')"),
+    kpress("M4+Right","goto_dir('right')"),
+    kpress("M4+l",    "goto_dir('right')"),
+    kpress("M4+Ct+H", "tiling_split('right')"),
+    kpress("M4+Ct+L", "tiling_split('left')"),
+    kpress("M4+Ct+K", "tiling_split('bottom')"),
+    kpress("M4+Ct+J", "tiling_split('top')"),
+    kpress("M4+2",    "tiling_transpose()"),
+    kpress("M4+Sh+3", "tiling_flip()"),
+    kpress("M4+Ct+X", "tiling_unsplit()"),
     kpress("M4+Ct+W", function(ws) move_current.move(ws, "up") end),
     kpress("M4+Ct+S", function(ws) move_current.move(ws, "down") end),
     kpress("M4+Ct+A", function(ws) move_current.move(ws, "left") end),
     kpress("M4+Ct+D", function(ws) move_current.move(ws, "right") end),
+    -- kpress("M4+Ct+D", "collapse.collapse(_)"),
+    -- vertical layout
+    --  kpress("Mod4+Control+i" , "multiple_split(_, _sub, 'top')"),
+    -- horizontal layout
+    -- kpress("M4+Ct+o" , "multiple_split(_, _sub, 'left')"),
 }
 
 defbindings("WMPlex.toplevel", Neg_kb.WMPlex_toplevel)
