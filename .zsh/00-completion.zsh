3549cc5ff6d927298e0c2b72188c4fa92601460a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-15 22:40:48 +0300

    [zsh] expand complete with dots
    Former-commit-id: 3e8b5a068fbb8189c018f95e53a1613a5fed4d81 [formerly 37a1108e06992a0a1fb24a93242ab1096cd0b412] [formerly a597573cceea71849304bf902c1fed2baf37b661 [formerly a87531daef8892e5c24f1cd1ca1bda9349127dbe]]
    Former-commit-id: eb5898ffeb50d7aa21f413ace23884f55411fe43 [formerly fba04b849e5519e5354b7111278408e006ce34a8]
    Former-commit-id: dd425e1d293fe204b756e97e81f4f44f72e6c855

--- .zsh/00-completion.zsh
+++ .zsh/00-completion.zsh
@@ -165,9 +165,9 @@ zstyle ':completion:*:*:*:users' ignored-patterns \
         rpc rpcuser rpm shutdown squid sshd sync uucp vcsa xfs
 
 function expand-or-complete-with-dots() {
-  #echo -n "\e[31m......\e[0m"
-  zle expand-or-complete
-  zle redisplay
+    echo -n "\e[36m-=--...--=-\e[0m"
+    zle expand-or-complete
+    zle redisplay
 }
 zle -N expand-or-complete-with-dots
 bindkey "^I" expand-or-complete-with-dots
