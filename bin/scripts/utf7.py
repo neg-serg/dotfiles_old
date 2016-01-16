95e6328acd7dd455f81992e98315776b48052e0d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-16 18:01:38 +0300

    [offlineimap] add password support

--- bin/scripts/utf7.py
+++ bin/scripts/utf7.py
@@ -2,6 +2,11 @@
 import binascii
 import codecs
 
+from subprocess import check_output
+
+def get_pass():
+    return check_output("gpg -dq ~/.offlineimappass.gpg", shell=True).strip("\n")
+
 # encoding
 
 def modified_base64(s):
