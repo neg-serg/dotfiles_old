9a379f1ff4edb86e1cc92011e311b75070903edb
Sergey Miroshnichenko <serg.zorg@gmail.com>
2016-01-16 18:57:40 +0300

    [scripts] update hi generator
    Former-commit-id: 85cca9f3d0ab8e7da708ab3ae5567911dd06384d [formerly 82660a64a34282214c30bb70ea58e3d7379af7cf] [formerly 0ebf40ea46e68880a60c95df5ae29fc01b4ce5fb [formerly a6887a84914b9ef6f9beec36c122e8e86fbac0ab]]
    Former-commit-id: 459895eeacced55a7a5df7fadc0de6a9012c2020 [formerly bf6b95a208b90ce2ec85c2e074762fbff6490e15]
    Former-commit-id: d9777975acc2f78beb5738e0a36664425a63cf83

--- bin/scripts/hi_gen.zsh
+++ bin/scripts/hi_gen.zsh
@@ -2,50 +2,34 @@
 
 eat () {
         prog=${0##*/}
-        bldblu='\e[1;34m'
-        bldred='\e[1;31m'
-        bldwht='\e[1;37m'
-        txtbld=$(tput bold)
-        txtund=$(tput sgr 0 1)
-        txtrst='\e[0m'
-        info=${bldwht}*${txtrst}
-        pass=${bldblu}*${txtrst}
-        warn=${bldred}!${txtrst}
+        local bldblu='\e[1;34m'
+        local txtbld=$(tput bold)
+        local txtund=$(tput sgr 0 1)
+        local txtrst='\e[0m'
+        local pass=${bldblu}*${txtrst}
         filename=$@
-        if [[ -z ${filename} ]]
-        then
-                echo " ${prog} <filename> - copy a file to the clipboard"
-                exit
-        fi
-        if [[ ! -f ${filename} ]]
-        then
-                echo -e "${warn} File ${txtund}${filename}${txtrst} doesn't exist"
-                exit
-        fi
-        if [[ $(whoami) = root ]]
-        then
-                echo -e "${warn} Must be regular user to copy a file to the clipboard"
-                exit
-        fi
-        xclip -in -selection c < "$filename"
+        xclip -in -selection c < "${filename}"
         echo -e "${pass} ${txtund}"${filename##*/}"${txtrst} copied to clipboard"
 }
 
-egrep '^[.*]' ~/.config/dircolors/.dircolors|sed 's/#.*//'|sed 's/^\.\W*/ftype-/' > /tmp/generation
+dircolors_location="${XDG_CONFIG_HOME}/dircolors/.dircolors"
+egrep '^[.*]' ${1:-$dircolors_location}|sed 's/#.*//'|sed 's/^\.\W*/ftype-/' > /tmp/generation
 
 echo "-----------------------------"
 echo "you should handle it by hand:"
 echo "-----------------------------"
 
 egrep '^ftype-' -v /tmp/generation 
-egrep '^ftype-' /tmp/generation | sed -e 's/38;5;/fg=/' \
-                                      -e 's/48;5;/bg=/g' \
-                                      -e "/\(01;\|;1\)/s/$/,bold/" \
-                                      -e 's/00;//' \
-                                      -e 's/\(01;\|;1\)//' \
-                                      -e 's/;/,/g' \
-                                      -e 's/,4/,underline/g' \
-                                      -e "/\ 3[0-9][0-9]*/s/3/fg=/" > /tmp/_result
+egrep '^ftype-' /tmp/generation | sed -e 's/38;5;/fg=/'                  \
+                                      -e 's/48;5;/bg=/g'                 \
+                                      -e "/\(01;\|;1\)/s/$/,bold/"       \
+                                      -e "/\(04;\|;4\)/s/$/,underline/"  \
+                                      -e 's/00;//'                       \
+                                      -e 's/\(01;\|;1\)//'               \
+                                      -e 's/;/,/g'                       \
+                                      -e 's/,4/,underline/g'             \
+                                      -e "/\ 3[0-9][0-9]*/s/3/fg=/"      \
+                                      -e "s/, //g" > /tmp/_result
 
 vim -c 'Tabularize/[bf]g=.*' /tmp/_result -c 'wq'
 sed 's/^ftype-//' /tmp/_result|awk '{print "*."$1") style=$ZSH_HIGHLIGHT_STYLES[ftype-"$1"];;"}' > /tmp/_rules
