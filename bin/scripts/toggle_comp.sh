3afbf2d6db1f333d29ef70deedd4329f1461a611
Neg <serg.zorg@gmail.com>
2016-01-24 19:13:15 +0300

    fix compton toggle

--- bin/scripts/toggle_comp.sh
+++ bin/scripts/toggle_comp.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-if [[ $(pidof compton) ]]; then
-    killall compton 
+if [[ $1 == "twice" ]]; then
+    killall compton && compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf &
 else
-    compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf &
+    if [[ $(pidof compton) ]]; then
+        killall compton 
+    else
+        compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf &
+    fi
 fi
