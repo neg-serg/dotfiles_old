a376914ae76683e6c819cf331f495a8e538c98d0
Neg <serg.zorg@gmail.com>
2016-02-01 12:26:02 +0300

    [scripts] redirect compton messages

--- bin/scripts/toggle_comp.sh
+++ bin/scripts/toggle_comp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [[ $1 == "twice" ]]; then
-    killall compton && compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf &
+    killall compton && compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf 2> /dev/null&
 else
     if [[ $(pidof compton) ]]; then
         killall compton 
     else
-        compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf &
+        compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf 2> /dev/null &
     fi
 fi
