b8416696f20d018412c7d9d9125fc310578f8ed6
Neg <serg.zorg@gmail.com>
2016-02-23 16:42:13 +0300

    [notion] cleanup dbg
    Former-commit-id: 386cbd70212d2b5f3f99badadee871d975c5fbb4 [formerly 301d9146be644fda178076a1a101cf582e24543a] [formerly 3f47e053a15b5429ffca1ea28d1853d8ca66eaff [formerly a2f7653a5a103fcf6dc618a7ba985a1b15c759f5]]
    Former-commit-id: a4364bdd1239726fe857695b8fdc445c234f97a4 [formerly aba30baec5e68cc5f65c3c27358a81f130941231]
    Former-commit-id: b9fecf7d898581e5adc90fd183a56d1c09c0182a

--- .notion/dbg.lua
+++ .notion/dbg.lua
@@ -5,38 +5,38 @@ function dbg.echon(s)
 end
 
 function dbg.tostring(obj)
-        local mt
-        local tname
-        local o
-        if type(obj) == 'userdata' then
-                mt = getmetatable(obj)
-                o = '['
-                if mt and mt.__index then
-                        tname = mt.__index.__typename
-                        if tname then
-                                o = o .. tname
-                        end
+    local mt
+    local tname
+    local o
+    if type(obj) == 'userdata' then
+        mt = getmetatable(obj)
+        o = '['
+        if mt and mt.__index then
+                tname = mt.__index.__typename
+                if tname then
+                        o = o .. tname
                 end
-                if obj.name then
-                        if tname and tname == 'WClientWin' then
-                                o = o .. ': ' .. dbg.tostring(obj:get_ident())
-                        else
-                                o = o .. ': ' .. obj:name()
-                        end
-                end
-                return o .. ']'
-        elseif type(obj) == 'table' then
-                o = '{'
-                sep = ''
-                for k, v in pairs(obj)
-                do
-                    o = o .. sep .. dbg.tostring(k) .. '='
-                    o = o .. dbg.tostring(v)
-                    sep = ', '
-                end
-                return o .. '}'
         end
-        return tostring(obj)
+        if obj.name then
+                if tname and tname == 'WClientWin' then
+                        o = o .. ': ' .. dbg.tostring(obj:get_ident())
+                else
+                        o = o .. ': ' .. obj:name()
+                end
+        end
+        return o .. ']'
+    elseif type(obj) == 'table' then
+        o = '{'
+        sep = ''
+        for k, v in pairs(obj)
+        do
+            o = o .. sep .. dbg.tostring(k) .. '='
+            o = o .. dbg.tostring(v)
+            sep = ', '
+        end
+        return o .. '}'
+    end
+    return tostring(obj)
 end
 
 --[[ Save Table to File Load Table from File v 1.0 
@@ -167,63 +167,3 @@ function table.load( sfile )
 end
 -- close do
 end
-
-function mod_query.query_lua(mplex)
-    local env=mod_query.create_run_env(mplex)
-
-    local function complete(cp, code)
-        cp:set_completions(mod_query.do_complete_lua(env, code))
-    end
-
-    local function handler(mplex, code)
-        return mod_query.do_handle_lua(mplex, env, code)
-    end
-
-    mod_query.query(mplex, TR("Lua code:"), nil, handler, complete, "lua")
- end
-
-function do_handle_lua(mplex, env, code)
-    local print_res
-    local function collect_print(...)
-        local tmp=""
-        local arg={...}
-        local l=#arg
-        for i=1,l do
-            tmp=tmp..tostring(arg[i])..(i==l and "\n" or "\t")
-        end
-        print_res=(print_res and print_res..tmp or tmp)
-    end
-
-    local f, err=loadstring(code)
-    if not f then
-        return
-    end
-
-    env.print=collect_print
-    setfenv(f, env)
-
-    local result
-    err=collect_errors(function () result = f() end)
-
-    if err then
-
-    elseif print_res then
-       mod_query.message(mplex, print_res .. "\nResult: "..tostring(result))
-    end
-end
-
-function repl(ws)
-   local env=mod_query.create_run_env(ws)
-
-   local function complete(cp, code)
-        cp:set_completions(mod_query.do_complete_lua(env, code))
-     end
-
-   local function handler(mplex, code)
-      return do_handle_lua(mplex, env, code)
-   end
-
-mod_query.query(ws, "[Lua] >> ", nil, handler, complete, "lua")
-end
-
-
