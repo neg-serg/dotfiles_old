ea8158409bf9b47fc8b6d6ce7ed08bb03ca18659
Neg <serg.zorg@gmail.com>
2016-02-23 19:43:23 +0300

    [zsh] disable expand-or-complete-highlight
    Former-commit-id: 94c6265e531beb8a621de247e6c6beec0591a10f [formerly 7595b5ce64957fd6f76dcb56c1722d6504ff8e5e] [formerly 098baefc9c2223f9c2b87532160b1f87e0de452f [formerly cb6c1c3e4a6a684b12910ad4739d59d39d89c55a]]
    Former-commit-id: 28b6856cdc4a36801d9fa68c2d36141f2b975ae0 [formerly 5b410beaf05c4a79220dd61ac05b7ebcc39ed387]
    Former-commit-id: b7d07799932ac09bffc80991b9591506c4c8b7f3

--- .zsh/05-functions.zsh
+++ .zsh/05-functions.zsh
@@ -323,13 +323,6 @@ function fg-widget() {
 zle -N fg-widget
 
 
-function expand-or-complete-and-highlight() {
-    zle expand-or-complete
-    _zsh_highlight
-}
-
-zle -N expand-or-complete-and-highlight expand-or-complete-and-highlight
-
 #pcp - copy files matching pattern $1 to $2
 function pcp(){ find . -regextype awk -iregex ".*$1.*" -print0 | xargs -0 cp -vR -t "$2" }
 
@@ -758,33 +751,6 @@ function sls(){
                 print id ": " name;
             }'
 }
-
-function _zwrap() { echo "$fg[blue][$fg[white]$1$fg[blue]]$fg[default]" }
-
-function _zfwrap(){
-    apply=$1;
-    body=$2;
-    shift
-    echo $(apply) ${body} $(apply)
-}
-
-function _zgwrap(){
-    side=$1;
-    body=$2;
-    shift
-    echo ${side} ${body} ${side}
-}
-
-function _zfwrap() {
-    local tmp_name="$(echo $1|sed "s|^${HOME}|$fg[green]~|;s|/|$fg[blue]&$fg[white]|g")"
-    local decoration="$fg[green]‒$fg[white]"
-    local fancy_name="${decoration} $fg[white]${tmp_name} ${decoration}"
-    echo ${fancy_name}
-}
-
-function _zpref() { echo $(_zwrap ">>") }
-function _zfg(){ echo -ne "[38;5;$1m" }
-function _zdelim(){ echo -ne "$(_zfg 24)::"$(_zfg 8) }
 
 function record(){
     local rpath
