30459d4f70bb6e35467d2c5a13ce0a6e9a3ec8fe
Neg <serg.zorg@gmail.com>
2016-02-26 21:18:13 +0300

    [zsh] there is no need in expand-or-complete-with-dots anymore
    Former-commit-id: 894fce730d860094cabe4780b2f789ade1b96420 [formerly e3ba367a64fdb0efbc372b35902f8ad3bbc6c915] [formerly 401b094a85867ca20b20ec62ec455cc660b010c6 [formerly 95b19e41f4ece795bb14ad086abd1a79a0846de7]]
    Former-commit-id: 6983bdb5b5d9512dabc5c7e97232e10dbf99fe4b [formerly 99aa8fb2220143966b58dd2be810371bf16e9828]
    Former-commit-id: 6e701247666f84917c8c8cdad707597af0b6bb31

--- .zsh/12-completion.zsh
+++ .zsh/12-completion.zsh
@@ -185,14 +185,12 @@ zle -N forward-word-match
 zstyle ':zle:backward-kill-word*' word-style standard
 zstyle ':zle:*kill*' word-chars '*?_-.[]~=&;!#$%^(){}<>'
 
-
 expand-or-complete-with-dots() {
     echo -n "\e[31m......\e[0m"
     zle expand-or-complete
     zle redisplay
 }
 zle -N expand-or-complete-with-dots
-bindkey "^I" expand-or-complete-with-dots
 
 # Load completion from bash, which isn't available in zsh yet.
 bash_completions=()
