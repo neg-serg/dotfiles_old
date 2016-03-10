f46c7551246df87e1e4642e4f7a00cf3ed38cbf0
Neg <serg.zorg@gmail.com>
2016-03-10 03:12:43 +0300

    [notion] add tables
    Former-commit-id: 125240eab5cd6259dba56bf300dd772a639fc594 [formerly 02890884413c6ba227851729623fd9ebf11723fc] [formerly c68a343400f7666c631c8c8f4638fd88824d0226 [formerly e935d9eced57a476c37aca1497b676c8365f63eb]]
    Former-commit-id: 1bb564a36beabd66608d8f92be198bbaa58718e5 [formerly cc19b7e97cff256ac1fd9fa3244137dd0eb86b42]
    Former-commit-id: cecae2cb1257728deee21df4a48d97c9e01800be

---
+++ .notion/dzen/fancy_table.lua
@@ -0,0 +1,16 @@
+neg.dzen.fancy_table = {
+    term="",    dev="",
+    admin="",   web="",
+    media="",   im="",
+    look="",    right="",
+    center="",  left="",
+    data="",    vertical_dots="",
+    grid="",    gtd="",
+    gtd="",     text="",
+    play="",    pic="",
+    game="",    monitor="",
+    news="",    dev2="",
+    dev3="",    term2="",
+    firefox="", viber="",
+    vid="",     steam=""
+}
