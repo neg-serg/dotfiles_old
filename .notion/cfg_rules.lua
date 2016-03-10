f0357c91771fb4cad319023c32377cf5b7653472
Neg <serg.zorg@gmail.com>
2016-03-10 04:28:05 +0300

    [notion] set wine windows transient mode to default
    Former-commit-id: a35ec669d1899359a6ab46faf526336a0ebd6d8e [formerly 4b064f0cfa782878b685e47eb56c279c851ccf7c] [formerly c65c40567112a77ead4bae29b4d24a68e5eb50ef [formerly 93b7a625995452c596234c9539ad2fd4138a0ae1]]
    Former-commit-id: 4f9a21463317b4505ea5d53ae632d16a53d551fc [formerly 0905382cc4e8784536827c11282333a99b18d1c7]
    Former-commit-id: 023a672d17bc4610b205af2084995414ffc14634

--- .notion/cfg_rules.lua
+++ .notion/cfg_rules.lua
@@ -140,10 +140,10 @@ defwinprop{instance="seahorse",target="admin",jumpto=true}
 defwinprop{class="Xfce*",float=true}
 defwinprop{class="Xmessage",float=true}
 defwinprop{class="com-sun-javaws-Main",float=true}
-defwinprop{class="Crossover",target="wine",transient_mode="off",jumpto=true,tag="virt",float=true}
-defwinprop{class="Wine",target="wine",transient_mode="off",jumpto=true,tag="virt",float=true}
-defwinprop{class="*.exe",target="wine",transient_mode="off",jumpto=true,tag="virt",float=true}
-defwinprop{class="explorer.exe",target="wine",transient_mode="off",jumpto=true,tag="virt",float=true}
+defwinprop{class="Crossover",target="wine",jumpto=true,tag="virt",float=true}
+defwinprop{class="Wine",target="wine",jumpto=true,tag="virt",float=true}
+defwinprop{class="*.exe",target="wine",jumpto=true,tag="virt",float=true}
+defwinprop{class="explorer.exe",target="wine",jumpto=true,tag="virt",float=true}
 defwinprop{class="Steam",target="wine",jumpto=true,tag="virt"}
 defwinprop{instance="recoll",jumpto=false,winlist_ignore=true,target="search"}
 defwinprop{instance="stardict",jumpto=true,winlist_ignore=true,transient_mode="off",winlist_ignore=true,target="float"}
