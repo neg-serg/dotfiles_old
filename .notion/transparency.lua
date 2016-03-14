37626b47764a812bb973d113a03511c070f34c18
Neg <serg.zorg@gmail.com>
2016-03-14 17:17:30 +0300

    [notion] cleanup transparency.lua
    Former-commit-id: 7e7b4075dc606e1a599e47e44524fab20e066ba0 [formerly 3fea3588a16aa4c58ad7930c356918d15ecd3c1a] [formerly c95949a3e8ee196a97ecdff76bb45395909bdb93 [formerly 5544a58a8195af461e73a70c28927ca2236ecb50]]
    Former-commit-id: 6286902e2913ffa5454074ec3e0c28eeebf1564a [formerly 21c1be686a3ea385b53aacdddeb836cd268b80e8]
    Former-commit-id: 08a6f26f7d5a347e4af63f24e6622970382c3e17

--- .notion/transparency.lua
+++ .notion/transparency.lua
@@ -4,10 +4,9 @@ local function framelist(iter)
 
     local entries={}
     local ws_add=function(s) if s then table.insert(entries, s) end end
-    local name=""
 
     iter(function(obj)
-             if obj_is(obj, "WFrame") then
+             if obj_is(obj, "WMPlex") then
                  if obj:mx_count() == 0 then
                     table.insert(transparent_table, obj)
                     ws_add(obj:xid())
@@ -22,16 +21,15 @@ end
 
 local function maketransparent(reg)
     local atom_client_opacity = notioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
-    local atom_client_opacity = notioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
-
     local opacity_level = 3435973836
-    -- local cur=notioncore.find_manager(notioncore.current(), "WFrame")
+    -- local full_opacity_level = 4294967295
 
     framelist(notioncore.region_i)
+
     for _,reg in ipairs(transparent_table) do
         notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {opacity_level})
     end
-    
+
     for _,reg in ipairs(nontransparent_table) do
         notioncore.x_delete_property(reg:xid(), atom_client_opacity)
     end
