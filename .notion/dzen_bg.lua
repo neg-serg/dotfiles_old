4bfc81cfa8764858c22580b6920feac7df11d22f
Neg <serg.zorg@gmail.com>
2016-03-17 17:45:40 +0300

    [notion] dzen_bg refactoring

--- .notion/dzen_bg.lua
+++ .notion/dzen_bg.lua
@@ -1,20 +1,9 @@
---------------------------------------------------------------------------------------------------------------------------------------------
-local mpd = {
-    update_interval = 500,  -- 500 or less makes seconds increment relatively smoothly while playing
-    address  = "localhost", -- mpd server info (localhost:6600 are mpd defaults)
-    port     = 6600,        -- default mpd port
-    password = nil,         -- mpd password (if any)
-}
-local timers    = {}
-local callbacks = {}
---------------------------------------------------------------------------------------------------------------------------------------------
+local timers,callbacks = {}, {}
+local remove_lines = false
+
 if not dzen_tab then
     dzen_tab = {
-        { 
-            t = 'mpd_writter', 
-            prog = 'mpd.lua', 
-            delay = mpd.update_interval, 
-        },
+        { name="mpd", prog = "mpd.lua", delay = 500},
     }
 end
 
@@ -29,14 +18,40 @@ local function flush_stderr(partial_data)
     print(result, "\n")
 end
 
+local function init_dzen()
+    if dzen_bg_pipe == nil then
+        dzen_bg_pipe = io.popen(
+            "dzen2 -dock -bg ".. 
+            neg.dzen.bg_ ..
+            " -h ".. neg.dzen.h_ ..
+            " -tw 0 -x 0 -ta r -w ".. 
+            neg.dzen.mpd_w_ .. 
+            " -p -fn ".. 
+            neg.dzen.panel_font_ .. 
+            " ", "w"
+        )
+        dzen_bg_pipe:setvbuf("line")
+    end
+end
+
+local function pipe_write(d)
+    -- dzen_bg_pipe:write(d..'\n')
+    dzen_bg_pipe:write(d)
+end
+
+--remove all but the last line
+local function remove_all_but_last_line(str)
+    str = string.gsub(str, "\n$", "")
+    str = string.gsub(str, ".*\n", "")
+end
+
 -- Execute a program.  This will continuously retrieve output from the program.
 function start_execute(key)
    -- Read output from the program and (if data) then update the statusbar
    local handle_output_changes = coroutine.create(
        function(input_data)
             local key = input_data
-            local data = ""
-            local partial_data = ""
+            data, partial_data = "", ""
             -- Keep reading data until we have it all
             while partial_data do
                 data = data .. partial_data
@@ -44,61 +59,34 @@ function start_execute(key)
                 -- popen_bgread will call resume on us.
                 partial_data = coroutine.yield()
             end
-        
-            -- remove all but the last line
-            data = string.gsub(data, "\n$", "")
-            data = string.gsub(data, ".*\n", "");
-        
-            local sets = dzen_tab[key]
-            local t_key = sets.t
-            local t_length = sets.meter_length
-        
-            if not t_length or t_length < 1 then
-                t_length = string.len(data)
-            end
 
-            if mpd_pipe == nil then
-                mpd_pipe = io.popen("dzen2 -dock -bg ".. 
-                                    neg.dzen.bg_ ..
-                                    " -h ".. neg.dzen.h_ ..
-                                    " -tw 0 -x 0 -ta r -w ".. 
-                                    neg.dzen.mpd_w_ .. 
-                                    " -p -fn ".. 
-                                    neg.dzen.panel_font_ .. 
-                                    " ", "w"
-                                    )
-                mpd_pipe:setvbuf("line")
-            end
-            mpc = data
-            mpd_dzen_update()
+            if remove_lines then remove_all_but_last_line(data) end
+        
+            local ldata = data
+            pipe_write(ldata)
 
             -- If no updates, then just schedule another run
             if not data or data == "" then
                 return timers[key]:set(dzen_tab[key].delay, callbacks[key])
             end
-        
-            return timers[key]:set(
-                    dzen_tab[key].delay,
-                    callbacks[key])
-                    end
-        )
-
-        -- We need to pass it the key before calling a background call.
-        coroutine.resume(handle_output_changes, key)
-        notioncore.popen_bgread(dzen_tab[key].prog,
-                                function(cor) 
-                                    coroutine.resume(handle_output_changes, cor) 
-                                end,
+            return timers[key]:set(dzen_tab[key].delay, callbacks[key])
+        end
+    )
+    -- We need to pass it the key before calling a background call.
+    coroutine.resume(handle_output_changes, key)
+    notioncore.popen_bgread(dzen_tab[key].prog,
+                            function(cor) 
+                                coroutine.resume(handle_output_changes, cor) 
+                            end,
+                            function()
                                 coroutine.wrap(flush_stderr)
-                                )
+                            end
+    )
 end
 
+init_dzen()
 for key in pairs(dzen_tab) do
     timers[key] = notioncore.create_timer()
 	callbacks[key] = loadstring("start_execute("..key..")")
     start_execute(key)
-end
-
-function mpd_dzen_update()
-    mpd_pipe:write(mpc..'\n')
 end
