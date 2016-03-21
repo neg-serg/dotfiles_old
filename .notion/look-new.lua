ae31275a24b2ae62103b8fde6ac9f1c22995cf89
Neg <serg.zorg@gmail.com>
2016-03-21 11:20:34 +0300

    [notion] look-new cleanup

--- .notion/look-new.lua
+++ .notion/look-new.lua
@@ -1,32 +1,4 @@
--- local utf8 = require 'lua-utf8'
-
-neg      = {}
-neg.dzen = {}
-neg.rofi = {}
-
-neg.font       = "PragmataPro for Powerline"
-neg.font_size  = "12"
-neg.basefont   = "xft:".. neg.font .."-"..neg.font_size..":bold"
-neg.statusfont = "xft:".. neg.font .."-"..neg.font_size..":bold"
-
-neg.padding_color     = "#000000"
-neg.def_bg            = "#000000"
-neg.def_fg            = "#666666"
-neg.scratchpad_border = "#1F3B4F"
-neg.frame_def_border  = "#000000"
-
-neg.rofi.bg   = "\"#000000\""
-neg.rofi.fg   = "\"#666666\""
-neg.rofi.hlfg = "\"#aaaaaa\""
-neg.rofi.hlbg = "\"#194558\""
-neg.rofi.bc   = "\"#202020\""
-
-neg.dzen.panel_font_ = "'".. neg.font..":style=bold:size=".. neg.font_size .."'"
-neg.dzen.bg_         = '"#000000"'
-neg.dzen.h_          = 19
-neg.dzen.main_w_     = "1000"
-neg.dzen.mpd_w_      = "910"
-
+dopath("style_settings.lua")
 if not gr.select_engine("de") then return end
 de.reset()
 
