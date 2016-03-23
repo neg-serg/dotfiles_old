d50c6a3f1573058bb893120be70d3f1f8b6a5996
Neg <serg.zorg@gmail.com>
2016-03-24 00:08:19 +0300

    [zsh] store imgur output
    Former-commit-id: fdde859315924a3fb2a6620c7b034d2f9ab8f1ee [formerly 622d477861de403eb6d6cb9b1cc8ed13aedf8c7e] [formerly f3afade082c992773a72a02b72b6052a4a040f48 [formerly 52327ebed607e3c6ac4ad2f6a5f595d8427ccf36]]
    Former-commit-id: 224c4386e61e00e3dd2e6580163633294eeda5b0 [formerly 7c186b8f811aa72216cdff42b50cd7ef05c72554]
    Former-commit-id: b12bee7f86115d9dc5866b9c6b16b3030d1740c3

--- .zsh/05-functions.zsh
+++ .zsh/05-functions.zsh
@@ -786,10 +786,10 @@ function img(){
     if [[ $# == 0 ]]; then
         imgur -h
     elif [[ $# == 1 ]]; then
-        imgur $(gpg -dq ~/.imgur_pass.gpg) upload $1
+        imgur $(gpg -dq ~/.imgur_pass.gpg) upload $1 | tee -a ~/tmp/imgur_output_
     else
         imgur_command="$1"; shift
-        imgur $(gpg -dq ~/.imgur_pass.gpg) "$imgur_command" "$@"
+        imgur $(gpg -dq ~/.imgur_pass.gpg) "$imgur_command" "$@" | tee -a ~/tmp/imgur_output_
         unset imgur_command
     fi
 }
