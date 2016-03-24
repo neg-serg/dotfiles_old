85a88dc6d66ea66632f3fbef685da7a649adf57d
Neg <serg.zorg@gmail.com>
2016-03-24 23:51:44 +0300

    [notion] rules cleanup

--- .notion/cfg_rules.lua
+++ .notion/cfg_rules.lua
@@ -25,8 +25,8 @@ defwinprop{class="Firefox",role="Manager",instance="Download",transient_mode="of
 defwinprop{class="Firefox",instance="Dialog",float=true}
 defwinprop{class="Firefox",role="Organizer",target="float2"}
 defwinprop{class="Firefox",instance="firefox",role="GtkFileChooserDialog",
-    -- max_size = {w=1024,h=768},
-    -- min_size = {w=800,h=600},
+    max_size = {w=1024,h=768},
+    min_size = {w=800,h=600},
     float=true,
 }
 defwinprop{class="Conkeror",instance="Navigator",transient_mode="off",target="web",tag="www"}
@@ -87,29 +87,28 @@ tag="editor"
 defwinprop{class="Emacs",instance="emacs",target="dev",lazyresize=true,jumpto=true, tag="geditor"}
 defwinprop{class="Emacs",instance="emacs",name="Question",float=true, tag="geditor"}
 defwinprop{class="com-sun-javaws-Main",instance="sun-awt-X11-XFramePeer",target="topcoder"}
-
+-----------------------------------[[ IDEA ]]--------------------------------------
 defwinprop{class="jetbrains-idea",instance="*",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-idea-ce",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-idea",instance="sun-awt-X11-XWindowPeer",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-idea",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-idea",instance="sun-awt-X11-XFramePeer",target="jetbrains",floating=true,transient_mode="off",tag="ide"}
-
+----------------------------------[[ WEBSTORM ]]-----------------------------------
 defwinprop{class="jetbrains-webstorm",instance="*",target="jetbrains",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-webstorm-ce",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-webstorm",instance="sun-awt-X11-XWindowPeer",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-webstorm",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-webstorm",instance="sun-awt-X11-XFramePeer",target="jetbrains",floating=true,transient_mode="off",tag="ide"}
-
+----------------------------------[[  CLION   ]]-----------------------------------
 defwinprop{class="jetbrains-clion",instance="*",target="jetbrains",target="jetbrains",floating=true,transient_mode="current",tag="ide"}
 defwinprop{class="jetbrains-clion",instance="sun-awt-X11-XWindowPeer",target="jetbrains",floating=true,transient_mode="current",target="jetbrains",tag="ide"}
 defwinprop{class="jetbrains-clion",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current",target="jetbrains",tag="ide"}
 defwinprop{class="jetbrains-clion",instance="sun-awt-X11-XFramePeer",target="jetbrains",floating=true,transient_mode="off",target="jetbrains",tag="ide"}
-
+-------------------------------[[ ANDROID STUDIO ]]--------------------------------
 defwinprop{class="jetbrains-android-studio",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current", tag="ide"}
 defwinprop{class="jetbrains-android-studio",instance="sun-awt-X11-XWindowPeer",target="jetbrains",floating=true,transient_mode="current", tag="ide"}
 defwinprop{class="jetbrains-android-studio",instance="sun-awt-X11-XDialogPeer",target="jetbrains",floating=true,transient_mode="current", tag="ide"}
 defwinprop{class="jetbrains-android-studio",instance="sun-awt-X11-XFramePeer",target="jetbrains",floating=true,transient_mode="off", tag="ide"}
-
 defwinprop{class="UE4Editor",floating=true,transient_mode="off",target="unreal",tag="unreal"}
 -------------------------------------[[ VM ]]--------------------------------------
 defwinprop{class="vmware",instance="Vmware",target="vm",jumpto=true,transient_mode="off",float=true,tag="virt"}
@@ -138,14 +137,23 @@ defwinprop{instance="glances_term",target="admin",jumpto=true,tag="admin"}
 defwinprop{instance="gparted",target="admin",jumpto=true,tag="admin"}
 defwinprop{instance="gnome-disks",target="admin",jumpto=true,tag="admin"}
 defwinprop{instance="seahorse",target="admin",jumpto=true,tag="admin"}
+-------------------------------------[[  WINE  ]]------------------------------------
+defwinprop{class="Crossover",target="wine",jumpto=true,tag="virt",float=true}
+defwinprop{class="Wine",target="wine",jumpto=true,tag="virt"}
+defwinprop{name=".*.exe",target="wine",jumpto=true,tag="virt"}
+defwinprop{name="wine.*",target="wine",jumpto=true,tag="virt"}
+defwinprop{name="explorer.exe",target="wine",jumpto=true,tag="virt"}
+defwinprop{name="Explorer.exe",target="wine",jumpto=true,tag="virt"}
+defwinprop{class="explorer.exe",target="wine",jumpto=true,tag="virt"}
+defwinprop{class="Explorer.exe",target="wine",jumpto=true,tag="virt"}
+-------------------------------------[[ TORRENT ]]-----------------------------------
+defwinprop{class="Ktorrent",winlist_ignore=true,transient_mode="off",instance="ktorrent",target="torrent",tag="torrent"}
+defwinprop{class="Vuze",winlist_ignore=true,transient_mode="off",target="torrent",tag="torrent",float=false}
+defwinprop{class="Vuze",is_transient=true,winlist_ignore=true,transient_mode="off",target="torrent",tag="torrent",float=true}
 -------------------------------------[[  ETC  ]]-------------------------------------
 defwinprop{class="Xfce*",float=true}
 defwinprop{class="Xmessage",float=true}
 defwinprop{class="com-sun-javaws-Main",float=true}
-defwinprop{class="Crossover",target="wine",jumpto=true,tag="virt",float=true}
-defwinprop{class="Wine",target="wine",jumpto=true,tag="virt",float=true}
-defwinprop{name="*.exe",target="wine",jumpto=true,tag="virt",float=true}
-defwinprop{name="[eE]xplorer.exe",target="wine",jumpto=true,tag="virt",float=true}
 defwinprop{class="Steam",target="wine",jumpto=true,tag="virt"}
 defwinprop{instance="recoll",jumpto=false,winlist_ignore=true,target="search"}
 defwinprop{instance="stardict",jumpto=true,winlist_ignore=true,transient_mode="off",winlist_ignore=true,target="float"}
@@ -168,12 +176,8 @@ defwinprop{instance="console",winlist_ignore=true,transient_mode="off",target="c
 defwinprop{instance="gdb",winlist_ignore=true,transient_mode="off",target="gdb"}
 defwinprop{instance="mixer",winlist_ignore=true,transient_mode="off",target="alsa"}
 defwinprop{instance="htop",winlist_ignore=true,transient_mode="off",target="top"}
-defwinprop{instance="gcolor2",winlist_ignore=true,transient_mode="off",float=true}
-defwinprop{instance="gpick",winlist_ignore=true,transient_mode="off",target=float}
-defwinprop{class="Ktorrent",winlist_ignore=true,transient_mode="off",instance="ktorrent",target="torrent",tag="torrent"}
-defwinprop{class="Vuze",winlist_ignore=true,transient_mode="off",target="torrent",tag="torrent",float=false}
-defwinprop{class="Vuze",is_transient=true,winlist_ignore=true,transient_mode="off",target="torrent",tag="torrent",float=true}
-defwinprop{class="Anamnesis",winlist_ignore=true,transient_mode="off",target="float2",jumpto=true}
+defwinprop{instance="gcolor2",winlist_ignore=true,transient_mode="off",target="float"}
+defwinprop{instance="gpick",winlist_ignore=true,transient_mode="off",target="float"}
 defwinprop{class="Nicotine",instance="nicotine",transient_mode="off",target="float2",tag="soulseek"}
 defwinprop{class="Nicotine.py",instance="nicotine.py",transient_mode="off",target="float2",tag="soulseek"}
 defwinprop{class="*",instance="*",transparent=false}
