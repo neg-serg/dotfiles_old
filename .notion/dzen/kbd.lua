7b6b7251029aaf5e55f15075289d8e6ec8b2de1b
Neg <serg.zorg@gmail.com>
2016-04-02 13:29:46 +0300

    [notion] new layout printer

--- .notion/dzen/kbd.lua
+++ .notion/dzen/kbd.lua
@@ -1,6 +1,6 @@
 function kbd_update()
     local klay f = nil t = ""
-    f = io.popen(home_.."/bin/mon/klay2", "r")
+    f = io.popen(home_.."/bin/mon/klay3", "r")
     klay = f:read("*l") f:close()
     t = t..klay
     dmain.kbd = t
