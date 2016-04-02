27e3af163aca7b66a67b1ac723e4eb79ba5ab265
Neg <serg.zorg@gmail.com>
2016-04-02 13:20:04 +0300

    [zsh] better exit status

--- .zsh/04-oldprompt.zsh
+++ .zsh/04-oldprompt.zsh
@@ -4,6 +4,49 @@ local _lambda_ok="" # "%{$fg_bold[green]%}=λ\\"
 local _lambda_err="%{$fg_bold[red]%}=λ\\"
 local _lambda="%(?,${_lambda_ok},${_lambda_err})%{${reset_color}%}"
 
+function nice_exit_code() {
+    local exit_status="${1:-$(print -P %?)}";
+    # nothing to do here
+    [[ -z $exit_status || $exit_status == 0 ]] && return;
+
+    local sig_name;
+    
+    # is this a signal name (error code = signal + 128) ?
+    case $exit_status in
+        129)  sig_name=HUP ;;
+        130)  sig_name=INT ;;
+        131)  sig_name=QUIT ;;
+        132)  sig_name=ILL ;;
+        134)  sig_name=ABRT ;;
+        136)  sig_name=FPE ;;
+        137)  sig_name=KILL ;;
+        139)  sig_name=SEGV ;;
+        141)  sig_name=PIPE ;;
+        143)  sig_name=TERM ;;
+    esac
+
+    # usual exit codes
+    case $exit_status in
+        -1)   sig_name=FATAL ;;
+        1)    sig_name=WARN ;; # Miscellaneous errors, such as "divide by zero"
+        2)    sig_name=BUILTINMISUSE ;; # misuse of shell builtins (pretty rare)
+        126)  sig_name=CCANNOTINVOKE ;; # cannot invoke requested command (ex : source script_with_syntax_error)
+        127)  sig_name=CNOTFOUND ;; # command not found (ex : source script_not_existing)
+    esac
+
+    # assuming we are on an x86 system here
+    # this MIGHT get annoying since those are in a range of exit codes
+    # programs sometimes use.... we'll see.
+    case $exit_status in
+        19)  sig_name=STOP ;;
+        20)  sig_name=TSTP ;;
+        21)  sig_name=TTIN ;;
+        22)  sig_name=TTOU ;;
+    esac
+
+    echo "%{$fg[blue]%}[%{$fg[white]%}$ZSH_PROMPT_EXIT_SIGNAL_PREFIX${exit_status}:${sig_name:-$exit_status}%{$fg[blue]%}]%{$reset_color%}"
+}
+
 case ${UID} in
 0)
     PROMPT="%{$fg[red]%}<%T>%{$fg[red]%}[root@%m] %(!.#.$) %{${reset_color}%}%{${fg[red]}%}[%~]%{${reset_color}%} "
@@ -41,6 +84,6 @@ case ${UID} in
         echo "${${KEYMAP/vicmd/${mode_ind}}/(main|viins)/}"
     }
     # define right prompt, if it wasn't defined by a theme
-    [[ "${RPS1}" == "" && "${RPROMPT}" == "" ]] && RPS1='${_lambda}$(vi_mode_prompt_info)'
+    [[ "${RPS1}" == "" && "${RPROMPT}" == "" ]] && RPS1='$(nice_exit_code)${_lambda}$(vi_mode_prompt_info)'
     ;;
 esac
