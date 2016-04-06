7c650cd8c578cd151cc65a47b3d7fa49b9b35016
Neg <serg.zorg@gmail.com>
2016-04-06 23:00:40 +0300

    [scripts] add script to start jetbrains ide
    Former-commit-id: d1b9f0de4d60d77acba16c7da921889e3167067e [formerly 3ff91133233f985824d8552818e658b03842fd58] [formerly 95c304ce6c7e7051dbc4b7113fb35fb8182e608a [formerly 4c5c29bebb7f0f8a46e0452c51bb8df7bf897419]]
    Former-commit-id: 7f23a45e70ae6aa984ad235702be61bf7eb32f67 [formerly ee4210f653d767fc0b1d5f8198290f74dacba0bf]
    Former-commit-id: f986d061612926e5d98be6cbc53f19593cb66bd0

---
+++ bin/scripts/jetbrains.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+function contains_element () {
+    local e
+    for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+    return 1
+}
+
+function main(){
+    local dist_dir="/one/dist"
+    local jetbrain_products=(idea pycharm rubymine webstorm clion)
+    for i in ${jetbrain_products[@]}; do
+        jetbrains_path="${dist_dir}/${i}/bin/${i}.sh"
+        if readlink -fq ${jetbrains_path}>/dev/null; then
+            alias "$i=${jetbrains_path} > /dev/shm/${i}$$ &" 
+        fi
+    done
+    if contains_element "$1" ${jetbrain_products[@]}; then
+        eval "$1"
+    fi
+}
+
+main $1
