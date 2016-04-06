eaa2308c9fb4fbe409078f690ba274e80e34f754
Neg <serg.zorg@gmail.com>
2016-04-06 20:52:25 +0300

    [notion] fix init problem
    Former-commit-id: 636555c2ecbcca43e52543d848511bc492d79e14 [formerly a12ec869a7a72000975c89617d85f92281836b8d] [formerly b8a63b643edc0f1d29bd38372da966029eef4a91 [formerly 0eccd542544741a12dd0be92a5cbd792bc2aa8d5]]
    Former-commit-id: 5a66e39741dd63f0f5d2bf963461e5e140339272 [formerly 21b268ba80560384df8835ef7790976070c1d86f]
    Former-commit-id: 4db9f90d710ed97b5d6646cd2086220f0547d2f5

--- .notion/dzen/kbd.lua
+++ .notion/dzen/kbd.lua
@@ -1,5 +1,7 @@
 function kbd_update()
-    local klay f = nil t = ""
+    local klay
+    local f = nil
+    local t = ""
     f = io.popen(home_.."/bin/mon/klay3", "r")
     klay = f:read("*l") f:close()
     t = t..klay
