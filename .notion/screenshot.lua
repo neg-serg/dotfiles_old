e7b5fad3e38ed17b4e2ddb71c0b63514ca801d37
Neg <serg.zorg@gmail.com>
2016-04-16 18:27:31 +0300

    [notion] better screenshot notify
    Former-commit-id: 416ac4b0e0bd46235a4a9e597f9248f73d6fec74 [formerly 7e53666fd48062217acd695bc252d205b9d6cc45] [formerly 94eda19e8cef7a99210e5a665549da632006a559 [formerly 61f936f4e34b5ea066572c610ba148d15bc439b4]]
    Former-commit-id: 4bec00f76ff77224e89768942290a8a285a70398 [formerly 8dc07d934f952d410641daca86f1e9ec229e2633]
    Former-commit-id: 46e47fc8f99a8b74b2f220eb4633aa99d42fb425

--- .notion/screenshot.lua
+++ .notion/screenshot.lua
@@ -12,14 +12,15 @@ function screenshot_filename(postfix)
 end
 
 function make_screenshot(windowname, filename)
-   notioncore.exec('/usr/bin/import -window "' .. windowname .. '"' ..
-                   ' \'' .. filename .. '\'')
-   local echo_str = '[ Scr :: { scr_' .. datetime .. '.png} ]'
-   local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
+    local scr_pipe = io.popen('/usr/bin/import -window "' .. windowname .. '"' ..
+    ' \'' .. filename .. '\'; echo $?')
+    scr_pipe:close()
+    local echo_str = '[ Scr :: { scr_' .. datetime .. '.png} ]'
+    local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
 end
 
 function make_root_screenshot()
-   make_screenshot('root', screenshot_filename(''))
+    make_screenshot('root',screenshot_filename(''))
 end
 
 -- TODO: add non-acsii window name support
@@ -33,4 +34,31 @@ function make_current_window_screenshot_with_windowname()
    local safe_winname = string.gsub(windowname, "(/)", "|")
    local scrshot_name_with_winname = screenshot_filename('[ ' .. safe_winname .. ' ]')
    make_screenshot(windowname, scrshot_name_with_winname)
+end
+
+function make_scr()
+    local postfix = ""
+    local filename = screenshot_path .. '/' .. prefix .. datetime .. postfix .. extension
+    local screen_bg_pipe = 0xDEADBEEF
+    -- Read output from the program and (if data) then update the statusbar
+    local handle_output_changes = coroutine.create(
+    function(input_data)
+        local key = input_data
+        local data, partial_data = "", ""
+        -- Keep reading data until we have it all
+        while partial_data do
+            data = data .. partial_data
+            -- Yield until we get more input.
+            -- popen_bgread will call resume on us.
+            partial_data = coroutine.yield()
+        end
+        local datetime = os.date("%Y_%m_%d-%H_%M_%S")
+        local echo_str = '[ Scr :: { scr_' .. datetime .. '.png} ]'
+        local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
+    end
+    )
+    coroutine.resume(handle_output_changes, key)
+    notioncore.popen_bgread('/usr/bin/import -window "root" '..filename,
+        function(cor) coroutine.resume(handle_output_changes, cor) end
+    )
 end
