81eba90b24694349c9d532dc8e5f3632d4b6befd
Neg <serg.zorg@gmail.com>
2016-04-16 20:46:06 +0300

    [notion] screenshot script refactoring

--- .notion/screenshot.lua
+++ .notion/screenshot.lua
@@ -1,64 +1,40 @@
-local prefix = "scr_"
-local extension = ".png"
-local screenshot_path = "/tmp"
-local datetime = ""
+require "lfs"
 
-function screenshot_filename(postfix)
-   datetime = os.date("%Y_%m_%d-%H_%M_%S")
-   local filename = screenshot_path .. '/' .. prefix .. datetime .. postfix .. extension
-   local echo_str = '[ Scr :: {' ..  prefix .. datetime .. postfix .. extension ..'} ]'
-   local fd = io.popen("zsh -c \"echo '".. filename .."'|xsel -i\"") fd:close()
-   return filename
-end
+local screenshot_path = os.getenv("HOME").."/tmp/shots"
+lfs.mkdir(screenshot_path)
 
-function make_screenshot(windowname, filename)
-    local scr_pipe = io.popen('/usr/bin/import -window "' .. windowname .. '"' ..
-    ' \'' .. filename .. '\'; echo $?')
-    scr_pipe:close()
-    local echo_str = '[ Scr :: { scr_' .. datetime .. '.png} ]'
-    local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
-end
-
-function make_root_screenshot()
-    make_screenshot('root',screenshot_filename(''))
-end
-
--- TODO: add non-acsii window name support
-function make_current_window_screenshot()
-   local windowname = WRegion.name(notioncore.current())
-   make_screenshot(windowname, screenshot_filename(''))
-end
-
-function make_current_window_screenshot_with_windowname()
-   local windowname = WRegion.name(notioncore.current())
-   local safe_winname = string.gsub(windowname, "(/)", "|")
-   local scrshot_name_with_winname = screenshot_filename('[ ' .. safe_winname .. ' ]')
-   make_screenshot(windowname, scrshot_name_with_winname)
-end
-
-function make_scr()
-    local postfix = ""
+function scr_info(postfix)
+    local prefix = "scr_"
+    local extension = ".png"
+    local datetime = os.date("%Y_%m_%d-%H_%M_%S")
     local filename = screenshot_path .. '/' .. prefix .. datetime .. postfix .. extension
-    local screen_bg_pipe = 0xDEADBEEF
-    -- Read output from the program and (if data) then update the statusbar
-    local handle_output_changes = coroutine.create(
-    function(input_data)
-        local key = input_data
-        local data, partial_data = "", ""
-        -- Keep reading data until we have it all
-        while partial_data do
-            data = data .. partial_data
-            -- Yield until we get more input.
-            -- popen_bgread will call resume on us.
-            partial_data = coroutine.yield()
+    local msg = '[ Scr :: {' ..  prefix .. datetime .. postfix .. extension ..'} ]'
+    local fd = io.popen("zsh -c \"echo '".. filename .."'|xsel -i\"") fd:close()
+    return {["filename"]=filename,["msg"]=msg}
+end
+
+function make_scr_cur()
+    local windowname=WRegion.name(notioncore.current())
+    local safe_winname=string.gsub(windowname, "(/)", "|")
+    local scrshot_name_with_winname=screenshot_filename('[ '..safe_winname..' ]')
+    make_scr(windowname, scrshot_name_with_winname)
+end
+
+function make_scr(wname)
+    if wname == nil or wname == '' then wname = 'root' end
+    local si = scr_info('')
+    local filename = si["filename"]
+    local stdout_cor = coroutine.create(
+        function(input_data)
+            local key=input_data
+            local data, pdata = "", ""
+            while pdata do data=data..pdata pdata = coroutine.yield() end
+            local msg = si["msg"]
+            local fd = io.popen("notify-send '"..msg.."'") fd:close()
         end
-        local datetime = os.date("%Y_%m_%d-%H_%M_%S")
-        local echo_str = '[ Scr :: { scr_' .. datetime .. '.png} ]'
-        local fd = io.popen("notify-send '".. echo_str .."'") fd:close()
-    end
     )
-    coroutine.resume(handle_output_changes, key)
-    notioncore.popen_bgread('/usr/bin/import -window "root" '..filename,
-        function(cor) coroutine.resume(handle_output_changes, cor) end
+    coroutine.resume(stdout_cor, key)
+    notioncore.popen_bgread('/usr/bin/import -window "'..wname..'" '..filename,
+        function(_) coroutine.resume(stdout_cor, _) end
     )
 end
