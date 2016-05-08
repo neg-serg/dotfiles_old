951cdf4c28e6fa790d1c04ec462d48b3b3596b08
Neg <serg.zorg@gmail.com>
2016-05-08 20:15:24 +0300

    [notion] fix dzen/kbd

--- .notion/dzen/kbd.lua
+++ .notion/dzen/kbd.lua
@@ -2,7 +2,7 @@ function kbd_update()
     local f = io.popen(home_.."/bin/mon/klay3", "r")
     local klay = f:read("*l") f:close()
     local t = ""
-    if klay ~= nil then t = t..klay end
+    t = t..klay
     dmain.kbd = t
     dzen_update()
 end
