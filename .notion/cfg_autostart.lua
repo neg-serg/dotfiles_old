e5bd2324502643579297b09d6c4815f141098c9a
Neg <serg.zorg@gmail.com>
2016-05-10 19:39:06 +0300

    [notion] rofi daemon mode is not supported anymore
    Former-commit-id: 9099ff26d5da6f417ab20513045924a508ac7caf [formerly 5d29139f6407bada1a70f639831236221284b3a7] [formerly c977ae2b47c46e31a451d932d3a490e3c1b63c80 [formerly 5e95bd062db6298e43848bc24b521ba8b49c4021]]
    Former-commit-id: 48b2bcc614490e3b781ad22105b9f9708f7da6ec [formerly dad2fc96a6b317916731b96c40faf1213582c009]
    Former-commit-id: 28ca132650d3850686aef3ca45201baaf6fec92a

--- .notion/cfg_autostart.lua
+++ .notion/cfg_autostart.lua
@@ -1,11 +1,14 @@
-function start_rofi()
-    local colors = ' -fg '..neg.rofi.fg..' -bg '..neg.rofi.bg..' -hlfg '..neg.rofi.hlfg..' -hlbg '..neg.rofi.hlbg..' -bc '..neg.rofi.bc
-    notioncore.exec('/usr/bin/rofi ' .. rofi.font() ..  
-                                        colors .. 
-                                        ' -b -disable-history -columns 10 -lines 1 -width '
-                                        ..rofi.width..
-                                        ' -yoffset -22 -key-run Alt-grave -location 6'
-                                        .. rofi.pid
-                   )
+rofi_daemon = false
+if (rofi_daemon) then
+    function start_rofi()
+        local colors = ' -fg '..neg.rofi.fg..' -bg '..neg.rofi.bg..' -hlfg '..neg.rofi.hlfg..' -hlbg '..neg.rofi.hlbg..' -bc '..neg.rofi.bc
+        notioncore.exec('/usr/bin/rofi ' .. rofi.font() ..  
+                                            colors .. 
+                                            ' -b -disable-history -columns 10 -lines 1 -width '
+                                            ..rofi.width..
+                                            ' -yoffset -22 -key-run Alt-grave -location 6 '
+                                            -- .. rofi.pid
+                    )
+    end
+    start_rofi()
 end
-start_rofi()
