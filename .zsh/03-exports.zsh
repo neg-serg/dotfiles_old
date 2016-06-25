6365a10110b4b82d7ca65051a0d80446a2d27ae2
Neg <serg.zorg@gmail.com>
2016-06-25 23:25:58 +0300

    [zsh] update drip binding

--- .zsh/03-exports.zsh
+++ .zsh/03-exports.zsh
@@ -133,7 +133,11 @@ export _JAVA_AWT_WM_NONREPARENTING=1
 export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 _SILENT_JAVA_OPTIONS="${_JAVA_OPTIONS}"
 unset _JAVA_OPTIONS
-export JAVACMD=drip
+
+if which drip > /dev/null 2>&1; then
+  export DRIP_SHUTDOWN=30
+  export JAVACMD=$(which drip)
+fi
 
 export FZF_DEFAULT_COMMAND='ag -l -g ""'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
