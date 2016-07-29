1584e0dd79600f49000b03d81f4a5140ec0be0e6
Neg <serg.zorg@gmail.com>
2016-07-29 15:03:53 +0300

    [bin] I've missed local
    Former-commit-id: 9dcd2c05b991a859ae9fca8c0dbb977a26346629 [formerly b2a0e1b3edd2e879a67858f6057d69c716cf7d56] [formerly aa18c3288627e2f99a8bfc35bda92a1e0cff9e88 [formerly 74c745afa872546dc7dbd0faaf215e9d4269fb81]]
    Former-commit-id: e2842a0d8d2a0de821c1c9111362ceeaebc2203e [formerly cb74be312d2b3bfd8087a59e7795bc0725dec7e8]
    Former-commit-id: 17cb81606fe583b676affa32408a4a49083744a2

--- bin/mpd_sel.sh
+++ bin/mpd_sel.sh
@@ -1,15 +1,15 @@
 #!/bin/zsh
 
 function main(){
-    font="Pragmata Pro for Powerline bold 12"
-    mon_width_=$(xrandr -q |awk '/Screen/{print $8}')
-    gap_="70"
-    rofi_='rofi -font "${font}" -yoffset -22 -location 6 -width $((${mon_width_} - ${gap_})) -lines 10 -dmenu -p "[>>]"'
-    artist_=$( mpc ls | eval ${rofi_})
+    local font="Pragmata Pro for Powerline bold 12"
+    local mon_width_=$(xrandr -q |awk '/Screen/{print $8}')
+    local gap_="70"
+    local rofi_='rofi -font "${font}" -yoffset -22 -location 6 -width $((${mon_width_} - ${gap_})) -lines 10 -dmenu -p "[>>]"'
+    local artist_=$( mpc ls | eval ${rofi_})
 
     [[ ${artist_} == "" ]] && exit 1
 
-    album_=$( echo -e "Play All\n$( mpc ls "${artist_}" )" | eval ${rofi_})
+    local album_=$( echo -e "Play All\n$( mpc ls "${artist_}" )" | eval ${rofi_})
     if [[ ${album_} == "" ]]; then
         exit 1
     else
