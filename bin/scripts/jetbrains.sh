610c5bbf06cd7ecfc2dac621d19ef54ffc6e9fe4
Neg <serg.zorg@gmail.com>
2016-08-08 12:37:59 +0300

    [jetbrains] fix runner for nvme
    Former-commit-id: cb063ee532bb813512ed3c60340c1b8477e430c9 [formerly 2548a33ecae20c1f7be44134c2f8d0eb6f171598] [formerly a95f37e358f2554f8a9dffa928b943e0231f698a [formerly 833149927805e79d371d3a225b9f747f820b6d82]]
    Former-commit-id: 316c17aa82d76ae82408a27cff7716ff21aa39da [formerly ff80e8759b056069b8dc5f19136b42bc923db9b1]
    Former-commit-id: d88903ce37ce0f80605b22133ad4b5b0e308d307

--- bin/scripts/jetbrains.sh
+++ bin/scripts/jetbrains.sh
@@ -11,8 +11,12 @@ function main(){
     local jetbrain_products=(idea pycharm rubymine webstorm clion)
     for i in ${jetbrain_products[@]}; do
         jetbrains_path="${dist_dir}/${i}/bin/${i}.sh"
+        jetbrains_path_alter="/opt/${i}/bin/${i}.sh"
         if readlink -fq ${jetbrains_path}>/dev/null; then
             alias "$i=${jetbrains_path} > /dev/shm/${i}$$ &" 
+        fi
+        if readlink -fq ${jetbrains_path_alter} > /dev/null; then
+            alias "$i=${jetbrains_path_alter} > /dev/shm/${i}$$ &" 
         fi
     done
     if contains_element "$1" ${jetbrain_products[@]}; then
