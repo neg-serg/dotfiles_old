ffc8e0820adba6d6acacbcd0e388e0b7e0e23c4d
Neg <serg.zorg@gmail.com>
2016-09-13 19:40:38 +0300

    [zsh] add strip trailing workspaces from vim
    Former-commit-id: d7d1241b80a5c293f6cfd68e50fbe822f81cc5c6 [formerly 51c9362648bba4da6c5addf1c0f3215907749651] [formerly b6fb190e47c60bce9f0b27e72c563830aa02686f [formerly 4b7ce3a2ac7a4cac358e649de0513da1b2d3f0f0]]
    Former-commit-id: a43c7e90814e72b5a3fec8ea96e0659a4ff49603 [formerly f6555616f06e0655a9e6552f70bbcac7429a9f20]
    Former-commit-id: 4fa83dc99d8ac02edc99aef5645aef8819d7097c

--- .zsh/05-functions.zsh
+++ .zsh/05-functions.zsh
@@ -379,3 +379,4 @@ function clojure(){
 # this is similar to cut(1) but using awk(1) fields:
 # print only the given columns, numbered from 1 to N
 kut() { awk "{ print $(for n; do echo -n "\$$n,"; done | sed 's/,$//') }" ;}
+strip_trailing_workspaces(){  sed ${1:+-i} 's/\s\+$//' "$@" }
