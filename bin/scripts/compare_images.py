18cb86c273a71de14b762a407fb8f06db3c94865
Neg <serg.zorg@gmail.com>
2016-10-03 19:33:45 +0300

    [bin/scripts] cleanup compare_images
    Former-commit-id: 1b01566d240678b82a934f95b0ce13fdb032e36f [formerly 97930647e648fd7ed501c9fe402e165e39f7f34e] [formerly 7775b97fc6cb86f7bcc0891d03de0daa15fd5bf8 [formerly 1ca67c050ea9b9a801f5afdf1fc01a479c3334d8]]
    Former-commit-id: 95f99afe656dd2034c53c8c1913b421fbdf3b088 [formerly 419f7d63f8bbe648b55ce1f8ca85545719b4de61]
    Former-commit-id: 20ccb0f0fd59896f94af9510ddc81d94c6a43844

--- bin/scripts/compare_images.py
+++ bin/scripts/compare_images.py
@@ -4,8 +4,7 @@ from PIL import Image
 import imagehash
 import sys
 
-h1      = imagehash.average_hash(Image.open(sys.argv[1]))
+h1 = imagehash.average_hash(Image.open(sys.argv[1]))
 h2 = imagehash.average_hash(Image.open(sys.argv[2]))
 
-# print(h1, " ", h2, " compare: ", h1 == h2, " diff:", h1 - h2)
 print(h1 == h2)
