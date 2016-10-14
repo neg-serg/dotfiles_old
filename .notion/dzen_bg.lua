440f1a0bf290864b3eb3ce3d0e134babe22a1174
Neg <serg.zorg@gmail.com>
2016-10-14 17:15:13 +0300

    [notion] cleanup dzen_bg
    Former-commit-id: 201dc68e9217d5e630bfb52ccd4edd572857f0be [formerly 3d1b696b24695d02742f2a5920e5282d690f79ef] [formerly 39ce5ebb33fdad73a40743efc4228c1f65753966 [formerly 3c6d7fd0a91e12fc061c27f073a1ce518976c368]]
    Former-commit-id: 8362393ae168760d480cabec30d1132f2a5e5448 [formerly 77b58e25d459f0d278d0d252c6902113b1acd7a3]
    Former-commit-id: 3cee178a1b3a91645e8f887a33ac27d7ab067a8f

--- .notion/dzen_bg.lua
+++ .notion/dzen_bg.lua
@@ -3,10 +3,7 @@ local remove_lines = false
 gdata = ""
 
 if not dzen_tab then
-    dzen_tab = {
-        { name="mpd", prog = "mpd.lua", delay = 500},
-        -- { name="mpd", prog = "echo -e olool", delay = 500},
-    }
+    dzen_tab = { { name="mpd", prog = "mpd.lua", delay = 500}, }
 end
 
 -- Just print out stderr for now.
@@ -23,13 +20,13 @@ end
 local function init_dzen()
     if dzen_bg_pipe == nil then
         dzen_bg_pipe = io.popen(
-            "dzen2 -dock -bg ".. 
+            "dzen2 -dock -bg "..
             neg.dzen.bg_ ..
             " -h ".. neg.dzen.h_ ..
-            " -tw 0 -x 0 -ta r -w ".. 
-            neg.dzen.mpd_w_ .. 
-            " -p -fn ".. 
-            neg.dzen.panel_font_ .. 
+            " -tw 0 -x 0 -ta r -w "..
+            neg.dzen.mpd_w_ ..
+            " -p -fn "..
+            neg.dzen.panel_font_ ..
             " ", "w"
         )
         dzen_bg_pipe:setvbuf("line")
@@ -44,6 +41,8 @@ end
 local function remove_all_but_last_line(str)
     str = string.gsub(str, "\n$", "")
     str = string.gsub(str, ".*\n", "")
+    str = string.gsub(str, "(.*)\n.*", "%0")
+    str = string.gsub(str, ".*\n(.*\n.*)", "%1")
 end
 
 -- Execute a program.  This will continuously retrieve output from the program.
@@ -62,33 +61,34 @@ function start_execute(key)
             end
 
             if remove_lines then remove_all_but_last_line(data) end
-        
             gdata = data
             gdata = string.gsub(gdata, "\n", "")
-
+            pipe_write(dzen_bg_pipe, gdata.."\n")
             -- If no updates, then just schedule another run
             if not data or data == "" then
-                return timers[key]:set(dzen_tab[key].delay, callbacks[key])
+                return timers[key]:set(
+                    dzen_tab[key].delay,
+                    function() start_execute(key) end
+                )
             end
-            return timers[key]:set(dzen_tab[key].delay, callbacks[key])
+            return timers[key]:set(
+                    dzen_tab[key].delay,
+                    function() start_execute(key) end
+                )
         end
     )
     -- We need to pass it the key before calling a background call.
     coroutine.resume(handle_output_changes, key)
     notioncore.popen_bgread(dzen_tab[key].prog,
-                            function(cor) 
-                                pipe_write(dzen_bg_pipe,gdata..'\n')
-                                coroutine.resume(handle_output_changes, cor) 
+                            function(cor)
+                                coroutine.resume(handle_output_changes, cor)
                             end,
-                            function()
-                                coroutine.wrap(flush_stderr)
-                            end
+                            coroutine.wrap(flush_stderr)
     )
 end
 
 init_dzen()
 for key in pairs(dzen_tab) do
     timers[key] = notioncore.create_timer()
-	callbacks[key] = loadstring("start_execute("..key..")")
     start_execute(key)
 end
