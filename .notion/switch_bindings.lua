bd14cbb3fcb8668fb758d84df48a67ad5ced5823
Neg <serg.zorg@gmail.com>
2016-10-15 14:32:56 +0300

    [notion] reindent
    Former-commit-id: 9c6680c826b8a7564e7cc85ac2dc33564769801c [formerly 67a31febdc76a89b5d46846eaede247e1419eddb] [formerly 402f4457c86743b26ea10a464f046a95b4f4c95a [formerly 90294946cee2086c8bec41ef79af6197d5484e50]]
    Former-commit-id: cd331014599b984b136b93d626ea0e290300e409 [formerly 04bf178db7c6e97b8187b75790f4d07b5ce772b5]
    Former-commit-id: cee2c79470d7da8eadb23a6e727124f2bbc41558

--- .notion/switch_bindings.lua
+++ .notion/switch_bindings.lua
@@ -17,50 +17,50 @@
 -- second set of bindings
 --
 local alternate = {
-WScreen = {
-kpress(META.."F8", "toggle_bindings()"),
-}
+    WScreen = {
+        kpress(META.."F8", "toggle_bindings()"),
+    }
 }
 
 function toggle_bindings()
-local save = table.copy(notioncore.getbindings(), true)
-local kcb_area
+    local save = table.copy(notioncore.getbindings(), true)
+    local kcb_area
 
--- First empty all the bindings
-for name, bindings in pairs(save) do
-for index, bind in pairs(bindings) do
-if bind.area then
-kcb_area = bind.kcb.."@"..bind.area
-else
-kcb_area = bind.kcb
-end
--- This can most definitely be improved
-if bind.action == "kpress" then
-notioncore.defbindings(name, {kpress(bind.kcb, nil)})
--- "kpress_wairel" appearing as bind.action
-elseif string.find(bind.action, "kpress_wai") then
-notioncore.defbindings(name, {kpress_wait(bind.kcb, nil)})
-elseif bind.action == "mpress" then
-notioncore.defbindings(name, {mpress(kcb_area, nil)})
-elseif bind.action == "mdrag" then
-notioncore.defbindings(name, {mdrag(kcb_area, nil)})
-elseif bind.action == "mclick" then
-notioncore.defbindings(name, {mclick(kcb_area, nil)})
-elseif bind.action == "mdblclick" then
-notioncore.defbindings(name, {mdblclick(kcb_area, nil)})
-end
-end
-end
+    -- First empty all the bindings
+    for name, bindings in pairs(save) do
+        for index, bind in pairs(bindings) do
+            if bind.area then
+                kcb_area = bind.kcb.."@"..bind.area
+            else
+                kcb_area = bind.kcb
+            end
+            -- This can most definitely be improved
+            if bind.action == "kpress" then
+                notioncore.defbindings(name, {kpress(bind.kcb, nil)})
+                -- "kpress_wairel" appearing as bind.action
+            elseif string.find(bind.action, "kpress_wai") then
+                notioncore.defbindings(name, {kpress_wait(bind.kcb, nil)})
+            elseif bind.action == "mpress" then
+                notioncore.defbindings(name, {mpress(kcb_area, nil)})
+            elseif bind.action == "mdrag" then
+                notioncore.defbindings(name, {mdrag(kcb_area, nil)})
+            elseif bind.action == "mclick" then
+                notioncore.defbindings(name, {mclick(kcb_area, nil)})
+            elseif bind.action == "mdblclick" then
+                notioncore.defbindings(name, {mdblclick(kcb_area, nil)})
+            end
+        end
+    end
 
--- Store the last saved on
-for a, b in pairs(alternate) do
-notioncore.defbindings(a, b)
-end
+    -- Store the last saved on
+    for a, b in pairs(alternate) do
+        notioncore.defbindings(a, b)
+    end
 
-alternate = save
+    alternate = save
 end
 
 notioncore.defbindings("WScreen", {
-kpress(META.."F8", "toggle_bindings()"),
+    kpress(META.."F8", "toggle_bindings()"),
 })
 
