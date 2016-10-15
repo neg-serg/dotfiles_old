f2f6ebbc1474d1057ef8da6bde8ec40171826bc2
Neg <serg.zorg@gmail.com>
2016-10-16 02:00:35 +0300

    [bin/mon] add network speed monitor
    Former-commit-id: 0fcbdb910b6b62b80f0d2887902a6b67617ed589 [formerly fb385a93d2eb7f46fbcf74c0253120eb0a11b2b6] [formerly a57d227b432232aaca3d0640aed6fd9ed24f518d [formerly ff79c2f126840f085fce366e7daf424f3a24bc04]]
    Former-commit-id: c414a430f9de87500adb1ac2637a4f8282c82305 [formerly 0100cb3c06e0ccc2899fb245901623693ff5642d]
    Former-commit-id: 3681a9393e6972516ecc2734c56bcb0976d99abe

---
+++ bin/mon/network_speed.sh
@@ -0,0 +1,69 @@
+#!/bin/zsh
+
+# Taken from : http://www.adminsehow.com/2010/03/shell-script-to-show-network-speed/
+
+# Global variables
+interface=$1
+received_bytes=""
+old_received_bytes=""
+transmitted_bytes=""
+old_transmitted_bytes=""
+
+
+# This function parses /proc/net/dev file searching for a line containing $interface data.
+# Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
+get_bytes() {
+    line=$(cat /proc/net/dev \
+        | grep ${interface} \
+        | cut -d ':' -f 2 \
+        | awk '{print "received_bytes="$1, "transmitted_bytes="$9}'\ 
+    )
+    eval ${line}
+}
+
+
+# Function which calculates the speed using actual and old byte number.
+# Speed is shown in KByte per second when greater or equal than 1 KByte per second.
+# This function should be called each second.
+get_velocity() {
+    value=$1   old_value=$2
+
+    let vel=$[value-old_value]
+    let velKB=$[vel/1024]
+    if [[ $velKB != 0 ]]; then
+        echo -n "${velKB}K";
+    else
+        echo -n "${vel}B";
+    fi
+}
+
+# Gets initial values.
+get_bytes
+old_received_bytes=$received_bytes
+old_transmitted_bytes=$transmitted_bytes
+
+# Main loop. It will repeat forever.
+while true; do
+
+    # Get new transmitted and received byte number values.
+    get_bytes
+
+    # Calculates speeds.
+    vel_recv=$(get_velocity ${received_bytes} ${old_received_bytes})
+    vel_trans=$(get_velocity ${transmitted_bytes} ${old_transmitted_bytes})
+
+    # Shows results in the console.
+    if false; then
+        builtin printf "%b" "\e[2K"
+        builtin printf "%b" "${interface} ${vel_recv}/${vel_trans}\r"
+    else 
+        builtin printf "%b" "net: ${vel_recv}/${vel_trans}\r"
+        builtin printf "\n"
+    fi
+
+    # Update old values to perform new calculations.
+    old_received_bytes=${received_bytes}
+    old_transmitted_bytes=${transmitted_bytes}
+
+    sleep $2
+done
