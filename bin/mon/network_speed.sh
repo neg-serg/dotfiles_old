fe9ae9094c0ae7362b256a2920fecaa1edd6c03e
Neg <serg.zorg@gmail.com>
2016-10-16 03:02:53 +0300

    [bin/mon] count speed like before and add interface autodetect
    Former-commit-id: 4900879a68adb85962f6759becca3db053cf9f83 [formerly 64527811413c15e5187e5c464f78109e8776d274] [formerly f375198d3a3af7cae980efe2be592530f17b1b5d [formerly 0bf53218768acbcdc6b39f8e393d7cc25cb01aa4]]
    Former-commit-id: 0b76912778a4bf0aa5d18d42fa293ccde81ebd2d [formerly 5c2f7e2a781456e61cd6474804f6910c5fb18cb9]
    Former-commit-id: abdd4c7dd0445b8345fd736ecedfb26c8fd0a9d4

--- bin/mon/network_speed.sh
+++ bin/mon/network_speed.sh
@@ -1,74 +1,103 @@
 #!/bin/zsh
 
-# Taken from : http://www.adminsehow.com/2010/03/shell-script-to-show-network-speed/
+# Original taken from : http://www.adminsehow.com/2010/03/shell-script-to-show-network-speed/
 
-# Global variables
-interface=$1
-received_bytes=""
-old_received_bytes=""
-transmitted_bytes=""
-old_transmitted_bytes=""
+function interface_autodetect(){
+    local host="google.com"
+    local default_net_="enp7s0"
 
-Kb="K" Mb="M"
+    local host_ip=$(getent ahosts ${host} | head -1 | awk '{print $1}')
+    local active_net_interface=$(ip link show up | awk -F \":\" '/state UP/ {print $2}')
+
+    if [[ host_ip != "" ]]; then
+        local host_dev=$(ip route get "${host_ip}" | grep -Po '(?<=(dev )).*(?= src)'|tr -d '[:space:]')
+    fi
+
+    if [[ host_dev != "" && host_dev ]]; then
+        builtin printf "%s\n" ${host_dev}
+    else
+        builtin printf "%s\n" ${default_net_}
+    fi
+}
 
 # This function parses /proc/net/dev file searching for a line containing $interface data.
 # Within that line, the first and ninth numbers after ':' are respectively the received and transmited bytes.
-get_bytes() {
+function get_bytes() {
     line=$(cat /proc/net/dev \
         | grep ${interface} \
         | cut -d ':' -f 2 \
-        | awk '{print "received_bytes="$1, "transmitted_bytes="$9}'\ 
+        | awk '{print "received_bytes="$1, "transmitted_bytes="$9}'\
     )
     eval ${line}
 }
 
-
 # Function which calculates the speed using actual and old byte number.
 # Speed is shown in KByte per second when greater or equal than 1 KByte per second.
 # This function should be called each second.
-get_velocity() {
-    value=$1   old_value=$2
+function get_velocity() {
+    local Kb="K" Mb="M"
+    local value=$1
+    local old_value=$2
+    local use_bytes_=false
+    local use_mb_=false
 
     let vel=$[value-old_value]
     let velKB=$[vel/1024.]
     let velMB=$[vel/1024./1024.]
 
-    if [[ ${velMB} > 1 ]]; then
+    if [[ ${velMB} > 1 && ${use_mb_} == true ]]; then
         builtin printf "%.1f%s" "${velMB}" "${Mb}";
     elif [[ ${velKB} > 1 ]]; then
         builtin printf "%.1f%s" "${velKB}" "${Kb}";
-    else
+    elif [[ ${use_bytes_} == true ]]; then
         builtin printf "%.1f%s" "${vel}" "B";
+    else 
+        builtin printf "%.1f%s" "${velKB}" "${Kb}";
     fi
+
 }
+
+if [[ $# == 0 ]]; then
+    interface=$(interface_autodetect)
+else
+    interface=$1
+fi
+
+received_bytes=""
+old_received_bytes=""
+transmitted_bytes=""
+old_transmitted_bytes=""
 
 # Gets initial values.
 get_bytes
-old_received_bytes=${received_bytes}
-old_transmitted_bytes=${transmitted_bytes}
+old_received_bytes="${received_bytes}"
+old_transmitted_bytes="${transmitted_bytes}"
 
 # Main loop. It will repeat forever.
 while true; do
-
+    local use_terminal_=false
     # Get new transmitted and received byte number values.
     get_bytes
-
     # Calculates speeds.
-    vel_recv=$(get_velocity ${received_bytes} ${old_received_bytes})
-    vel_trans=$(get_velocity ${transmitted_bytes} ${old_transmitted_bytes})
+    vel_recv=$(get_velocity "${received_bytes}" "${old_received_bytes}")
+    vel_trans=$(get_velocity "${transmitted_bytes}" "${old_transmitted_bytes}")
 
     # Shows results in the console.
-    if false; then
+    if [[ ${use_terminal_} == true ]]; then
         builtin printf "%b" "\e[2K"
         builtin printf "%b" "${interface} ${vel_recv}/${vel_trans}\r"
-    else 
+    else
         builtin printf "%b" "net: ${vel_recv}/${vel_trans}\r"
         builtin printf "\n"
     fi
 
     # Update old values to perform new calculations.
-    old_received_bytes=${received_bytes}
-    old_transmitted_bytes=${transmitted_bytes}
+    old_received_bytes="${received_bytes}"
+    old_transmitted_bytes="${transmitted_bytes}"
 
-    sleep $2
+    if [[ $# < 2 ]]; then
+        sleep 1s
+    else
+        sleep "$2"
+    fi
 done
