2b1a9e6ddeb1ff352d64f29f28695aefa4b13bb0
Neg <serg.zorg@gmail.com>
2016-11-12 01:58:46 +0300

    [bin/scripts] add secure-settings script
    Former-commit-id: 94dbdbe1887b72dcffbc95e506d15f3c7d4d5217 [formerly c64eb69b3d1c39f66106b99134fed50ac7ab8b7f] [formerly 80c090f0346b986711b5be2684a30929f87bffbd [formerly e29be96be43885fb04debf3c85e04e1dcc57069f]]
    Former-commit-id: 8473174a0815d784902bddb22c2d644c656dd832 [formerly f1d92faaf6dd5aa9738d3a412112b60b9f05e88b]
    Former-commit-id: 4b7eeaa026df5fe0397f371889e7a3f5205f9611

---
+++ bin/scripts/secure_settings.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Partially taken from:
+# https://github.com/marshyski/quick-secure
+
+
+# --[ Set basic kernel parameters ]--
+if [[ $(which sysctl 2>/dev/null) != "" ]]; then
+  #Turn on Exec Shield for RHEL systems
+  sysctl -w kernel.exec-shield=1
+  #Turn on ASLR Conservative Randomization
+  sysctl -w kernel.randomize_va_space=1
+  #Hide Kernel Pointers
+  sysctl -w kernel.kptr_restrict=1
+  #Allow reboot/poweroff, remount read-only, sync command
+  sysctl -w kernel.sysrq=176
+  #Restrict PTRACE for debugging
+  sysctl -w kernel.yama.ptrace_scope=1
+  #Hard and Soft Link Protection
+  sysctl -w fs.protected_hardlinks=1
+  sysctl -w fs.protected_symlinks=1
+  #Enable TCP SYN Cookie Protection
+  sysctl -w net.ipv4.tcp_syncookies=1
+  #Disable IP Source Routing
+  sysctl -w net.ipv4.conf.all.accept_source_route=0
+  #Disable ICMP Redirect Acceptance
+  sysctl -w net.ipv4.conf.all.accept_redirects=0
+  sysctl -w net.ipv6.conf.all.accept_redirects=0
+  sysctl -w net.ipv4.conf.all.send_redirects=0
+  sysctl -w net.ipv6.conf.all.send_redirects=0
+  #Enable IP Spoofing Protection
+  sysctl -w net.ipv4.conf.all.rp_filter=1
+  sysctl -w net.ipv4.conf.default.rp_filter=1
+  #Enable Ignoring to ICMP Requests
+  sysctl -w net.ipv4.icmp_echo_ignore_all=1
+  #Enable Ignoring Broadcasts Request
+  sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
+  #Enable Bad Error Message Protection
+  sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
+  #Enable Logging of Spoofed Packets, Source Routed Packets, Redirect Packets
+  sysctl -w net.ipv4.conf.all.log_martians=1
+  sysctl -w net.ipv4.conf.default.log_martians=1
+  #Perfer Privacy Addresses
+  net.ipv6.conf.all.use_tempaddr = 2
+  net.ipv6.conf.default.use_tempaddr = 2
+  sysctl -p
+fi
