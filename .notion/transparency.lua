3c83d62610080b0fb175aaecc2781d08344e3d5f
Neg <serg.zorg@gmail.com>
2016-11-16 04:09:22 +0300

    [notion] tmp fix for wallpaper transparency
    Former-commit-id: 4c0cfd4d5b13a0bb9297de844377e5e8a741061b [formerly 7884e1a3468cbb81a324e6f7d44eaf4b4833a0ac] [formerly b11fd2efeefcf7df33e07f05bb250eeb0bd0138c [formerly fcb997c06fccac06ae5db1797b983e5bf8356ba5]]
    Former-commit-id: 4b4a78988fcfe9e60536f4325003f06a329d65fe [formerly 18a4be527cacfb49a64fe9452f8f4d81a1ad912a]
    Former-commit-id: f7709805a5fafe9e1334e5169bd56f304e346d0a

--- .notion/transparency.lua
+++ .notion/transparency.lua
@@ -1,28 +1,58 @@
+scratchpad_table = {}
+local function find(a, tbl)
+    for _,a_ in ipairs(tbl) do if
+        a_ == a then return true end
+    end
+end
+
+local function find_manager(obj, t)
+    while obj~=nil do
+        if obj_is(obj, t) then
+            return obj
+        end
+        obj=obj:manager()
+    end
+end
+
 local function framelist(iter)
-    transparent_table = {}
-    nontransparent_table = {}
-
-    local entries={}
-    local ws_add=function(s) if s then table.insert(entries, s) end end
-
     iter(function(obj)
-             if obj_is(obj, "WMPlex") then
-                 if obj:mx_count() == 0 then
-                    table.insert(transparent_table, obj)
-                    ws_add(obj:xid())
-                 else
-                    table.insert(nontransparent_table, obj)
-                 end
-             end
-             return true
-         end)
-    return entries
+        if obj_is(obj, "WFrame") then
+            if find(obj, scratchpad_table) then
+                table.insert(transparent_table, obj)
+                -- table.insert(ultratransparent_table, obj)
+            else
+                table.insert(ultratransparent_table, obj)
+            end
+        end
+        return true
+    end)
+    return true
 end
 
 local function maketransparent()
     local atom_client_opacity = notioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
     local opacity_level = 3435973836
-    local full_opacity_level = 4294967295
+    local full_opacity_level = 40000000
+    local ultra_opacity_level = 400000000
+    local s
+
+    ultratransparent_table = {}
+    transparent_table = {}
+    nontransparent_table = {}
+
+    notioncore.clientwin_i(function(cwin)
+        local winprop = ioncore.getwinprop(cwin)
+        if winprop and winprop.scratchpad == "true" then 
+            table.insert(scratchpad_table, find_manager(cwin, "WFrame"))
+        end
+        if cwin:name() ~= "" then
+            local reg_ = find_manager(cwin, "WFrame")
+            if reg_ then
+                table.insert(nontransparent_table, reg_)
+            end
+        end
+        return true
+    end)
 
     framelist(notioncore.region_i)
 
@@ -30,36 +60,35 @@ local function maketransparent()
         notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {opacity_level})
     end
 
+    for _,reg in ipairs(ultratransparent_table) do
+        notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {full_opacity_level})
+    end
+
     for _,reg in ipairs(nontransparent_table) do
         notioncore.x_delete_property(reg:xid(), atom_client_opacity)
     end
 end
 
--- add defer(???)
+local setup_scratchpads = function()
+    local s
+    notioncore.clientwin_i(function(cwin)
+        local winprop = ioncore.getwinprop(cwin)
+        if winprop and winprop.scratchpad == "true" then 
+            s = cwin
+            return false
+        else
+            return true
+        end
+    end)
+    if s then
+        find_manager(s,"WFrame"):set_grattr("scratchpad", "set")
+    end
+end
 
--- local function scratchpadlist(iter)
---     sclist = {}
---     iter(function(obj)
---         if obj_is(obj, "WMPlex") then
---             local winprop = notioncore.getwinprop(obj)
---             if (winprop.scratchpad == "true") then
---                 table.insert(sclist, obj)
---             end
---         end
---         return true
---     end)
--- end
-
--- local function highlightscratchpads()
---     scratchpadlist(notioncore.region_i)
---     for _,reg in ipairs(sclist) do
---         reg:set_grattr("scratchpad", "set")
---     end
--- end
 
 local function hookhandler(reg, how)
     notioncore.defer(function() maketransparent() end)
-    -- notioncore.defer(function() highlightscratchpads() end)
+    notioncore.defer(function() setup_scratchpads() end)
 end
 
 notioncore.get_hook("clientwin_mapped_hook"):add(hookhandler)
