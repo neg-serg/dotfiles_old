b399b595e0729cbb943681e9ff69f26f0ec1f5f3
Neg <serg.zorg@gmail.com>
2016-11-19 05:04:35 +0300

    [notion] cleanup transparency list
    Former-commit-id: d2236a05ee3601da980a5ef9684c90c655b3eb8c [formerly 38dc809d992c7c7e581aa7a4114164bbf46957dc] [formerly d5e19c2166e6515014ce9bd2556aa89839da443c [formerly 98296c0964be5eab6caab35ecb0059d301b5effc]]
    Former-commit-id: 54802afef02088910ee6d34f2a421505bd1c1266 [formerly e5cf9ef469d64723b3d103269386258a9378fb37]
    Former-commit-id: c32ebb99aa19a5eaba604e513baf0524c34f37bb

--- .notion/transparency.lua
+++ .notion/transparency.lua
@@ -1,5 +1,3 @@
-scratchpad_ = {}
-
 local function find(a, tbl)
     for _,a_ in ipairs(tbl) do if
         a_ == a then return true end
@@ -45,57 +43,12 @@ local function maketransparent()
             if obj_is(obj, "WFrame") then
                 if find(obj, scratchpad_) or
                     find(obj:name(), scratchpad_list) 
-                   then
+                then
                     table.insert(transparent_, obj)
                 else
                     local group = find_manager(obj,"WTiling")
                     if group then
-                        local split__ =  WTiling.split_tree(group)
-                        local mt
-                        local tname
-                        local o
-                        local ret__
-
-                        if split__ then
-                            if type(split__) == 'userdata' then
-                                mt = getmetatable(split__)
-                                o = '['
-                                if mt and mt.__index then
-                                    tname = mt.__index.__typename
-                                    if tname then
-                                        o = o .. tname
-                                    end
-                                    if tdir then
-                                        o = o .. tdir
-                                    end
-                                end
-                                ret__ = o .. ']'
-                            end
-                            
-                            if ret__ == "[WSplitSplit]" then
-                                table.insert(ultratransparent_, obj)
-                            -- elseif ret__ == "[WSplitRegion]" then
-                            --     table.insert(nontransparent_, obj)
-                            --     -- table.insert(ultratransparent_, obj)
-                            end
-                        end
-                        ----------------------------------------------------------
-                        -- local lol__=false
-                        -- if split__.br ~= nil then
-                        --     if split__.br().br ~= nil or split__.br().lr then
-                        --         table.insert(transparent_, obj)
-                        --         lol__=true
-                        --     end
-                        -- end
-                        -- if split__.lr ~= nil then
-                        --     if split__.lr().lr ~= nil or split__.lr().br then
-                        --         table.insert(transparent_, obj)
-                        --         lol__=true
-                        --     end
-                        -- end
-                        -- if lol__ == false then
                         table.insert(ultratransparent_, obj)
-                        -- end
                     end
                 end
             end
@@ -140,6 +93,7 @@ local function transparency_handler(reg, how)
 end
 
 local function scratchpads_handler(reg, how)
+    scratchpad_ = {}
     core.defer(function() setup_scratchpads() end)
     scratchpad_ = {}
 end
