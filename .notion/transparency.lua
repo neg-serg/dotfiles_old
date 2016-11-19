caaf37f4cce7f30daf314a4c3b0511463dc70825
Neg <serg.zorg@gmail.com>
2016-11-19 03:20:42 +0300

    [notion] cleanup notion transparency
    Former-commit-id: 61f74755677e5cc69bc58c0b79a458612489b250 [formerly 4c684528db227066287d432da380b6b476384b83] [formerly d7f1121da6d8c479972e855be3bb268935c1f44f [formerly 063ab1fdd08aaeb24549b935f351d80607c51d82]]
    Former-commit-id: a7cf3473bb22e94e36e3dee66e7747fdfd396c3c [formerly 235d5ef70360d7dbe8a69f33c0787a3745aa744f]
    Former-commit-id: 05d39295c48b80ae2c4e0d2eb50a44f858cbe3eb

--- .notion/transparency.lua
+++ .notion/transparency.lua
@@ -1,6 +1,3 @@
--- TODO ::
--- maybe it's better to match scratchpad with 
--- name like <im,ncmpcpp,...> ?
 scratchpad_ = {}
 
 local function find(a, tbl)
@@ -19,17 +16,17 @@ local function find_manager(obj, t)
 end
 
 local function maketransparent()
-    local atom_client_opacity = notioncore.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
-    local opacity_level       = 3435973836
-    local full_opacity_level  = 4000000
-    local ultra_opacity_level = 400000000
+    local atom_client_opacity = core.x_intern_atom("_NET_WM_WINDOW_OPACITY", false)
+    local std_opacity         = 3435973836
+    local full_opacity        = 4000000
+    local ultra_std_opacity   = 400000000
     local s
 
     local ultratransparent_ = {}
     local transparent_ = {}
     local nontransparent_ = {}
 
-    notioncore.clientwin_i(function(cwin)
+    core.clientwin_i(function(cwin)
         local winprop = ioncore.getwinprop(cwin)
         if winprop and winprop.scratchpad == "true" then 
             table.insert(scratchpad_, find_manager(cwin, "WFrame"))
@@ -43,59 +40,46 @@ local function maketransparent()
         return true
     end)
 
-    -- local group = find_manager(obj,"WTiling")
-    -- if group then
-    --     -- if WTiling.split_tree(group)["type"] == "WSplitSplit" then
-    --     table.insert(ultratransparent_, obj)
-    --     -- end
-    --     -- table.insert(ultratransparent_, obj)
-    --     -- -- dbg.echon(find_manager(obj,"WSplit"))
-    --     -- if WTiling.split_tree(group).type then
-    --     --     dbg.echon(WTiling.split_tree(group).type)
-    --     -- end
-    --     -- LOOK FOR get SPLIT_REGION AND SPLIT_SPLIT!
-    --     dbg.echon(WTiling.split_tree(group))
-    -- end
-
     local function framelist(iter)
         iter(function(obj)
             if obj_is(obj, "WFrame") then
-                if find(obj, scratchpad_) then
+                if find(obj, scratchpad_) or
+                    find(obj:name(), scratchpad_list) 
+                   then
                     table.insert(transparent_, obj)
                 else
                     local group = find_manager(obj,"WTiling")
                     if group then
                         local split__ =  WTiling.split_tree(group)
-                        -- local mt
-                        -- local tname
-                        -- local o
-                        -- local ret__
+                        local mt
+                        local tname
+                        local o
+                        local ret__
 
-                        -- if split__ then
-                        --     if type(split__) == 'userdata' then
-                        --         mt = getmetatable(split__)
-                        --         o = '['
-                        --         if mt and mt.__index then
-                        --             tname = mt.__index.__typename
-                        --             if tname then
-                        --                 o = o .. tname
-                        --             end
-                        --             if tdir then
-                        --                 o = o .. tdir
-                        --             end
-                        --         end
-                        --         ret__ = o .. ']'
-                        --     end
+                        if split__ then
+                            if type(split__) == 'userdata' then
+                                mt = getmetatable(split__)
+                                o = '['
+                                if mt and mt.__index then
+                                    tname = mt.__index.__typename
+                                    if tname then
+                                        o = o .. tname
+                                    end
+                                    if tdir then
+                                        o = o .. tdir
+                                    end
+                                end
+                                ret__ = o .. ']'
+                            end
                             
-                        --     if ret__ == "[WSplitSplit]" then
-                        --         table.insert(ultratransparent_, obj)
-                        --     -- elseif ret__ == "[WSplitRegion]" then
-                        --     --     table.insert(nontransparent_, obj)
-                        --     --     -- table.insert(ultratransparent_, obj)
-                        --     end
-                        -- end
+                            if ret__ == "[WSplitSplit]" then
+                                table.insert(ultratransparent_, obj)
+                            -- elseif ret__ == "[WSplitRegion]" then
+                            --     table.insert(nontransparent_, obj)
+                            --     -- table.insert(ultratransparent_, obj)
+                            end
+                        end
                         ----------------------------------------------------------
-                        
                         -- local lol__=false
                         -- if split__.br ~= nil then
                         --     if split__.br().br ~= nil or split__.br().lr then
@@ -112,8 +96,6 @@ local function maketransparent()
                         -- if lol__ == false then
                         table.insert(ultratransparent_, obj)
                         -- end
-
-                        ----------------------------------------------------------
                     end
                 end
             end
@@ -121,24 +103,24 @@ local function maketransparent()
         end)
         return true
     end
-    framelist(notioncore.region_i)
+    framelist(core.region_i)
 
     for _,reg in ipairs(transparent_) do
-        notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {opacity_level})
+        core.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {std_opacity})
     end
 
     for _,reg in ipairs(ultratransparent_) do
-        notioncore.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {full_opacity_level})
+        core.x_change_property(reg:xid(), atom_client_opacity, 6, 32, "replace", {full_opacity})
     end
 
     for _,reg in ipairs(nontransparent_) do
-        notioncore.x_delete_property(reg:xid(), atom_client_opacity)
+        core.x_delete_property(reg:xid(), atom_client_opacity)
     end
 end
 
 local setup_scratchpads = function()
     local s
-    notioncore.clientwin_i(function(cwin)
+    core.clientwin_i(function(cwin)
         local winprop = ioncore.getwinprop(cwin)
         if winprop and winprop.scratchpad == "true" then 
             s = cwin
@@ -154,10 +136,16 @@ end
 
 
 local function transparency_handler(reg, how)
-    notioncore.defer(function() maketransparent() end)
-    notioncore.defer(function() setup_scratchpads() end)
+    core.defer(function() maketransparent() end)
 end
 
-notioncore.get_hook("clientwin_mapped_hook"):add(transparency_handler)
-notioncore.get_hook("clientwin_unmapped_hook"):add(transparency_handler)
-notioncore.get_hook("region_notify_hook"):add(transparency_handler)
+local function scratchpads_handler(reg, how)
+    core.defer(function() setup_scratchpads() end)
+    scratchpad_ = {}
+end
+
+core.get_hook("clientwin_mapped_hook"):add(transparency_handler)
+core.get_hook("clientwin_unmapped_hook"):add(transparency_handler)
+core.get_hook("region_notify_hook"):add(transparency_handler)
+
+core.get_hook("clientwin_mapped_hook"):add(scratchpads_handler)
