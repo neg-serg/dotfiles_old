d357c232e166f884770841e81d8ff9396d702efd
Neg <serg.zorg@gmail.com>
2016-12-22 02:34:10 +0300

    [zsh] simple nvim start
    Former-commit-id: cf457a88333d3838b4863115cfb7f99ffa851c76 [formerly 623a7d5ceac2d059e6405169f78f4a5ee91cd09f] [formerly aa50bd44b596ba3cd0b66c426d1aa21a4e9637e8 [formerly c8b9a2b7bc13cd30218e7d5507f674a062deaf83]]
    Former-commit-id: b234420475d72c7f631295d4ca5c1d615bbf2710 [formerly d086c1099f43a9ec8479b85b41c59fd372bc652b]
    Former-commit-id: e24809f7e90a903ac8e09bad7cc50bb5345547a1

--- .zsh/05-functions.zsh
+++ .zsh/05-functions.zsh
@@ -306,3 +306,6 @@ kut() { awk "{ print $(for n; do echo -n "\$$n,"; done | sed 's/,$//') }" ;}
 +strip_trailing_workspaces(){  sed ${1:+-i} 's/\s\+$//' "$@" }
 +show_coredumps() { locate -b '^core\.?[0-9]*$' --regex | xargs file | fgrep ELF | awk '{print $1}' | sed 's,:$,,'}
 wh() {_v -c 'set ft=sh' <<< $(which $1)}
+function wnvim(){
+    st nvim &
+}
