7f8e5f3ba8da4bb0b58a4ef5b3ec9e0c5354ac66
Neg <serg.zorg@gmail.com>
2016-12-24 13:34:00 +0300

    [zsh] update games bindings
    Former-commit-id: affceaf414567b3bfda0ac6d65af75f4e3fb2424 [formerly 5da2cfb9c778628c2411413bfc3add01bdc71688] [formerly b18340a3d4e0788315ae79a2fa5bc52ef2641187 [formerly 1ae2dd1109e4835a9942a298b5306d883960faa4]]
    Former-commit-id: e93918121f3723885bf734303b2847ac74c5ad6c [formerly 286c8a8b3574faaf1e3c666e22b00cd97509b8ad]
    Former-commit-id: 48722adfb0b3a0bd3e3dd58b977dd9f4ec7c162c

--- .zsh/70-games.zsh
+++ .zsh/70-games.zsh
@@ -1,18 +1,15 @@
-alias wine="WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine"
-alias steamwine='WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe' 
-function bnet(){
-    local prefix_=""
-    if [[ $1 =~ ".*32" ]]; then
-        prefix_="setarch i386 -3 "
-        _zwrap "set to 32 bit"
-    fi
-    local dir_=~/.wine/drive_c/Program\ Files\ \(x86\)/
-    cd "${dir_}/Battle.net"
-    eval WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C \
-    ${prefix_} wine ./Battle.net.exe &
-}
-alias crossover="LANG=ru_RU.utf8 setarch i386 -3 /mnt/home/crossover/bin/crossover"
+local games_dir_="$(readlink -f ${HOME}/games)"
+local wine_pref_="${HOME}/.wine"
+IFS= local wine_progs_="$(echo ${wine_pref_}'/drive_c/Program Files (x86)')"
 
+alias wine="WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine"
+alias crossover="LANG=ru_RU.utf8 setarch i386 -3 /mnt/home/crossover/bin/crossover"
+alias q3="$(echo -e ${games_dir_}/q3/qq/ioquake3)"
+
+function steam_wine(){
+    cd "${wine_progs_}/Steam"
+    WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C wine Steam.exe
+}
 
 function sls(){
     steamcmd '+apps_installed +quit' |\
@@ -22,4 +19,21 @@ function sls(){
                 sub(" : .*", "", name);
                 print id ": " name;
             }'
+}
+
+function bnet(){
+    local prefix_=""
+    if [[ $1 =~ ".*32" ]]; then
+        prefix_="setarch i386 -3 "
+        _zwrap "set to 32 bit"
+    fi
+    cd "${wine_progs_}/Battle.net"
+    eval WINEDEBUG=-all LC_ALL=ru_RU.utf8 LC_COLLATE=C LC_MESSAGES=C \
+    ${prefix_} wine ./Battle.net.exe &
+}
+
+function doom(){
+    cd "${wine_progs_}/Steam/steamapps/common/DOOM/"
+    local doom_path="DOOMx64vk.exe"
+    wine ${doom_path}
 }
