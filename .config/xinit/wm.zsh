204cef7c0addd9a2ba8b6b003da816646854f6e1
Neg <serg.zorg@gmail.com>
2017-01-16 20:12:42 +0300

    [xinit] herbstluftwm support
    Former-commit-id: 4c88daccd62eac5c97bbcb1e701ced770d4d7f20 [formerly 1238bfa877e6cd997d6c578049bb9191da9f735a] [formerly c3316a39b4bff1e3c56201462fba9563e8f8ef11 [formerly 4ba812663474cc392ba3f08b4a58afac5bb5072c]]
    Former-commit-id: 716b1b6dc4d28d2cecfb9273001bd0a49ec6fb59 [formerly 5a6c08abc96948fd6b0ee29dd3827595edbc5045]
    Former-commit-id: 1aaab93dd1ae2c27a1638e3997a8d799a8e9a73e

--- .config/xinit/wm.zsh
+++ .config/xinit/wm.zsh
@@ -1,16 +1,26 @@
 #!/bin/zsh
 
-if [[ ${X11_WM} == "notion" ]]; then
+function notion_run(){
     if [[ -e "${PANELS}" ]]; then
         "${SCRIPT_HOME}/panels"
     fi
     source ${XDG_CONFIG_HOME}/xinit/hotkeys.zsh
 
+    (urxvtd -q -f -o && ${BIN_HOME}/urxvt) &
+
     cat /tmp/ws_out.txt &
     ~/bin/scripts/panels
 
     exec notion 2>> ~/tmp/${X11_WM}err$$ 2>&1
-elif [[ ${X11_WM} == "windowchef" ]]; then
+}
+
+function windowchef_run() {
     sxhkd -c "${XDG_CONFIG_HOME}/windowchef/sxhkd.conf" &
     exec windowchef 2>> ~/tmp/${X11_WM}err$$ 2>&1
-fi
+}
+
+function herbstluftwm_run() {
+    herbstluftwm -c ~/.config/herbstluftwm/autostart
+}
+
+eval "${X11_WM}"_run "$@"
