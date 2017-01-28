77bcee7a89149353610750c9eeda679f6ec9005c
Neg <serg.zorg@gmail.com>
2017-01-28 23:37:12 +0300

    [bin/scripts] add xhost local
    Former-commit-id: cefecc968a78dc76044bd72a6ea0703215713497 [formerly 979fc52daf499d61f46e063b0531622810bdacca] [formerly 910378dd7c1bc11384907c26945e9af8d60f2429 [formerly 73513798fe879789fe951d485e6e23077e1b0990]]
    Former-commit-id: dadb74c2207e504cf8b5d664630d1545e077acf3 [formerly 8c0eb94112b1770fddfdf15a9286d9c1b00867e4]
    Former-commit-id: 6904a0008f07ec9f13aecb233fb2ad165af66612

--- bin/scripts/systemd_env_forwarding.zsh
+++ bin/scripts/systemd_env_forwarding.zsh
@@ -3,4 +3,4 @@ systemctl --user import-environment DISPLAY XAUTHORITY
 if which dbus-update-activation-environment >/dev/null 2>&1; then
     dbus-update-activation-environment DISPLAY XAUTHORITY
 fi
-
+xhost +local:
