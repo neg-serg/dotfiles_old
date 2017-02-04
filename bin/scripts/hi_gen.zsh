7f788546ffffbe6acd84383b808ae4e5d62f281c
Neg <serg.zorg@gmail.com>
2017-02-05 00:48:07 +0300

    [bin/scripts] update hi_gen
    Former-commit-id: 5cd1c8ed4a71af8cc562aeb69e3d6a0f03edaaf6 [formerly 7f5f700ed1fe1053e7c5da8139d8ee04cfa2f570] [formerly d69fe171f41ba6993279a0d80d72040d81e9a856 [formerly dba5f994ceb670df425c3f77fd5b985eef123fee]]
    Former-commit-id: ab24758cd062bb6e631a0a4b3f76b9b8e983e761 [formerly e7380bd3357cec42b811811377ca072dd81615e5]
    Former-commit-id: 144768393de77345891d77674e5a5a7748fdfa2a

--- bin/scripts/hi_gen.zsh
+++ bin/scripts/hi_gen.zsh
@@ -1,55 +1,59 @@
 #!/bin/zsh
 
+source ${ZSH}/03-helpers.zsh
+
+autoload colors && colors
+
 function eat () {
     prog=${0##*/}
-    local bldblu='\e[1;34m'
-    local txtbld=$(tput bold)
-    local txtund=$(tput sgr 0 1)
-    local txtrst='\e[0m'
-    local pass=${bldblu}*${txtrst}
-    filename=$@
+    local pass="*"
+    filename="$@"
     xclip -in -selection c < "${filename}"
-    echo -e "${pass} ${txtund}"${filename##*/}"${txtrst} copied to clipboard"
+    _zwrap "${pass} ${txtund}"${filename##*/}"${txtrst} copied to clipboard"
 }
 
 function main() {
-    local ftype_pref="ftype-"
-    local dircolors_location="${XDG_CONFIG_HOME}/dircolors/.dircolors"
+    readonly ftype_pref="ftype-"
+    readonly dircolors_location="${XDG_CONFIG_HOME}/dircolors/.dircolors"
 
-    local tmp_file=$(mktemp)
-    local ftype_arr="/tmp/ftype_arr"
-    local ftype_rule="/tmp/ftype_rule"
+    readonly tmp_file="$(mktemp)"
+    readonly ftype_arr="/tmp/ftype_arr"
+    readonly ftype_rule="/tmp/ftype_rule"
 
-    local is_bold="\\(01;\|;1\\)"
-    local is_uline="\\(04;\|;4\\)"
-    local bold_regex="/${is_bold}/"
-    local uline_regex="/${is_uline}/"
+    readonly is_bold="\\(01;\|;1\\)"
+    readonly is_uline="\\(04;\|;4\\)"
+    readonly bold_regex="/${is_bold}/"
+    readonly uline_regex="/${is_uline}/"
 
-    local fg_eq="38;5;" bg_eq="48;5;"
+    readonly fg_eq="38;5;" 
+    readonly bg_eq="48;5;"
 
-    egrep '^[.*]' ${1:-$dircolors_location}|sed 's/#.*//'|sed "s/^\.\W*/${ftype_pref}/" > ${tmp_file}
+    egrep '^[.*]' ${1:-$dircolors_location} \
+        |sed 's/#.*//' \
+        |sed "s/^\.\W*/${ftype_pref}/" > "${tmp_file}"
 
-    echo "-----------------------------"
-    echo "you should handle it by hand:"
-    echo "-----------------------------"
+    _zwrap "-----------------------------"
+    _zwrap "You should handle it by hand:"
+    _zwrap "-----------------------------"
 
-    egrep "^${ftype_pref}" -v ${tmp_file} 
-    egrep "^${ftype_pref}" ${tmp_file} | sed -e "s/${fg_eq}/fg=/"               \
-                                             -e "s/${bg_eq}/bg=/g"              \
-                                             -e "${bold_regex}s/$/,bold/"       \
-                                             -e "${uline_regex}s/$/,underline/" \
-                                             -e "s/00;//"                       \
-                                             -e "s/${is_bold}//"                \
-                                             -e "s/${is_uline}//"               \
-                                             -e "s/;/,/g"                       \
-                                             -e "/\ 3[0-9][0-9]*/s/3/fg=/"      \
-                                             -e "s/, //g" > ${ftype_arr}
+    egrep "^${ftype_pref}" -v "${tmp_file}"
+    egrep "^${ftype_pref}" "${tmp_file}" | sed -e "s/${fg_eq}/fg=/"               \
+                                               -e "s/${bg_eq}/bg=/g"              \
+                                               -e "${bold_regex}s/$/,bold/"       \
+                                               -e "${uline_regex}s/$/,underline/" \
+                                               -e "s/00;//"                       \
+                                               -e "s/${is_bold}//"                \
+                                               -e "s/${is_uline}//"               \
+                                               -e "s/;/,/g"                       \
+                                               -e "/\ 3[0-9][0-9]*/s/3/fg=/"      \
+                                               -e "s/, //g" > "${ftype_arr}"
 
-    vim +'Tabularize/[bf]g=.*' ${ftype_arr} +'wq'
-    sed "s/^${ftype_pref}//" ${ftype_arr}|awk '{print "*."$1") style=ftype-"$1" ;;"}' > ${ftype_rule}
-    vim +'Tabularize/)\zs ' ${ftype_rule} +'wq'
+    nohup zsh -c "vim +'Tabularize/[bf]g=.*' "${ftype_arr}" +'wq'" 2> /tmp/vimlol
+    sed "s/^${ftype_pref}//" "${ftype_arr}" |awk '{print "*."$1") style=ftype-"$1" ;;"}' > "${ftype_rule}"
+    nohup zsh -c "vim +'Tabularize/)\zs ' "${ftype_rule}" +'wq'" 2> /tmp/vimlol
 
-    for t in "${ftype_arr}" "${ftype_rule}"; eat ${t}
+    for t in "${ftype_arr}" "${ftype_rule}"; eat "${t}"
+    rm "${tmp_file}"
 }
 
-main $1
+main "${1}"
