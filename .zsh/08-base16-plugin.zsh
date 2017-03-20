be09adabe2c9670b0aeb45ba288bf163ebb3d688
Neg <serg.zorg@gmail.com>
2017-03-20 16:13:03 +0300

    [zsh] update current state of base16 plugin
    Former-commit-id: 57b6c6e683552c42c0aaa6c008af2f3f5220d09a [formerly eb9fd7d716f227580162033793b2c8937da87b3f] [formerly 1fbf1f9c76603b63de458b7daf87e2a464ec4a66 [formerly 4876bb098e2315f32c1e1ff81428622eb6420ffc]]
    Former-commit-id: 3d14ff4bee9d76f8f54fbb47ad606e50dbbfe0ad [formerly a6c6abf5fdd42945e50891b592e4f193dbe58846]
    Former-commit-id: 3dbdef783a7b76794eab3f0247c51250a59eb537

--- .zsh/08-base16-plugin.zsh
+++ .zsh/08-base16-plugin.zsh
@@ -1,2 +1,4 @@
-BASE16_SHELL=${XDG_CONFIG_HOME}/base16-shell
-[[ -n "${PS1}" ]] && [[ -s ${BASE16_SHELL}/profile_helper.sh ]] && eval "$(${BASE16_SHELL}/profile_helper.sh)"
+if [[ $(readlink ${HOME}/.base16_theme|awk '/base16-dark3/') == "" ]]; then
+    BASE16_SHELL=${XDG_CONFIG_HOME}/base16-shell
+    [[ -n "${PS1}" ]] && [[ -s ${BASE16_SHELL}/profile_helper.sh ]] && eval "$(${BASE16_SHELL}/profile_helper.sh)"
+fi
