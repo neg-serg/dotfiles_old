2394bcadbe2457d065048bb3afe360972ff4a928
Neg <serg.zorg@gmail.com>
2017-04-05 20:58:44 +0300

    [i3] cycle: fix crash
    Former-commit-id: 5c07a1710a3790809185439b1e52c16a039d2a9a [formerly 1060b4e6eb8196db126250ce30fd64d1a6f54aea] [formerly 5d23d94a00c3d2c670d5e75ef45b87409caa8d2b [formerly 998a5be0f0652d9b4e2e553e66e8fc5683720841]]
    Former-commit-id: 20c793a59567251e26699af7d41e13d8d117a944 [formerly bf6dd7f4d58074925dbaca0420dcf4f3979b023b]
    Former-commit-id: 50329b4245e12949d68aa7e0bb4e5132bb8ab5eb

--- .config/i3/cycle_i3.py
+++ .config/i3/cycle_i3.py
@@ -33,10 +33,6 @@ from itertools import cycle
 glob_settings=cycle_settings().settings
 q = Queue()
 
-tagged={}
-for i in glob_settings:
-    tagged[i]=list({})
-
 # Based on tornado.ioloop.IOLoop.instance() approach.
 # See https://github.com/facebook/tornado
 class SingletonMixin(object):
@@ -55,32 +51,27 @@ class cycle_window(SingletonMixin):
     def __init__(self):
         self.win_iter=None
         self.tab_counter=0
+        self.tagged={}
+        for i in glob_settings:
+            self.tagged[i]=list({})
 
     def go_next(self, tag):
-        self.win_iter=cycle(tagged[tag])
+        try:
+            self.win_iter=cycle(self.tagged[tag])
 
-        if len(tagged[tag]) == 0:
-            prog=glob_settings[tag]["prog"]
-            i3.command('exec {}'.format(prog))
-        elif len(tagged[tag]) == 1:
-            tagged[tag][0]['win'].command('focus')
-            tagged[tag][0]['focused']=True
-            self.tab_counter+=1
-
-            return
-        else:
-            tagged[tag][self.tab_counter%len(tagged[tag])]['win'].command('focus')
-            self.tab_counter+=1
-
-            return
-
-def handle_unfocus(self, event):
-    con = event.container
-    global tagged
-    for tag in glob_settings:
-        if not con.window_class in glob_settings[tag]["classes"]:
-            for win in tagged[tag]:
-                win['focused']=False
+            if len(self.tagged[tag]) == 0:
+                prog=glob_settings[tag]["prog"]
+                i3.command('exec {}'.format(prog))
+            elif len(self.tagged[tag]) == 1:
+                self.tagged[tag][0]['win'].command('focus')
+                self.tagged[tag][0]['focused']=True
+                self.tab_counter+=1
+            else:
+                self.tagged[tag][self.tab_counter%len(self.tagged[tag])]['win'].command('focus')
+                self.tab_counter+=1
+        except KeyError:
+            find_all()
+            self.go_next(tag)
 
 fifo_=os.path.realpath(os.path.expandvars('$HOME/tmp/i3_tags.fifo'))
 if os.path.exists(fifo_):
@@ -93,7 +84,7 @@ except OSError as oe:
         raise
 
 def fifo_listner():
-    cl=cycle_window.instance()
+    cw=cycle_window.instance()
     with open(fifo_) as fifo:
         while True:
             data = fifo.read()
@@ -103,7 +94,7 @@ def fifo_listner():
             args=list(filter(lambda x: x != '', eval_str.split(' ')))
             if len(args) > 0:
                 if args[0] == "next":
-                    cl.go_next(args[1])
+                    cw.go_next(args[1])
 
 def worker():
     while True:
@@ -119,23 +110,25 @@ def mainloop_cycle():
 
 
 def find_acceptable_windows_by_class(tag, wlist):
-    global tagged
+    cw=cycle_window.instance()
     for con in wlist:
         if con.window_class in glob_settings[tag]["classes"]:
-            tagged[tag].append(
-                { 'win':con, 'focused':False }
-            )
+            cw.tagged[tag].append({ 'win':con, 'focused':False })
 
 def print_tagged_info(tag):
-    for win in tagged[tag]:
-        print("tagged[{}]={}~{}".format(tag,i['win'].name,win['focused']))
+    cw=cycle_window.instance()
+    for win in cw.tagged[tag]:
+        print("tagged[{}]={}~{}".format(tag,win['win'].name,win['focused']))
 
 def find_all():
-    global tagged
+    cw=cycle_window.instance()
     wlist = i3.get_tree().leaves()
-    tagged={}
+    cw.tagged={}
+    cw.tab_counter=0
+
     for i in glob_settings:
-        tagged[i]=list({})
+        cw.tagged[i]=list({})
+
     for tag in glob_settings:
         find_acceptable_windows_by_class(tag, wlist)
         print_tagged_info(tag)
@@ -145,21 +138,18 @@ def cleanup_all():
         os.remove(fifo_)
 
 def add_acceptable(self, event):
+    cw=cycle_window.instance()
     con = event.container
     for tag in glob_settings:
         if con.window_class in glob_settings[tag]["classes"]:
-            tagged[tag].append(
-                {
-                    'win':con,
-                    'focused':con.focused
-                }
-            )
+            cw.tagged[tag].append({ 'win':con, 'focused':con.focused })
 
 def del_acceptable(self, event):
+    cw=cycle_window.instance()
     con = event.container
     for tag in glob_settings:
         if con.window_class in glob_settings[tag]["classes"]:
-            del tagged[tag]
+            del cw.tagged[tag]
 
 # handle events, do not try to do it from the one script
 if __name__ == '__main__':
@@ -174,7 +164,6 @@ if __name__ == '__main__':
 
         i3.on('window::new', add_acceptable)
         i3.on('window::close', del_acceptable)
-        i3.on('window::focus', handle_unfocus)
 
         mainloop=Thread(target=mainloop_cycle).start()
 
