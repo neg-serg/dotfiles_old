a081bf4293f889b8b382efbe7ed1dcbccc658041
Neg <serg.zorg@gmail.com>
2017-04-06 15:25:12 +0300

    [i3] refactor switch-last
    Former-commit-id: 73ef9cc362470d1d7cd9b620debe4a3aca18d0f4 [formerly 3f1fb61ccbdd333280036da9541d7b4044ad1e0c] [formerly 59dccba42d6e8b3158c06ed13833bf5d27f91674 [formerly 2aa711b592e5236c4e3ce7cc28eaae2cd55e57f6]]
    Former-commit-id: 1621a59b2dcff0fe3c1397f7b01904741601bcf0 [formerly bba75e8e5cf14fa64fa2d0e2c64fe56ae1e902fe]
    Former-commit-id: 9847e60c83e26f228d997777b7683de8b76ef678

--- .config/i3/focus-last.py
+++ .config/i3/focus-last.py
@@ -1,94 +1,105 @@
 #!/usr/bin/env python3
 
-import os
-import socket
-import selectors
-import threading
-from argparse import ArgumentParser
+"""
+
+Focus last focused window.
+This script should be launch from the .xsessionrc without argument.
+Then you can bind this script with the `--switch` option to one of your
+i3 keybinding.
+
+Usage:
+    focus-last.py daemon
+
+Options:
+    -h, --help  show this help message and exit
+    --switch    Switch to the previous window
+"""
+
 import i3ipc
+import os
 
-SOCKET_FILE = '/tmp/i3_focus_last'
-MAX_WIN_HISTORY = 15
+from threading import Thread
+from docopt import docopt
+from singleton_mixin import *
+from queue import Queue
 
-class FocusWatcher:
+max_win_history_ = 15
 
-    def __init__(self):
-        self.i3 = i3ipc.Connection()
-        self.i3.on('window::focus', self.on_window_focus)
-        self.listening_socket = socket.socket(socket.AF_UNIX,
-            socket.SOCK_STREAM)
-        if os.path.exists(SOCKET_FILE):
-            os.remove(SOCKET_FILE)
-        self.listening_socket.bind(SOCKET_FILE)
-        self.listening_socket.listen(1)
-        self.window_list = []
-        self.window_list_lock = threading.RLock()
+q = Queue()
+fifo_=os.path.realpath(os.path.expandvars('$HOME/tmp/focus_last_i3.fifo'))
+if os.path.exists(fifo_):
+    os.remove(fifo_)
 
-    def on_window_focus(self, i3conn, event):
-        with self.window_list_lock:
-            window_id = event.container.props.id
-            if window_id in self.window_list:
-                self.window_list.remove(window_id)
-            self.window_list.insert(0, window_id)
-            if len(self.window_list) > MAX_WIN_HISTORY:
-                del self.window_list[MAX_WIN_HISTORY:]
+try:
+    os.mkfifo(fifo_)
+except OSError as oe:
+    if oe.errno != errno.EEXIST:
+        raise
 
-    def launch_i3(self):
-        self.i3.main()
-
-    def launch_server(self):
-        selector = selectors.DefaultSelector()
-
-        def accept(sock):
-            conn, addr = sock.accept()
-            selector.register(conn, selectors.EVENT_READ, read)
-
-        def read(conn):
-            data = conn.recv(1024)
-            if data == b'switch':
-                with self.window_list_lock:
-                    tree = self.i3.get_tree()
-                    windows = set(w.id for w in tree.leaves())
-                    for window_id in self.window_list[1:]:
-                        if window_id not in windows:
-                            self.window_list.remove(window_id)
-                        else:
-                            self.i3.command('[con_id=%s] focus' % window_id)
-                            break
-            elif not data:
-                selector.unregister(conn)
-                conn.close()
-
-        selector.register(self.listening_socket, selectors.EVENT_READ, accept)
-
+def fifo_listner():
+    fw=FocusWatcher.instance()
+    with open(fifo_) as fifo:
         while True:
-            for key, event in selector.select():
-                callback = key.data
-                callback(key.fileobj)
+            data = fifo.read()
+            if len(data) == 0:
+                break
+            eval_str=data.split('\n', 1)[0]
+            if len(eval_str) > 0:
+                if eval_str == "switch":
+                    fw.alt_tab()
 
-    def run(self):
-        t_i3 = threading.Thread(target=self.launch_i3)
-        t_server = threading.Thread(target=self.launch_server)
-        for t in (t_i3, t_server):
-            t.start()
+def worker():
+    while True:
+        if q.empty():
+            exit()
+        i = q.get()
+        q.task_done()
+
+def mainloop_fw():
+    while True:
+        q.put(fifo_listner())
+        Thread(target=worker).start()
+
+
+class FocusWatcher(SingletonMixin):
+    def __init__(self):
+        self.window_list = []
+
+    def alt_tab(self):
+        windows = set(w.id for w in i3.get_tree().leaves())
+        for wid in self.window_list[1:]:
+            if wid not in windows:
+                self.window_list.remove(wid)
+            else:
+                i3.command('[con_id=%s] focus' % wid)
+                break
+
+def on_window_focus(i3conn, event):
+    wid = event.container.props.id
+    fw=FocusWatcher.instance()
+
+    if wid in fw.window_list:
+        fw.window_list.remove(wid)
+
+    fw.window_list.insert(0, wid)
+
+    if len(fw.window_list) > max_win_history_:
+        del fw.window_list[max_win_history_:]
+
+def cleanup_all():
+    if os.path.exists(fifo_):
+        os.remove(fifo_)
 
 if __name__ == '__main__':
-    parser = ArgumentParser(prog='focus-last.py',
-        description='''
-        Focus last focused window.
-        This script should be launch from the .xsessionrc without argument.
-        Then you can bind this script with the `--switch` option to one of your
-        i3 keybinding.
-        ''')
-    parser.add_argument('--switch', dest='switch', action='store_true',
-        help='Switch to the previous window', default=False)
-    args = parser.parse_args()
+    argv = docopt(__doc__, version='i3 nice alt-tab 1.0')
+    i3 = i3ipc.Connection()
 
-    if not args.switch:
-        focus_watcher = FocusWatcher()
-        focus_watcher.run()
-    else:
-        client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        client_socket.connect(SOCKET_FILE)
-        client_socket.send(b'switch')
-        client_socket.close()
+    import atexit
+    atexit.register(cleanup_all)
+
+    if argv["daemon"]:
+        i3.on('window::focus', on_window_focus)
+
+        mainloop=Thread(target=mainloop_fw).start()
+
+        i3.main()
