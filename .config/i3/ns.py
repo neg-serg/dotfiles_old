022de4b2b1cb711cac7d052d984d21058ce2e3f5
Neg <serg.zorg@gmail.com>
2017-04-06 20:54:13 +0300

    [i3] ns refactoring
    Former-commit-id: 93a6dee1d866390eac9feed001dce07e27010107 [formerly e77c7942afa5aec8b507b7b1c66c3cfa4d205e47] [formerly 2397a3ac4a33006fa366e606dadf7cc5f2ef511c [formerly c35f013b938298fca2b6077b6a88aead9633129b]]
    Former-commit-id: 01d7d75df074d025a7a29a92fb5725b7b3c93d20 [formerly ca756a75ee6c05ffc6d6999efe3245439e7822b5]
    Former-commit-id: cd7d71f35d195302f0dffdc26dfff0278e63ca7f

--- .config/i3/ns.py
+++ .config/i3/ns.py
@@ -33,29 +33,17 @@ import errno
 import os
 
 q = Queue()
-glob_settings=ns_settings().settings
-marked={}
-
-for i in glob_settings:
-    marked[i]=list()
+settings_=ns_settings().settings
+marked={i:[] for i in settings_}
 
 class named_scratchpad(SingletonMixin):
     def __init__(self):
-        self.settings=ns_settings().settings
         self.group_list=[]
         self.fullscreen_list=[]
-
-        for group in self.settings:
-            self.group_list.append(group)
-
-        self.ns_data="/tmp/ns_data"
-
-    def strwrap(self, s):
-        return "[-- " + s + " --]"
+        [self.group_list.append(gr) for gr in settings_]
 
     def parse_geom(self, group):
-        geom={}
-        geom=re.split(r'[x+]', self.settings[group]["geom"])
+        geom=re.split(r'[x+]', settings_[group]["geom"])
         return "move absolute position {2} {3}, resize set {0} {1}".format(*geom)
 
     def make_mark(self, group):
@@ -67,8 +55,8 @@ class named_scratchpad(SingletonMixin):
             marked[gr][j].command('move container to workspace current')
 
     def toggle(self, gr):
-        if marked[gr] == [] and "prog" in self.settings[gr]:
-            i3.command("exec {}".format(self.settings[gr]["prog"]))
+        if marked[gr] == [] and "prog" in settings_[gr]:
+            i3.command("exec {}".format(settings_[gr]["prog"]))
 
         focused = i3.get_tree().find_focused()
         if self.visible(gr) > 0:
@@ -83,13 +71,10 @@ class named_scratchpad(SingletonMixin):
         if focused.fullscreen_mode:
             focused.command('fullscreen toggle')
             self.fullscreen_list.append(focused)
-
         self.focus(gr)
 
     def restore_fullscreens(self, gr):
-        for i in self.fullscreen_list:
-            i.command('fullscreen toggle')
-
+        [i.command('fullscreen toggle') for i in self.fullscreen_list]
         self.fullscreen_list=[]
 
     def unfocus(self, gr):
@@ -98,7 +83,6 @@ class named_scratchpad(SingletonMixin):
                 sorted(marked[gr], key=lambda im: im.name)
             ):
             marked[gr][j].command('move scratchpad')
-
         self.restore_fullscreens(gr)
 
     def get_windows_on_ws(self,i3):
@@ -118,23 +102,22 @@ class named_scratchpad(SingletonMixin):
             except FileNotFoundError:
                 raise SystemExit("The `xprop` utility is not found!"
                                 " Please install it and retry.")
-
             if '_NET_WM_STATE_HIDDEN' not in xprop:
                 visible_windows.append(w)
 
         return visible_windows
 
-    def iterate_over(self, gr):
+    def iterate_over(self, group):
         focused = i3.get_tree().find_focused()
-        self.focus(gr)
-        for j,i in zip(
-            range(len(marked[gr])),
-            sorted(marked[gr], key=lambda im: im.name)):
-
-            if focused.id != i.id:
-                marked[gr][j].command('move container to workspace current')
-                i.command('move scratchpad')
-        self.focus(gr)
+        self.focus(group)
+        for number,win in zip(
+                range(len(marked[group])),
+                sorted(marked[group], key=lambda im: im.name)
+            ):
+            if focused.id != win.id:
+                marked[group][number].command('move container to workspace current')
+                win.command('move scratchpad')
+        self.focus(group)
 
     def visible(self, gr):
         visible_windows = self.find_visible_windows(self.get_windows_on_ws(i3))
@@ -147,41 +130,11 @@ class named_scratchpad(SingletonMixin):
 
     def hide_current(self):
         focused = i3.get_tree().find_focused()
-        for group in glob_settings:
+        for group in settings_:
             for i in sorted(marked[group], key=lambda im: im.name):
                 if focused.id == i.id:
                     self.unfocus(group)
                     return
-
-    def scratch_list(self):
-        v=[]
-        for i in self.settings:
-            v.append(i)
-        return v
-
-def mark_group(self, event):
-    def scratch_move(by):
-        con.command(ns.make_mark(group)+', move scratchpad,'+ns.parse_geom(group))
-        marked[group].append(con)
-
-    def check_class():
-        return bool(con.window_class in ns.settings[group]["classes"])
-
-    def check_instance():
-        return bool(con.window_instance in ns.settings[group]["instances"])
-
-    for group in glob_settings:
-        ns=named_scratchpad.instance()
-        con=event.container
-        try:
-            if check_class():
-                scratch_move(by="class")
-                return
-            if check_instance():
-                scratch_move(by="instance")
-                return
-        except KeyError:
-            pass
 
 fifo_=os.path.realpath(os.path.expandvars('$HOME/tmp/ns_scratchpad.fifo'))
 if os.path.exists(fifo_):
@@ -202,17 +155,17 @@ def fifo_listner():
                 break
             eval_str=data.split('\n', 1)[0]
             args=list(filter(lambda x: x != '', eval_str.split(' ')))
-            if len(args) > 0:
-                if args[0] == "show":
-                    ns.focus(args[1])
-                elif args[0] == "hide":
-                    ns.unfocus(args[1])
-                elif args[0] == "toggle":
-                    ns.toggle(args[1])
-                elif args[0] == "next":
-                    ns.iterate_over(args[1])
-                elif args[0] == "hide_current":
-                    ns.hide_current()
+            switch = {
+                "show": ns.focus,
+                "hide": ns.unfocus,
+                "next": ns.iterate_over,
+                "toggle": ns.toggle,
+                "hide_current": ns.hide_current,
+            }
+            if len(args) == 2:
+                switch[args[0]](args[1])
+            elif len(args) == 1:
+                switch[args[0]]()
 
 def worker():
     while True:
@@ -226,6 +179,30 @@ def mainloop_ns():
         q.put(fifo_listner())
         Thread(target=worker).start()
 
+def mark_group(self, event):
+    def scratch_move(by):
+        con.command(ns.make_mark(group)+', move scratchpad,'+ns.parse_geom(group))
+        marked[group].append(con)
+
+    def check_class():
+        return bool(con.window_class in settings_[group]["classes"])
+
+    def check_instance():
+        return bool(con.window_instance in settings_[group]["instances"])
+
+    for group in settings_:
+        ns=named_scratchpad.instance()
+        con=event.container
+        try:
+            if check_class():
+                scratch_move(by="class")
+                return
+            if check_instance():
+                scratch_move(by="instance")
+                return
+        except KeyError:
+            pass
+
 def mark_all(hide=True):
     def scratch_move(by):
         if hide:
@@ -236,13 +213,13 @@ def mark_all(hide=True):
         marked[group].append(con)
 
     def check_class():
-        return bool(con.window_class in ns.settings[group]["classes"])
+        return bool(con.window_class in settings_[group]["classes"])
 
     def check_instance():
-        return bool(con.window_instance in ns.settings[group]["instances"])
+        return bool(con.window_instance in settings_[group]["instances"])
 
     window_list = i3.get_tree().leaves()
-    for group in glob_settings:
+    for group in settings_:
         ns=named_scratchpad.instance()
         for con in window_list:
             try:
@@ -256,10 +233,9 @@ def mark_all(hide=True):
                 pass
 
 def cleanup_mark(self, event):
-    cwid=event.container.id
-    for tag in glob_settings:
+    for tag in settings_:
         for j,win in zip(range(len(marked[tag])),marked[tag]):
-            if win.id == cwid:
+            if win.id == event.container.id:
                 del marked[tag][j]
 
 def cleanup_all():
