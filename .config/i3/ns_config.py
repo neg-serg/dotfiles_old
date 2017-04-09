f84bf9722aa1cc09d4b805f3aa6f8bd0d519c1d5
Neg <serg.zorg@gmail.com>
2017-04-09 17:57:46 +0300

    [i3] fix size
    Former-commit-id: 51a0240a69e7c98e35f3c5e3e4df35caac909359 [formerly f3ee4af241eff12678113b89444d66c915bff54e] [formerly d1b4904b9a4c9e3120ca1778518951d4b80c8a4c [formerly f9d841fd6be44621c0bd203ee5dc7b93fb991877]]
    Former-commit-id: 3ee6d605e187804e92f949267844949cadb14335 [formerly 0e6360cf15dce4066145e3a5016b1642a9082bb7]
    Former-commit-id: 6bd6a97ad0543ec6564eac3a31a9709929af3945

--- .config/i3/ns_config.py
+++ .config/i3/ns_config.py
@@ -35,7 +35,7 @@ class ns_settings(object):
             },
             'teardrop': {
                 'classes' : { 'teardrop' },
-                'geom' : "1844x731+38+2",
+                'geom' : "1844x704+39+4",
                 'prog' : "st -c teardrop -f \'PragmataPro for Powerline:size=18\' -e ~/bin/scripts/teardrop"
             }
         }
