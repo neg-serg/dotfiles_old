7206f9cd8d0c1eac9a3d692ca3093a5ebfbd4d9b
Neg <serg.zorg@gmail.com>
2017-04-23 18:52:31 +0300

    [i3] for_window info from ns_conf
    Former-commit-id: 6952ac308d2b2c4022749b9fc782f075276b578c [formerly d6bac7011e68428a6c40a6dde63432c03e413b11] [formerly e73e613dc4401c2eaebc54ca2f810a7e77f56395 [formerly 0af4e9d88046c8ba62f798a43aa1cf2b139af7f5]]
    Former-commit-id: 9210b31bb99783f27cea29cd122059d7122e0bfb [formerly 4056f4980dbc4790069be9a44a9d6d7d9a83a45d]
    Former-commit-id: 2612a8871bde2114374727acdaadf4a547b54c48

--- .config/i3/ns_conf.py
+++ .config/i3/ns_conf.py
@@ -1,10 +1,62 @@
 #!/usr/bin/env python3
+
+import re
+
 class ns_settings(object):
-    group_list=[]
-    ns_data=""
-    settings={}
+    def init_i3_win_cmds(self):
+        def ch_(list, ch):
+            if len(list) > 1:
+                ret=ch
+            else:
+                ret=''
+            return ret
+
+        def parse_attr_(attr):
+            ret=""
+            attrib_list=self.settings[tag][attr]
+            if len(attrib_list)>1:
+                ret+='('
+            for iter,item in zip(range(len(attrib_list)),attrib_list):
+                if iter+1 < len(attrib_list):
+                    ret+=item+'|'
+                else:
+                    ret+=item
+            if len(attrib_list) > 1:
+                ret+=")$"
+            ret+='] '
+
+            return ret
+
+        def mv_scratch():
+            return "move scratchpad, "
+
+        def hide_cmd():
+            return ", [con_id=__focused__] scratchpad show"
+
+        def ret_info(key, crop):
+            if key in attr:
+                lst=[item for item in self.settings[tag][key] if item != '']
+                if lst != []:
+                    pref="for_window [" + '{}='.format(attr[:-crop]) + ch_(self.settings[tag][attr],'^')
+                    for_win_cmd=pref + parse_attr_(key) + mv_scratch() + self.parse_geom(tag) + hide_cmd()
+                    return for_win_cmd
+            return ''
+
+        cmd_list=[]
+        for tag in self.settings:
+            for attr in self.settings[tag]:
+                cmd_list.append(ret_info('classes',2))
+                cmd_list.append(ret_info('instances',1))
+
+        self.cmd_list=filter(lambda str: str!='', cmd_list)
+
+    def parse_geom(self, group):
+        geom=re.split(r'[x+]', self.settings[group]["geom"])
+        return "move absolute position {2} {3}, resize set {0} {1}".format(*geom)
 
     def __init__(self):
+        self.cmd_list=[]
+
         self.settings = {
             'im' : {
                 'classes' : {
