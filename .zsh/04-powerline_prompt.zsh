61190da45af9e47d88f163280c0cc9c22fba6a72
Neg <serg.zorg@gmail.com>
2017-05-04 19:03:35 +0300

    [zsh] cleanup powerline prompt
    Former-commit-id: fd542d79352a9ecde4e2d50040eeb0678b02e1fa [formerly 57a0bfca5321b91c2bba8974fdd0f88480a978f0] [formerly 341048095bbcf01fed91462ce4b360880d983f88 [formerly a33066dbe583e03a67f3cc9864e77ac456962af8]]
    Former-commit-id: 79a8c67f508320821e162312c2bb3a8c1bc526e6 [formerly 2aef5e1bc2db249797e6eaafa942f69f98af5f3f]
    Former-commit-id: fb9f7c5b2632ae601e30a8f4da755eb0b0346e14

--- .zsh/04-powerline_prompt.zsh
+++ .zsh/04-powerline_prompt.zsh
@@ -1,15 +1,12 @@
 powerline-daemon -q
-local powerline_path_="/usr/lib/python3.5/site-packages/powerline"
+python_version=$(python --version|grep -wo '[0-9]\.[0-9]')
+local powerline_path_="/usr/lib/python${python_version}/site-packages/powerline"
 source "${powerline_path_}/bindings/zsh/powerline.zsh"
-
 DARK_BLUE="%{"$'\033[00;38;5;4m'"%}"
 _neg_user_pretok="${DARK_BLUE}[${NOCOLOR}"
 function precmd(){ 
-    # export PS1="$("$POWERLINE_COMMAND" $=POWERLINE_COMMAND_ARGS shell aboveleft -r .zsh --last-exit-code=$? --last-pipe-status="$pipestatus" --renderer-arg="client_id=$$" --renderer-arg="shortened_path=${(%):-%~}" --jobnum=$_POWERLINE_JOBNUM --renderer-arg="mode=$_POWERLINE_MODE" --renderer-arg="default_mode=$_POWERLINE_DEFAULT_MODE" --width=$(( ${COLUMNS:-$(_powerline_columns_fallback)} - ${ZLE_RPROMPT_INDENT:-1} )))"
     export PS1="${_neg_user_pretok}%40<..<$(${ZSH}/neg-prompt)"
 }
 PS2="%{$fg[magenta]%}» %{$reset_color%}"
-# selection prompt used within a select loop.
-PS3='?# '
-# the execution trace prompt (setopt xtrace). default: '+%N:%i
-PS4='+%N:%i:%_> '
+PS3='?# ' # selection prompt used within a select loop.
+PS4='+%N:%i:%_> ' # the execution trace prompt (setopt xtrace). default: '+%N:%i
