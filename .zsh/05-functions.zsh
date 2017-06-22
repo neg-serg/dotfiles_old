c35e98c6c49cac215e3300f1350df64e9098d98b
Neg <serg.zorg@gmail.com>
2017-06-23 00:53:08 +0300

    [zsh] add python switch function
    Former-commit-id: 5c623d5d6657cc68cba851a712edec1c5d8bbcbf [formerly 1438756a0c77ac31b9ca9f4bba5e4be970f7babf] [formerly e951fde6d3258985b18912d42a8eb06764abdb63 [formerly aa8b854c6965e62ef928d511ba37388a8a2bc4d4]]
    Former-commit-id: fc9c651e342239d751c816baf167069cd7812629 [formerly 70259f787794958160685bd4144855ae3a4e46e4]
    Former-commit-id: ce70b9a23c11bb1ee07fcfeee93e28dc596b6bd5

--- .zsh/05-functions.zsh
+++ .zsh/05-functions.zsh
@@ -326,6 +326,21 @@ function set_proxy(){
     fi
 }
 
+function py23switch(){
+    python_path="$(which python)"
+
+    if [[ $(basename $(readlink /usr/sbin/python)) == python3 ]]; then
+        echo ':: set python (3 to 2) ::'
+        sudo ln -fs /usr/sbin/python2 /usr/sbin/python && \
+            l ${python_path}
+    elif
+        [[ $(basename $(readlink /usr/sbin/python)) == python2 ]]; then
+        echo ':: set python (2 to 3) ::'
+        sudo ln -fs /usr/sbin/python3 /usr/sbin/python && \
+            l ${python_path}
+    fi
+}
+
 
 ::() { echo -e "\e[0;31m:: \e[0;32m$*\e[0m" >&2 "$@" }
 
