3e8facc523acf15ec9249f2d86d556dea33549aa
Neg <serg.zorg@gmail.com>
2017-06-22 13:17:02 +0300

    [xinit] missed to update compton config ...
    Former-commit-id: 6f14bc989fdb81b00679fcec19d6c22a0866c96b [formerly 2656b52ab9e8decdc2c353ecab9b47ba15f7d699] [formerly bed69d0413c4b4dd8351d004c7fb6a72cd710922 [formerly 294c0a6e6428b7b8ef348dda2403e1db7de320c8]]
    Former-commit-id: ba52d0d96e1b5bdb6122097ac8ecbd7cd89a118c [formerly cc0802c2ddfc6150b20e95fea44688b458ab56d9]
    Former-commit-id: 14d25fc23398a23263bd2058da12b921059c83b5

--- .config/xinit/compositing.zsh
+++ .config/xinit/compositing.zsh
@@ -2,5 +2,10 @@
 
 if [[ -z "$1" ]]; then
     nexec compton || sudo pkill compton
-    compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf &
+    if [[ $(egrep -i " connected|card detect|primary dev|Setting driver" /var/log/Xorg.0.log \
+        | grep -wo 'Using Kernel Mode Setting driver: i915') == "" ]]; then
+        compton -b --config "${XDG_CONFIG_HOME}/compton/compton.conf" &
+    else
+        compton -b --config "${XDG_CONFIG_HOME}/compton/compton_intel.conf" &
+    fi
 fi
