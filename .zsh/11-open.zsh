9e37df86d3759d1cb01817163ecc9181a4fa2dbe
Neg <serg.zorg@gmail.com>
2017-06-25 23:37:25 +0300

    [zsh] fix open
    Former-commit-id: 88ec312a37e6942e6a2d9079296e532bcbee07ca [formerly 2030d6a90b02206c29dec68f49bd105ea23ef361] [formerly 0e0ada726cc92da4c928e0ef17fb668d308e3de1 [formerly d68b54375c02461faa4f7df8dc1f902b7575f66b]]
    Former-commit-id: f5aa3de3f5ee59db485fd9dea973f037658b4dce [formerly 03acfd0e24adff4e51229b6cbcb4a460ec798736]
    Former-commit-id: cd8328c855091baad6169ce69f97cd7dff2d634c

--- .zsh/11-open.zsh
+++ .zsh/11-open.zsh
@@ -7,9 +7,7 @@ function open(){
     local doc_reader="zathura"
     local image_viewer="~/bin/scripts/sxiv_browser.sh"
 
-    if [[ -d $1 ]]; then
-        urxvt --chdir "$1"
-    elif [[ -e $1 ]]; then
+    if [[ -e $1 ]]; then
         mime_type=$(file -L -b --mime-type "$1")
         # the order is important, e.g. foo/bar must appear before foo/*
         case ${mime_type} in
@@ -22,6 +20,7 @@ function open(){
             image/*) ${image_viewer} "$1" ;;
             application/postscript) [[ $# -le 9 ]] && ${doc_reader} "$@" >/dev/null 2>/dev/null &! ;;
             application/pdf) [[ $# -le 9 ]] && ${doc_reader} "$@" >/dev/null 2>/dev/null &! ;;
+            application/djvu) [[ $# -le 9 ]] && ${doc_reader} "$@" >/dev/null 2>/dev/null &! ;;
             application/epub) [[ $# -le 9 ]] && ${doc_reader} "$@" >/dev/null 2>/dev/null &! ;;
             application/x-bittorrent) ta "$1" ;;
             application/vnd.ms-opentype\
@@ -36,25 +35,15 @@ function open(){
                 *.svg) display "$1" ;;
                 *.pps|*.PPS|*.ppt|*.PPT) ${web_browser} "$1" ;;
                 *.rtf|*.doc|*.docx)  libreoffice "$1" 2>/dev/null ;;
-                *.epub|*.ps|*.pdf|*.cb) ${doc_reader} "$@" >/dev/null 2>/dev/null &! ;;
+                *.epub|*.ps|*.pdf|*.cb|*.djvu) ${doc_reader} "$@" >/dev/null 2>/dev/null &! ;;
                 *.xls) catdoc -w -s cp1251 "$1" ;;
                 *.xpm) xls2csv -s cp1251 "$1" ;;
                 *.mp3|*.m3u|*.ogg) ${audio_player} "$1" ;;
                 *.mp4|*.avi|*.mpg|*.mpeg|*.mkv|*.ogv|*.f4v|*.m2ts) ${vid_pl} "$1" ;;
-                *) mime_encoding=$(file -L -b --mime-encoding "$1")
-                    case ${mime_encoding} in
-                        *) ${editor} "$1" ;;
-                    esac
-                    ;;
             esac
             ;;
-    esac
-else
-    case "$1" in
-        *://*) ${web_browser} "$1" ;;
-        *) echo "file not found: '$1'" >&2 ;;
-    esac
-fi
+        esac
+    fi
 }
 
 function finfo() {
