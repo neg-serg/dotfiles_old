cff77d43186e4bab4aad4865cbacd271ec32ea7e
Neg <serg.zorg@gmail.com>
2017-07-10 00:32:15 +0300

    [xinit] old touchpad settings becomes deprecated
    Former-commit-id: e2dededb85012c14bb757393f7e4440602242943 [formerly f90222d305d38f3226410492a9094a92e23cbc26] [formerly 720dd6cffa67839de824e1ab371b9fca92ee382d [formerly 086f90f839a8833d5c71a961f7c9186698dc7d28]]
    Former-commit-id: d8397efd1169a2323eb26c1b2e5302f568596658 [formerly 64aeb90735d1a4b91d299341aab7b79fcf7640a6]
    Former-commit-id: 26c6e11586e563cdac0f4e3b883833b3f50746ac

--- .config/xinit/touchpad.zsh
+++
@@ -1,8 +0,0 @@
-if egrep -iq 'touchpad' /proc/bus/input/devices; then
-    synclient PalmDetect=1
-    synclient VertEdgeScroll=0 &
-    synclient TapButton1=1 &
-    synclient HorizTwoFingerScroll=1 &
-    syndaemon -t -k -i 2 -d
-    syndaemon -i 0.8 -d &
-fi
