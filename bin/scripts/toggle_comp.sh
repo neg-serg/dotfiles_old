19b0a3ba15af9d276dc9892551b2c1bc6d65d426
Neg <serg.zorg@gmail.com>
2017-07-26 01:13:38 +0300

    [bin/scripts] cleanup
    Former-commit-id: 17ac03fbc1d5322bc538e7a7d79108833f70952c [formerly f10ef260b3456a8d771fc907da43ad43ead02777] [formerly db6aaba8c883925c2bcaa6524bdfb461e7454c3e [formerly a0420503b3e0e18db071b45df9e8b6dea01fc69b]]
    Former-commit-id: d93415984fac6ef7f335e6b58f0fdbfdb9cd0e81 [formerly a8f5cfbe554ece10db21c8b2255d414bb3897e91]
    Former-commit-id: 6e4d644200673782b823922b94c7d5178751733d

--- bin/scripts/toggle_comp.sh
+++ bin/scripts/toggle_comp.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
-if [[ $1 == "twice" ]]; then
-    killall compton && compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf 2> /dev/null&
+#!/bin/dash
+if [ $1 -eq "twice" ]; then
+    killall compton && compton -b --config $XDG_CONFIG_HOME/compton/compton.conf 2> /dev/null&
 else
-    if [[ $(pidof compton) ]]; then
+    if pidof compton > /dev/null; then
         killall compton 
     else
-        compton -b --config ${XDG_CONFIG_HOME}/compton/compton.conf 2> /dev/null &
+        compton -b --config $XDG_CONFIG_HOME/compton/compton.conf 2> /dev/null &
     fi
 fi
--- bin/scripts/toggle_layout.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ $(setxkbmap -print | grep xkb_symbols | awk '{print $4}' | cut -d"+" -f2) = ru ]; then
-        setxkbmap us;
-else
-        setxkbmap ru;
-fi
