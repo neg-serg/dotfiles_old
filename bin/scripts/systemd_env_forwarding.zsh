6ffef0fdb52cd740274fa4f7f2a1ca17fe3c0032
Neg <serg.zorg@gmail.com>
2017-07-26 03:03:01 +0300

    [bin/scripts] there is no need to explicitly forward env
    Former-commit-id: b2175e7c0d68c1dfbb8e6042452e281fa0921eea [formerly 674382cd31d2efc03cdc395eb490dc7650280239] [formerly d661acf707b1870c69876810b3eeb00fc4a32775 [formerly 96b72ad462f21e8200e020a01f0df971ab550828]]
    Former-commit-id: 194805b019067528328cb2769d7f39e1873b2325 [formerly 70ff58061fabbbaadf6f85c39864107ea746fa28]
    Former-commit-id: 6f1828e88afb98a56604f3bfcbc672c3e5176fe8

--- bin/scripts/systemd_env_forwarding.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-systemctl --user import-environment DISPLAY XAUTHORITY
-if which dbus-update-activation-environment >/dev/null 2>&1; then
-    dbus-update-activation-environment DISPLAY XAUTHORITY
-fi
-xhost +local:
