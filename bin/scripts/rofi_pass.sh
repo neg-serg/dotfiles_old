df2b2896c11dc3a8cc63d0b7471bbacf36a3458f
Neg <serg.zorg@gmail.com>
2017-07-26 04:57:06 +0300

    [bin] cleanup
    Former-commit-id: 36f28705970febd563d8fef4ad6141c1fc1addee [formerly ac374ec9c32d0d64944e6295ba100158914dab94] [formerly b6c437dbd015651751f58cfddfd155dacbd43a8d [formerly 3cabecb7063b4439613b485697e4b0f82ad60d99]]
    Former-commit-id: ccdb1d3c26d618ae50e5fd525ce51ee640354df0 [formerly c4510363c4b8587f0616ba502876918d4bb62522]
    Former-commit-id: 8c2a812082334c0585f79772bbc069a79157928d

--- bin/scripts/rofi_pass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-if [[ -n "$@" ]]; then
-	coproc { p clip "$@" > /dev/null 2>&1 } &
-    echo "$@" > "$(readlink -f ~/tmp)/pass_rofi_picked"
-	exit
-fi
-
-cat "$(readlink -f ~/tmp)/pass_rofi_fifo"
