717c2b2f7d69c22e7528f60198e331ac7894d80c
Neg <serg.zorg@gmail.com>
2017-07-28 21:19:06 +0300

    [bin] hi_gen: add support for bold, italic, use neovim
    Former-commit-id: 401a4b089126f927521cc3522e3fed123ba7662a [formerly fc2d84ef614f54d85d907818390f0dec3596f291] [formerly 8e6effd76bd762e5a2cdd4d544aa5e5a06e41968 [formerly 0de6d78711b261fd718bee22d7553af1a53f6102]]
    Former-commit-id: 4b3885aee2f15c2d87cd86c02ae4236dc3c7cb8f [formerly 39f519e8e6eee53d1d4115a87d5a2d5a1f351033]
    Former-commit-id: 2ec97088db73a0ae7622d07ef7d3bff2c7ca06f6

--- bin/scripts/hi_gen.zsh
+++ bin/scripts/hi_gen.zsh
@@ -21,8 +21,10 @@ function main() {
     readonly ftype_rule="/tmp/ftype_rule"
 
     readonly is_bold="\\(01;\|;1\\)"
+    readonly is_italic="\\(03;\|;3\\)"
     readonly is_uline="\\(04;\|;4\\)"
     readonly bold_regex="/${is_bold}/"
+    readonly italic_regex="/${is_italic}/"
     readonly uline_regex="/${is_uline}/"
 
     readonly fg_eq="38;5;" 
@@ -36,21 +38,27 @@ function main() {
     _zwrap "You should handle it by hand:"
     _zwrap "-----------------------------"
 
-    egrep "^${ftype_pref}" -v "${tmp_file}"
-    egrep "^${ftype_pref}" "${tmp_file}" | sed -e "s/${fg_eq}/fg=/"               \
-                                               -e "s/${bg_eq}/bg=/g"              \
-                                               -e "${bold_regex}s/$/,bold/"       \
-                                               -e "${uline_regex}s/$/,underline/" \
-                                               -e "s/00;//"                       \
-                                               -e "s/${is_bold}//"                \
-                                               -e "s/${is_uline}//"               \
-                                               -e "s/;/,/g"                       \
-                                               -e "/\ 3[0-9][0-9]*/s/3/fg=/"      \
-                                               -e "s/, //g" > "${ftype_arr}"
+    local -a sed_args=(
+        -e "s/${fg_eq}/fg=/"               
+        -e "s/${bg_eq}/bg=/g"              
+        -e "${bold_regex}s/$/,bold/"       
+        -e "${italic_regex}s/$/,italic/"       
+        -e "${uline_regex}s/$/,underline/" 
+        -e "s/00;//"                       
+        -e "s/${is_bold}//"                
+        -e "s/${is_uline}//"               
+        -e "s/${is_italic}//"               
+        -e "s/;/,/g"                       
+        -e "/\ 3[0-9][0-9]*/s/3/fg=/"      
+        -e "s/, //g"
+    )
 
-    nohup zsh -c "vim +'Tabularize/[bf]g=.*' "${ftype_arr}" +'wq'" 2> /tmp/vimlol
+    egrep "^${ftype_pref}" -v "${tmp_file}"
+    egrep "^${ftype_pref}" "${tmp_file}" | sed ${sed_args} > "${ftype_arr}"
+
+    nohup zsh -c "nvim +'Tabularize/[bf]g=.*' "${ftype_arr}" +'wq'" 2> /tmp/vimlol
     sed "s/^${ftype_pref}//" "${ftype_arr}" |awk '{print "*."$1") style=ftype-"$1" ;;"}' > "${ftype_rule}"
-    nohup zsh -c "vim +'Tabularize/)\zs ' "${ftype_rule}" +'wq'" 2> /tmp/vimlol
+    nohup zsh -c "nvim +'Tabularize/)\zs ' "${ftype_rule}" +'wq'" 2> /tmp/vimlol
 
     for t in "${ftype_arr}" "${ftype_rule}"; eat "${t}"
     rm "${tmp_file}"
