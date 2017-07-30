6dc489e08b4f2670c2f38deb8ecd8d238fd052bd
Neg <serg.zorg@gmail.com>
2017-07-30 21:33:33 +0300

    [bin/scripts] update lock script
    Former-commit-id: cc4b780c908c36b974e4e42d86de0125614b7fc6 [formerly 2a7f35e700f9a7cba136b3d9be87d12cf5d554ae] [formerly 2adcd8b283f22b876f4abffda67377f2c4f55218 [formerly 38db46123d27da6a9f36d1b24802ad29ea50bec0]]
    Former-commit-id: d8179fbb2f04e138e4c114bb4f75b9e09a4c2123 [formerly 1955c86018de8950e81cacbdd5ec298f66275b52]
    Former-commit-id: a54eb917a729c33c57e1744469defad40b8a7ca5

--- bin/scripts/lock/lock.sh
+++ bin/scripts/lock/lock.sh
@@ -1,46 +1,54 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
-icon='/one/dev/src/hyperbeast/Scripts/lock.png'
-tmpbg='/one/dev/src/hyperbeast/Scripts/lol.png'
+icon="$(readlink -f lock.png)"
+tmpbg="$(readlink -f lol.png)"
 
-(( $# )) && { icon=$1; }
+function i3lock_setup_params(){
+    B='#00000000'  # blank
+    C='#00000070'  # clear ish
+    D='#0e3066cc'  # default
+    T='#ffffffee'  # text
+    W='#e02772bb'  # wrong
+    I='#b5bd2dbb'  # input
+    V='#ffffffbb'  # verifying
 
-scrot "${tmpbg}"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-# convert "${tmpbg}" -blur 0x6 "${tmpbg}"
-convert "${tmpbg}" "$icon" -gravity center -composite -matte "${tmpbg}"
+    i3lock_params=(
+        --image=${tmpbg}
+        --insidecolor=${B}
+        --ringcolor=${D}
+        --linecolor=${B}
+        --separatorcolor=${B}
+        --insidevercolor=${C}
+        --ringvercolor=${D}
+        --insidewrongcolor=${C}
+        --ringwrongcolor=${W}
+        --textcolor=${T}
+        --timecolor=${T}
+        --datecolor=${T}
+        --keyhlcolor=${I}
+        --bshlcolor=${W}
+    )
+}
 
-B='#00000000'  # blank
-C='#00000070'  # clear ish
-D='#0e3066cc'  # default
-T='#ffffffee'  # text
-W='#e02772bb'  # wrong
-I='#b5bd2dbb'  # input
-V='#ffffffbb'  # verifying
+function i3lock_add_clock(){
+    i3lock_params+=(
+        --clock
+        --indicator
+        --timestr="%H:%M:%S"
+        --datestr="%A, %m %Y"
+    )
+}
 
-i3lock                \
---image=${tmpbg}        \
-\
---insidecolor=${B}      \
---ringcolor=${D}        \
---linecolor=${B}        \
---separatorcolor=${B}   \
-\
---insidevercolor=${C}   \
---ringvercolor=${D}     \
-\
---insidewrongcolor=${C} \
---ringwrongcolor=$W   \
-\
---textcolor=${T}        \
---timecolor=${T}        \
---datecolor=${T}        \
---keyhlcolor=${I}       \
---bshlcolor=${W}        \
-#\
-#--screen 0            \
-#--blur 5              \
-#--clock               \
-#--indicator           \
-#--timestr="%H:%M:%S"  \
-#--datestr="%A, %m %Y" \
+function main(){
+    i3lock_setup_params
+    (( $# )) && { icon=$1; }
+    scrot "${tmpbg}"
+    case $1 in
+        p*) convert "${tmpbg}" -scale 10% -scale 1000% "${tmpbg}";;
+        *)  convert "${tmpbg}" -blur 0x6 "${tmpbg}";;
+    esac
+    convert "${tmpbg}" "$icon" -gravity center -composite -matte "${tmpbg}"
+    i3lock "${i3lock_params[@]}"
+}
+
+main "$@"
