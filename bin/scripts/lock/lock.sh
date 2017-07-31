63f8cdf273a1664d018afc181e0697acf7f21e8f
Neg <serg.zorg@gmail.com>
2017-07-31 05:04:02 +0300

    [bin/scripts] lock: fix image size
    Former-commit-id: ee942f3f84c77b64dfd965633c2a981c8751ccd0 [formerly 815d5a6d9255c71e652ee3a219bf49caf7c8a718] [formerly 2d9fc122da41b8cc4175812615e2762773b316a2 [formerly e42e435303814d451cb1f8daef6281afcbc58229]]
    Former-commit-id: 2022bfbc471804858e52b268321f1db4075af163 [formerly 59686be7aca550a4704be7c3badc4532a1eb5726]
    Former-commit-id: fb739ebe9d7ba6c721f3400379f3099580c6eb82

--- bin/scripts/lock/lock.sh
+++ bin/scripts/lock/lock.sh
@@ -75,8 +75,8 @@ function main(){
             make_blur
             make_composition ;;
         *)
-            ffmpeg -f x11grab -y -i ${DISPLAY} -i ${icon} -filter_complex "boxblur=5,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 ${tmpbg} 2> /dev/null
-            i3lock "${i3lock_params[@]}" ;;
+            resolution_=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
+            ffmpeg -f x11grab -video_size "${resolution_}" -y -i ${DISPLAY} -i ${icon} -filter_complex "boxblur=5,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 ${tmpbg} 2> /dev/null
     esac
 
     i3lock "${i3lock_params[@]}"
