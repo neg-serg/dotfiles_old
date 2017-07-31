ac03e8a1a854724a8653ac9ce482dca95bf1e2de
Neg <serg.zorg@gmail.com>
2017-07-31 19:40:18 +0300

    [bin/mon] cleanup/move
    Former-commit-id: b84d8b64517b41543b3acb9beec2dc669f701ef3 [formerly 36f0f8d7a69e5d3d2418080320ce2bdf7dc92c24] [formerly 9fded2a5d0fa8e687e168ff83821954cabd27b52 [formerly 75708d3ed3db22387180a7df4c17dfbd31cdcd62]]
    Former-commit-id: 5c98aeb73e62d87c10eb61234a2a0154410d6916 [formerly 24e478286fabbe6778c9d53919f7462b94ed8227]
    Former-commit-id: 0b0cce3a1fcc857fd9fa279a8dba25d771f4be12

--- bin/mon/crntsng.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-l1=`mpc|head -1`; l2=mpc|head -2;
-l2=`$l2|perl -we 'undef $/; $x=<>;$x =~ /(\d+:\d.*)/;print $1;'`
-Vol=`mpc volume|awk -F \:  '{print $2}'`
-Vol=`echo $Vol|perl -pe 's/ //g'`
-if [[ $(echo $(mpc|grep '\[paused\]')) == "" && $(echo $(mpc|wc -l)) > 1  &&  $(echo $(mpc|grep 'ERROR')) == "" ]];
-then echo "[>>][ $l1 $l2 ][ Vol: $Vol ]";
-else if [[ $(echo $(mpc|wc -l)) == 4 ]]; then echo "[>>][ $l1 $l2 ][ Vol: $Vol ]"; else echo "" ; fi; fi
